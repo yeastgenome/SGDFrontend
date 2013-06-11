2668d19e24bd46dd7d9223191fc1cb8ca256c030
GregRR <greg@youwanttoknow.com>
2013-06-11 14:35:55 -0700

    Updated paths for 'fontend' repo.

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'zurb-foundation'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "sgd2/static/css"
-sass_dir = "sgd2/static/scss"
-images_dir = "sgd2/static/img"
-javascripts_dir = "sgd2/static/js"
-fonts_dir = "sgd2/static/fonts"
+css_dir = "sgdfrontend/static/css"
+sass_dir = "sgdfrontend/static/scss"
+images_dir = "sgdfrontend/static/img"
+javascripts_dir = "sgdfrontend/static/js"
+fonts_dir = "sgdfrontend/static/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
