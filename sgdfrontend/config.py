ab5fb51ec50c5c3a5d49b8c939c4043d07812ff1
kpaskov <kpaskov@stanford.edu>
2013-08-07 17:18:29 -0700

    Accidentally over-wrote config file.

--- sgdfrontend/config.py
+++ sgdfrontend/config.py
@@ -5,5 +5,5 @@ Created on Oct 18, 2012
 '''
 #Backend Config
 #backend_url = 'http://sgd-ng1.stanford.edu:6543'
-backend_url = 'http://localhost:6543'
-on_the_fly_url = 'http://localhost:6543'
+backend_url = 'http://sgd-ng1.stanford.edu:6543'
+on_the_fly_url = 'http://sgd-ng1.stanford.edu:6543'
