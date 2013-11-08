2c30bbd31d4e67395b27d2883b09d4c03f2d1e02
kpaskov <kpaskov@stanford.edu>
2013-11-08 11:49:16 -0800

    Added log_directory variable to config file.

--- sgdfrontend_utils/__init__.py
+++ sgdfrontend_utils/__init__.py
@@ -26,9 +26,12 @@ def set_up_logging(label):
     logging.basicConfig(format='%(asctime)s %(name)s: %(message)s', level=logging.INFO)
     log = logging.getLogger(label)
     
-    hdlr = logging.FileHandler('sgdfrontend_logs/' + label + '.' + str(datetime.now().date()) + '.txt')
-    formatter = logging.Formatter('%(asctime)s %(name)s: %(message)s')
-    hdlr.setFormatter(formatter)
+    if log_directory is not None:
+        hdlr = logging.FileHandler('sgdfrontend_logs/' + label + '.' + str(datetime.now().date()) + '.txt')
+        formatter = logging.Formatter('%(asctime)s %(name)s: %(message)s')
+        hdlr.setFormatter(formatter)
+    else:
+        hdlr = logging.NullHandler()
     log.addHandler(hdlr) 
     log.setLevel(logging.INFO)
     return log
