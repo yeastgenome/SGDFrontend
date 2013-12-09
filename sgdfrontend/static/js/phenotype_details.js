01722c02495d3a5b19d9341a0b77ed30ed8b58ee
kpaskov <kpaskov@stanford.edu>
2013-12-09 13:50:44 -0800

    Reverted table to old format.

--- sgdfrontend/static/js/phenotype_details.js
+++ sgdfrontend/static/js/phenotype_details.js
@@ -3,18 +3,12 @@ var ev_table;
 function set_up_evidence_table(header_id, phenotype_header_id, table_id, download_button_id, download_link, download_table_filename, data) { 
 	var datatable = [];
 	var format_name_to_id = new Object();
-	
-	var has_chemical = false;
-	var has_allele = false;
-	var has_reporter = false;
 
 	for (var i=0; i < data.length; i++) {
 		var evidence = data[i];
 		
 		format_name_to_id[evidence['bioconcept']['format_name']] = evidence['bioconcept']['id']
-		
-		var icon = create_note_icon('note' + i, evidence['note'])
-		
+
 		var bioent = create_link(evidence['bioentity']['display_name'], evidence['bioentity']['link'])
 			
 		var experiment = '';
@@ -30,7 +24,6 @@ function set_up_evidence_table(header_id, phenotype_header_id, table_id, downloa
 		
 		var chemical = '';
 		if(evidence['chemical'] != null) {
-			has_chemical = true;
 			if(evidence['chemical']['amount'] != null) {
 				chemical = evidence['chemical']['amount'] + ' ' + evidence['chemical']['display_name'];
 			}
@@ -45,29 +38,36 @@ function set_up_evidence_table(header_id, phenotype_header_id, table_id, downloa
 		
 		var allele = '';
 		if(evidence['allele'] != null) {
-			has_allele = true;
-			allele = evidence['allele']['display_name'];
+			allele = '<br>Allele: ' + evidence['allele']['display_name'];
 			var allele_icon = create_note_icon('allele_icon' + i, evidence['allele']['note']);
 			if(allele_icon != '') {
 				allele = allele + ' ' + allele_icon;
 			}
 		}
-		
+
 		var reporter = '';
 		if(evidence['reporter'] != null) {
-			has_reporter = true;
-			reporter = evidence['reporter']['display_name'];
+			reporter = 'Reporter: ' + evidence['reporter']['display_name'];
 			var reporter_icon = create_note_icon('reporter_icon' + i, evidence['reporter']['note']);
 			if(reporter_icon != '') {
 				reporter = reporter + ' ' + reporter_icon;
 			}
 		}
+
+        var note = '';
+        for (var j=0; j < evidence['condition'].length; j++) {
+            note = note + 'Condition: ' + evidence['condition'][j] + '<br>';
+        }
+        if(evidence['note'] != null) {
+            note = note + 'Details: ' + evidence['note'] + '<br>';
+        }
+        note = note + reporter;
 		
 		var biocon = create_link(evidence['bioconcept']['display_name'], evidence['bioconcept']['link']);
 		
-  		var reference = create_link(evidence['reference']['display_name'], evidence['reference']['link']);;
+  		var reference = create_link(evidence['reference']['display_name'], evidence['reference']['link']);
   		
-  		datatable.push([icon, bioent, evidence['bioentity']['format_name'], biocon, experiment, evidence['mutant_type'], strain, chemical, allele, reporter, reference, evidence['note']]);
+  		datatable.push([bioent, evidence['bioentity']['format_name'], biocon, experiment, 'Description: ' + evidence['mutant_type'] + allele, strain, chemical, note, reference]);
   	}
   	document.getElementById(header_id).innerHTML = data.length;
   	var total_interactors = Object.keys(format_name_to_id).length;
@@ -75,8 +75,8 @@ function set_up_evidence_table(header_id, phenotype_header_id, table_id, downloa
   		         
     var options = {};
 	options["bPaginate"] = true;
-	options["aaSorting"] = [[3, "asc"]];
-	options["aoColumns"] = [{"bSearchable":false, 'bSortable': false}, {"bSearchable":false, "bVisible":false}, {"bSearchable":false, "bVisible":false}, null, null, null, null, {"bSearchable":has_chemical, "bVisible":has_chemical}, {"bSearchable":has_allele, "bVisible":has_allele}, {"bSearchable":has_reporter, "bVisible":has_reporter}, null, {"bSearchable":false, "bVisible":false}];
+	options["aaSorting"] = [[2, "asc"]];
+	options["aoColumns"] = [{"bSearchable":false, "bVisible":false}, {"bSearchable":false, "bVisible":false}, null, null, null, null, null, null, null];
 	options["aaData"] = datatable;
   
    	setup_datatable_highlight();				
