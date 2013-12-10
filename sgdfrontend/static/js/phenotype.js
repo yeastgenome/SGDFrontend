35777d6f86be9833aab1646315a94d734edf23c1
kpaskov <kpaskov@stanford.edu>
2013-12-09 16:24:48 -0800

    Added summary to phenotype_details using Chart.js.

--- sgdfrontend/static/js/phenotype.js
+++ sgdfrontend/static/js/phenotype.js
@@ -75,7 +75,7 @@ function set_up_evidence_table(header_id, phenotype_header_id, table_id, downloa
     var options = {};
 	options["bPaginate"] = true;
 	options["aaSorting"] = [[0, "asc"]];
-    options["aoColumns"] = [{"bSearchable":false, "bVisible":false}, {"bSearchable":false, "bVisible":false}, null, null, null, null, null, null, null];
+    options["aoColumns"] = [null, {"bSearchable":false, "bVisible":false}, {"bSearchable":false, "bVisible":false}, null, null, null, null, null, null];
 	options["aaData"] = datatable;
   
    	setup_datatable_highlight();				
