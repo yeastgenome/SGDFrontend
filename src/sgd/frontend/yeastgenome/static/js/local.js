117bf871eed4c1130e25f31302100a91def0b5a5
GregRR <greg@youwanttoknow.com>
2014-07-18 11:45:39 -0700

    New look for all table types.

--- src/sgd/frontend/yeastgenome/static/js/local.js
+++ src/sgd/frontend/yeastgenome/static/js/local.js
@@ -374,7 +374,7 @@ function create_table(table_id, options) {
         options['sDom'] = '<"clearfix" <"dt-tools-head"<"left"><"right" f>>rt<"dt-tools-foot" il <"right" p>>>';
     }
     else {
-        options['sDom'] = '<"clearfix" <"left" f>>t<"clearfix" <"right" i>>';
+        options['sDom'] = '<"clearfix" <"dt-tools-head"<"left"><"right" f>>t<"dt-tools-foot" i <"right">>>';
     }
     options["bAutoWidth"] = false;
     setup_datatable_highlight();
