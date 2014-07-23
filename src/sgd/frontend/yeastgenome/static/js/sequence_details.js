a42a1a4d52ff2f32a6079423c69f2b9bc2ad8167
kpaskov <kpaskov@stanford.edu>
2014-07-23 12:23:11 -0700

    Made location diagrams look a bit more like a browser.

--- src/sgd/frontend/yeastgenome/static/js/sequence_details.js
+++ src/sgd/frontend/yeastgenome/static/js/sequence_details.js
@@ -370,6 +370,10 @@ function draw_label_chart(chart_id, strain_name) {
         options['height'] = $("#" + chart_id + " > div > div > div > svg").height() + 60;
         google.visualization.events.addListener(chart, 'ready', make_label_ready_handler(chart_id, chart, neighbor_data, display_name_to_format_name, data_array));
         chart.draw(dataTable, options);
+
+        //Adjust chart to look more like genome browser
+        $("#" + chart_id + " > div > div > svg > g:first > path:last").attr('stroke', 'black');
+        $("#" + chart_id + " > div > div > svg > g:first > rect:last").attr('stroke', 'white');
     }
 }
 
