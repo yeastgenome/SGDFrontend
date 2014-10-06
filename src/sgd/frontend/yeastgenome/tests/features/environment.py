1e413ac6a541fa0b118185760594a566a0ab1521
kpaskov <kpaskov@stanford.edu>
2014-10-06 12:59:54 -0700

    Working on cytoscape bug.

--- src/sgd/frontend/yeastgenome/tests/features/environment.py
+++ src/sgd/frontend/yeastgenome/tests/features/environment.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 def before_feature(context, feature):
     if 'browser' in feature.tags:
         context.browser = webdriver.Firefox()
+        context.browser.implicitly_wait(30) # seconds
         context.base_url = 'http://localhost:6545'
 
 def after_feature(context, feature):
