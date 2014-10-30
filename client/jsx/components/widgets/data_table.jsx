3d8fbd56dbcca33b0c3f0badfb930c71c35a108b
Travis Sheppard <tshepp@stanford.edu>
2014-10-30 14:33:13 -0700

    restore table highlighting

--- client/jsx/components/widgets/data_table.jsx
+++ client/jsx/components/widgets/data_table.jsx
@@ -43,9 +43,78 @@ module.exports = React.createClass({
 	componentDidMount: function () {
 		if (this.props.usePlugin) {
 			var options = this._getTableOptions();
-			$(this.refs.table.getDOMNode()).dataTable(options);
+			this._setupTableHighlight();
+			var $table = $(this.refs.table.getDOMNode()).dataTable(options);
 			$(document).foundation();
 			$(this.getDOMNode()).find("input").attr("placeholder", "Filter table");
+			$table.fnSearchHighlighting();
+		}
+	},
+
+	// legacy code to highlight search text within a table
+	_setupTableHighlight: function () {
+		$.fn.dataTableExt.oApi.fnSearchHighlighting = function(oSettings) {
+		    // Initialize regex cache
+		    oSettings.oPreviousSearch.oSearchCaches = {};
+		       
+		    oSettings.oApi._fnCallbackReg( oSettings, 'aoRowCallback', function( nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+		        // Initialize search string array
+		        var searchStrings = [];
+		        var oApi = this.oApi;
+		        var cache = oSettings.oPreviousSearch.oSearchCaches;
+		        // Global search string
+		        // If there is a global search string, add it to the search string array
+		        if (oSettings.oPreviousSearch.sSearch) {
+		            searchStrings.push(oSettings.oPreviousSearch.sSearch);
+		        }
+		        // Individual column search option object
+		        // If there are individual column search strings, add them to the search string array
+		        if ((oSettings.aoPreSearchCols) && (oSettings.aoPreSearchCols.length > 0)) {
+		            for (var i in oSettings.aoPreSearchCols) {
+		                if (oSettings.aoPreSearchCols[i].sSearch) {
+		                searchStrings.push(oSettings.aoPreSearchCols[i].sSearch);
+		                }
+		            }
+		        }
+		        // Create the regex built from one or more search string and cache as necessary
+		        if (searchStrings.length > 0) {
+		            var sSregex = searchStrings.join("|");
+		            if (!cache[sSregex]) {
+		                var regRules = "("
+		                ,   regRulesSplit = sSregex.split(' ');
+		                 
+		                regRules += "("+ sSregex +")";
+		                for(var i=0; i<regRulesSplit.length; i++) {
+		                  regRules += "|("+ regRulesSplit[i] +")";
+		                }
+		                regRules += ")";
+		             
+		                // This regex will avoid in HTML matches
+		                cache[sSregex] = new RegExp(regRules+"(?!([^<]+)?>)", 'ig');
+		            }
+		            var regex = cache[sSregex];
+		        }
+		        // Loop through the rows/fields for matches
+		        $('td', nRow).each( function(i) {
+		            // Take into account that ColVis may be in use
+		            var j = oApi._fnVisibleToColumnIndex( oSettings,i);
+		            // Only try to highlight if the cell is not empty or null
+		            if (aData[j]) {         
+		                // If there is a search string try to match
+		                if ((typeof sSregex !== 'undefined') && (sSregex)) {
+		                    this.innerHTML = aData[j].replace( regex, function(matched) {
+		                        return "<span class='filterMatches'>"+matched+"</span>";
+		                    });
+		                }
+		                // Otherwise reset to a clean string
+		                else {
+		                    this.innerHTML = aData[j];
+		                }
+		            }
+		        });
+		        return nRow;
+		    }, 'row-highlight');
+		    return this;
 		}
 	},
 
