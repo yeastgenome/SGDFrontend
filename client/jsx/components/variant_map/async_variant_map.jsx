e93142be51bbc88d82640886770ba5349708da59
Travis Sheppard <tshepp@stanford.edu>
2014-11-25 11:18:44 -0800

    add heatmap component

--- client/jsx/components/variant_map/async_variant_map.jsx
+++ client/jsx/components/variant_map/async_variant_map.jsx
@@ -4,11 +4,59 @@
 var React = require("react");
 var _ = require("underscore");
 
+var DropdownChecklist = require("../widgets/dropdown_checklist.jsx");
+var VariantHeatmap = require("./variant_heatmap.jsx");
+
 module.exports = React.createClass({
 	render: function () {
+		var heatmapNode = this._getHeatmapNode();
 		return (<div>
-			<h1>Variant Map</h1>
+			<div className="row">
+					<div className="columns small-6">
+						<h1>Variant Map</h1>
+					</div>
+					<div className="columns small-6 clearfix">
+						<div style={{ float: "right", marginTop: "0.5em" }}>
+							<DropdownChecklist />
+						</div>
+					</div>
+			</div>
 			<hr />
+			<div style={{ height: 600 }}>
+				{heatmapNode}
+			</div>
 		</div>);
+	},
+
+	_getHeatmapNode: function () {
+		// TEMP
+		// FAKE HEATMAP DATA
+		var heatpmapData = [];
+		var _numGenes = 6400;
+		var numStrains = 14;
+
+		for (var j = 0; j <= _numGenes; j++) {
+			var _variantData = [];
+			for (var k = numStrains; k >= 0; k--) {
+				_variantData.push(Math.random());
+			}
+			heatpmapData.push({
+				name: "Gene" + j,
+				variationData: _variantData
+			});
+		}
+		// strain data for heatmap, also FAKE
+		var _strainMetaData = [];
+		for (var i = numStrains; i >= 0; i--) {
+			_strainMetaData.push ({
+				name: "Strain" + i
+			});
+		};
+		_strainMetaData = _strainMetaData.reverse();
+
+		return (<VariantHeatmap
+			data={heatpmapData}
+			strainData={_strainMetaData}
+		/>);
 	}
 });
---
+++ client/jsx/components/variant_map/variant_heatmap.jsx
@@ -0,0 +1,158 @@
+"use strict";
+var d3 = require("d3");
+var React = require("react");
+var _ = require("underscore");
+
+var DEFAULT_DOM_SIDE_SIZE = 400; // height and width\
+var FONT_SIZE = 14;
+var HEADER_HEIGHT = 60;
+var NODE_SIZE = 16;
+var CANVAS_SIZE = 8000;
+var LABEL_WIDTH = 100;
+
+module.exports = React.createClass({
+	propTypes: {
+		data: React.PropTypes.array.isRequired,
+		onClick: React.PropTypes.func,
+		strainData: React.PropTypes.array.isRequired
+	},
+
+	getInitialState: function () {
+		return {
+			DOMWidth: DEFAULT_DOM_SIDE_SIZE,
+			DOMHeight: DEFAULT_DOM_SIDE_SIZE,
+			canvasScrollY: 0,
+			onClick: null
+		};
+	},
+
+	render: function () {
+		var _scrollZoneSize = this.props.data.length * NODE_SIZE;
+		var _canvasY = this._getCanvasY();
+
+		var strainLabelsNode = this._getLabelsNode();
+		var overlayNode = this._getOverlayNode();
+
+		return (
+		<div className="variant-heatmap" style={{ height: "100%", position: "relative"}}>
+			<div className="heatmap-header" style={{ height: HEADER_HEIGHT }}>
+				{strainLabelsNode}
+			</div>
+			<div ref="outerScroll" style={{ height: this.state.DOMHeight - HEADER_HEIGHT, overflowY: "scroll", position: "relative" }}>
+				<div style={{ position: "relative", height: _scrollZoneSize }}>
+					<canvas ref="canvas" width={this.state.DOMWidth} height={CANVAS_SIZE} style={{ position: "absolute", top: _canvasY }}/>
+				</div>
+				{overlayNode}
+			</div>
+		</div>);
+	},
+
+	componentDidMount: function () {
+		this._calculateWidth();
+		this.refs.outerScroll.getDOMNode().onscroll = _.throttle(this._checkScroll, 100);
+	},
+
+	_getOverlayNode: function () {
+		var chunkedData = this._getChunkedData();
+		var rectNodes = _.map(chunkedData, (d, i) => {
+			var _onClick;
+			if (this.props.onClick) _onClick = (e) => { this.props.onClick(d); };
+			var _transform = `translate(0, ${i * NODE_SIZE})`;
+			return (<g key={"heatmapOverlay" + i} transform={_transform}>
+				<text dy={13} fontSize={FONT_SIZE}>{d.name}</text>
+				<rect width={this.state.DOMWidth} height={NODE_SIZE} x={0} y={0} opacity={0} stroke="none" onClick={_onClick}/>
+			</g>);
+		});
+
+		var _canvasY = this._getCanvasY();
+		return (<svg ref="svg" style={{ position: "absolute", top: _canvasY, width: this.state.DOMWidth, height: CANVAS_SIZE }}>
+			{rectNodes}
+		</svg>);
+	},
+
+	// check to see if the scroll y needs to be redrawn
+	_checkScroll: function () {
+		var scrollTop = this.refs.outerScroll.getDOMNode().scrollTop;
+		var scrollDelta = Math.abs(scrollTop - this.state.canvasScrollY)
+		if (scrollDelta > CANVAS_SIZE / 4) {
+			this.setState({ canvasScrollY: scrollTop });
+		}
+	},
+
+	componentDidUpdate: function () {
+		this._renderCanvas();
+	},
+
+	_calculateWidth: function () {
+		var _clientRect = this.getDOMNode().getBoundingClientRect();
+		this.setState({
+			DOMWidth: _clientRect.width,
+			DOMHeight: _clientRect.height
+		});
+	},
+
+	_getChunkedData: function () {
+		var _canvasY = this._getCanvasY();
+		var _nodesPerCanvas = Math.round(CANVAS_SIZE / NODE_SIZE)
+		var _dataStartIndex = Math.round(this._getYScale().invert(_canvasY));
+		return this.props.data.slice(_dataStartIndex, _dataStartIndex + _nodesPerCanvas);
+	},
+
+	_getLabelsNode: function () {
+		var xScale = this._getXScale();
+		var nodes = this.props.strainData.map( (d, i) => {
+			var _style = {
+				position: "absolute",
+				left: Math.max(0, (i - 1) * NODE_SIZE + LABEL_WIDTH),
+				top: HEADER_HEIGHT / 2,
+				fontSize: FONT_SIZE,
+				transform: "rotate(-90deg)"
+			};
+			return <span key={"strainLabel" + i} style={_style}>{d.name}</span>;
+		});
+
+		return (<div style={{ position: "relative" }}>
+			{nodes}
+		</div>);
+	},
+
+	_getXScale: function () {
+		var maxColWidth = 11; // TEMP
+		return d3.scale.linear()
+			.domain([0, maxColWidth])
+			.range([0, this.state.DOMWidth]);
+	},
+
+	_getYScale: function () {
+		var _totalY = this.props.data.length * NODE_SIZE;
+		return d3.scale.linear()
+			.domain([0, this.props.data.length])
+			.range([0, _totalY]);
+	},
+
+	_getCanvasY: function () {
+		return Math.max(0, this.state.canvasScrollY - CANVAS_SIZE / 2);
+	},
+
+	_renderCanvas: function () {
+		// get canvas context and clear
+		var ctx = this.refs.canvas.getDOMNode().getContext("2d");
+		ctx.clearRect(0, 0, this.state.DOMWidth, this.state.DOMHeight);
+
+		// render rows of features with strain variation in each column
+		var colorScale = d3.scale.linear()
+			.domain([0, 1])
+			.range(["blue", "white"]);
+
+		// get data that fits into canvas
+		var chunkOfData = this._getChunkedData();
+
+		chunkOfData.forEach( (d, i) => {
+			d.variationData.forEach( (_d, _i) => {
+				// get color and draw rect
+				ctx.fillStyle = colorScale(_d);
+				ctx.fillRect(_i * NODE_SIZE + LABEL_WIDTH, i * NODE_SIZE, NODE_SIZE, NODE_SIZE);
+			});
+		});
+	}
+});
