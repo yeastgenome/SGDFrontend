ede308d586ba0c7775c0131a84a73f7b0779ac2d
Travis Sheppard <tshepp@stanford.edu>
2015-01-05 09:19:33 -0800

    bootstrap nav element styled and functional

--- client/jsx/components/widgets/navbar.jsx
+++ client/jsx/components/widgets/navbar.jsx
@@ -23,10 +23,7 @@ module.exports = React.createClass({
 
 	render: function () {
 
-		// title can be link or plain text, depending on if title has href property
-		var titleNode = this.props.title.href ? <a href={this.props.title.href}>{this.props.title.name}</a> : this.props.title;
-
-		var listElements = _.map(this.props.elements, (element) => {
+		var listElements = _.map(this.props.elements, element => {
 			if (!element) return null;
 			return (
 				<li data-magellan-arrival={element.target} id={`navbar_${element.target}`}>
@@ -34,16 +31,19 @@ module.exports = React.createClass({
 				</li>
 			);
 		});
+		var titleNode = this.props.title.href ? <a href={this.props.title.href}>{this.props.title.name}</a> : this.props.title;
+		listElements.unshift(<li key="titleNode" id="nav-title"><h4>{titleNode}</h4></li>);
 
 		var backToTopNode = null;
 		if (this.state.backToTopVisible) {
-			backToTopNode = <a href="#" className="back-to-top" style={{ position: "fixed", display: "inline", zIndex: 1 }}>Back to Top</a>;
+			backToTopNode = <a onClick={this._onClickToTop} href="#" className="back-to-top" style={{ position: "fixed", display: "inline", zIndex: 1 }}>Back to Top</a>;
 		}
 
-		var _style = this.state.backToTopVisible ? { position: "fixed", top: 0 } : {};
+		var _position = this.state.backToTopVisible ? "fixed" : "absolute";
+		var _style = { position: _position, top: "0.5rem" };
 		return (<div>
-			<div className="svg-navbar" style={_style}>
-				<ul className="nav nav-pills nav-stacked">
+			<div className="sgd-navbar" style={_style}>
+				<ul className="nav side-nav">
 					{listElements}
 				</ul>
 			</div>
@@ -55,12 +55,10 @@ module.exports = React.createClass({
 	// Setup magellan nav with jquery helper.
 	// Back to top button.
 	componentDidMount: function () {
-		$("body").scrollspy({ target: ".svg-navbar" })
+		$("body").scrollspy({ target: ".sgd-navbar" })
 
-		// back to top button
+		// fix navbar if scrolling down below 245 px
 		var offset = 245; // how far (px) to scroll to trigger link
-	    var duration = 500; // 0.5s fade in/out
-
 	    var _throttled = _.throttle( () => {
 	    	var yOffset = window.pageYOffset;
 	        if (yOffset > offset) {
@@ -70,11 +68,12 @@ module.exports = React.createClass({
 	        }
 	    }, 100);
 	    $(window).scroll(_throttled);
+	},
 
-	    $(".back-to-top").click( e => {
-	        e.preventDefault();
-	        $("html, body").animate({scrollTop: 0}, duration);
-	        return false;
-	    });
+	_onClickToTop: function (e) {
+		var duration = 500; // 0.5s fade in/out
+		e.preventDefault();
+        $("html, body").animate({scrollTop: 0}, duration);
+        window.location.hash = "";
 	}
 });
