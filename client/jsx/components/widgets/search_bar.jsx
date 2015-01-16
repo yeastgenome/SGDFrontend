a64ab10d4bba903f7a8d22d0cc489d616f858218
Travis Sheppard <tshepp@stanford.edu>
2015-01-16 09:59:27 -0800

    can clear search bar

--- client/jsx/components/widgets/search_bar.jsx
+++ client/jsx/components/widgets/search_bar.jsx
@@ -16,12 +16,22 @@ module.exports = React.createClass({
 		};
 	},
 
+	getInitialState: function () {
+		return {
+			query: ""
+		};
+	},
+
 	render: function () {
+		var _hasExit = this.state.query !== "";
+		var exitNode = (this.state.query === "") ? null : <span onClick={this._clear} style={{ position: "absolute", top: 5, right: "1rem", fontSize: 18, cursor: "pointer" }}><i className="fa fa-times"></i></span>;
+
 		return (
 			<div className="row collapse">
 				<form onSubmit={this._onSubmit}>
-					<div className="small-10 columns" style={{ paddingLeft: 0, paddingRight: 0 }}>
-						<input type="text" ref="searchInput" placeholder={this.props.placeholderText} />
+					<div className="small-10 columns" style={{ paddingLeft: 0, paddingRight: 0, position: "relative" }}>
+						{exitNode}
+						<input onChange={this._onType} type="text" ref="searchInput" placeholder={this.props.placeholderText} value={this.state.query} />
 					</div>
 					<div className="small-2 columns" style={{ paddingLeft: 0 }}>
 						<input type="submit" href="#" className="button secondary postfix" value="Search" />
@@ -31,10 +41,21 @@ module.exports = React.createClass({
 		);
 	},
 
+	_onType: function (e) {
+		this.setState({ query: this.refs.searchInput.getDOMNode().value });
+	},
+
 	_onSubmit: function (e) {
-		e.preventDefault();
+		if (e) e.preventDefault();
 		if (this.props.onSubmit) {
-			this.props.onSubmit(this.refs.searchInput.getDOMNode().value);
+			this.props.onSubmit(this.state.query);
+		}
+	},
+
+	_clear: function (e) {
+		this.setState({ query: "" });
+		if (this.props.onSubmit) {
+			this.props.onSubmit("");
 		}
 	}
 });
