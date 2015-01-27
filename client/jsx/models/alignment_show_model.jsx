8680c4bac34edac0342570aae48bce1ca06cea00
Travis Sheppard <tshepp@stanford.edu>
2015-01-27 11:08:59 -0800

    start reducing redundant sequence segments in alignment

--- client/jsx/models/alignment_show_model.jsx
+++ client/jsx/models/alignment_show_model.jsx
@@ -41,6 +41,41 @@ module.exports = class AlignmentShowModel extends BaseModel {
 
 	formatSegments (isProtein) {
 		var variants = isProtein ? this.attributes.variant_data_protein : this.attributes.variant_data_dna;
+		// make sure they're sorted
+		variants = _.sortBy(variants, d => {
+			return d.start;
+		});
+
+		// handle redundant variant segments
+		var repeatSegments = [];
+		// add segment which is known to be redundant
+		var mergeSegments = segment => {
+			// TODO
+			console.log("merge: ", segment)
+		};
+		var isOverlap = (segA, segB) => {
+			var _isBefore = (segA.end >= segB.start && segA.start <= segB.end);
+			var _isAfter = (segA.start <= segB.end && segA.end >= segB.start);
+			return (_isBefore || _isAfter);
+		};
+
+		// loop through segments
+		var _outer, _inner;
+		for (var i = variants.length - 1; i >= 0; i--) {
+			_outer = variants[i];
+			// look at others to check for overlap
+			for (var _i = variants.length - 1; _i >= 0; _i--) {
+				_inner = variants[_i];
+				// merge if inside
+				if (i ==! _i && isOverlap(_inner, _outer)) {
+					console.log(_inner.start, _inner.end, _outer.start, _outer.end)
+					mergeSegments(_inner);
+					break;
+				}
+			}
+		}
+			
+
 		var segments = _.reduce(variants, (memo, next, i) => {
 			// handle start if visible segment not at beginning
 			if (next.start > 1 && i === 0) {
