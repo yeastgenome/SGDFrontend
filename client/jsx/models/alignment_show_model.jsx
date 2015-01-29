03fe84d25e06c5c0526961fa08a8689bea1255c1
Travis Sheppard <tshepp@stanford.edu>
2015-01-29 09:25:11 -0800

    sequence segment formatting tweaks

--- client/jsx/models/alignment_show_model.jsx
+++ client/jsx/models/alignment_show_model.jsx
@@ -60,20 +60,16 @@ module.exports = class AlignmentShowModel extends BaseModel {
 
 	// pure function that returns true or false if two segments overlap
 	_isOverlap (segA, segB) {
-		_isBefore = (segA.end >= segB.start && segA.start <= segB.end);
-		_isAfter = (segA.start <= segB.end && segA.end >= segB.start);
-		_isSame = (segA === segB);
+		var _isBefore = (segA.end >= segB.start && segA.start <= segB.end + 1);
+		var _isAfter = (segA.start <= segB.end + 1 && segA.end >= segB.start);
+		var _isSame = (segA === segB);
 		return ((_isBefore || _isAfter) && (!_isSame));
 	}
-
-	// returns true if segA inside of segB
-	// _isInside (segA, segB) {
-	// 	return (segA.start >= segB.start && segA.end <= segB.end);
-	// }
 
 	// from raw variant data, produce segments as expected by multi_alignment_viewer
 	formatSegments (isProtein) {
 		var variants = isProtein ? this.attributes.variant_data_protein : this.attributes.variant_data_dna;
+		var sequences = isProtein ? this.attributes.aligned_protein_sequences : this.attributes.aligned_dna_sequences;
 		// make sure they're sorted by start
 		variants = _.sortBy(variants, d => {
 			return d.start;
@@ -83,16 +79,18 @@ module.exports = class AlignmentShowModel extends BaseModel {
 		var mergedSegments = [];
 		variants.forEach( (d, i) => {
 			// if completely within another segment, do nothing
+			var isMerged = false;
 			mergedSegments.forEach( (_d, _i) => {
-
+				// if overlapping, extend existing
+				if (!isMerged && this._isOverlap(d, _d)) {
+					isMerged = true;
+					mergedSegments[_i].start = Math.min(d.start, _d.start);
+					mergedSegments[_i].end = Math.min(d.end, _d.end);
+				}
 			});
-			// if overlapping, extend existing
-			if (this._isOverlap(d, _d)) {
-				mergedSegments[_i].start = Math.min(d.start, _d.start);
-				mergedSegments[_i].end = Math.min(d.end, _d.end);
-			}
+			
 			// otherwise, create new segment with this data
-			mergedSegments.push(d);
+			if (!isMerged) mergedSegments.push(_.extend(d, { visible: true }));
 		});
 
 		// add in needed summarized segments
@@ -104,23 +102,29 @@ module.exports = class AlignmentShowModel extends BaseModel {
 				end: mergedSegments[0].start - 1
 			});
 		}
-		// loop through and connect
-		mergedSegments.forEach( (d, i) => {
+		// loop through and connect visible segments with summarized segments
+		var _visibleSegments = _.where(mergedSegments, { visible: true });
+		_visibleSegments.forEach( (d, i) => {
 			// must not be last or visible
-			if (d.visible && i < mergedSegments.length) {
+			if (d.visible && i < _visibleSegments.length - 1) {
 				mergedSegments.push({
 					visible: false,
 					start: d.end + 1,
-					end: mergedSegments[i + 1].start
+					end: _visibleSegments[i + 1].start - 1
 				});
 			}
 		});
-		// add last
-		mergedSegments.push({
-			start: mergedSegments[mergedSegments.length - 1].end,
-			end: this.attributes.end
-		});
-
+		// add last if last segment is visible and not at the end
+		var _last = _.max(mergedSegments, d => { return d.end; });
+		var _maxLength = _.max(sequences, d => { return d.sequence.length; }).sequence.length;
+		if (_last.end < _maxLength) {
+			mergedSegments.push({
+				start: _last.end + 1,
+				end: _maxLength,
+				visible: false
+			});
+		}
+		
 		// change starts and ends to domains
 		mergedSegments = _.map(mergedSegments, d => {
 			d.domain = [d.start, d.end];
@@ -132,72 +136,5 @@ module.exports = class AlignmentShowModel extends BaseModel {
 		});
 
 		return mergedSegments;
-
-		// var mergedSegments = _.reduce(variants, (memo, next, i) => {
-
-			// compare all original segments to this one
-			// variants.forEach( d => {
-			// 	// if completely within another segment, do nothing
-			// 	// if overlapping on one side, extend existing
-			// 	// otherwise, create new segment with this data
-
-			// });
-
-
-
-			// ### Everything below is old and wrong !!! ###
-
-			// add needed visible segment(s)
-			// handle start if visible segment not at beginning
-			// if (next.start > 1 && i === 0) {
-			// 	memo.push({
-			// 		domain: [1, next.start - 1],
-			// 		visible: false
-			// 	});
-			// // if not consecutive, put in a summarized segment
-			// } else if (i !== 0 && memo[i-1].domain[1] + 1 !== next.start) {
-			// 	memo.push({
-			// 		domain: [memo[i-1].domain[1] + 1, next.start - 1],
-			// 		visible: false
-			// 	});
-			// }
-			// or needed at end
-			// TODO
-
-			// put in the visible segment
-			// if already has an intersecting segment, just extend that one
-			// var _isMerged = false;
-			// var _other, _isBefore, _isAfter, _isSame;
-			// for (var _i = memo.length - 1; _i >= 0; _i--) {
-			// 	_other = memo[_i];
-			// 	// check for intersect
-			// 	_isBefore = (next.end >= _other.domain[0] && next.start <= _other.domain[1]);
-			// 	_isAfter = (next.start <= _other.domain[1] && next.end >= _other.domain[0]);
-			// 	_isSame = (i === _i);
-			// 	if ((_isBefore || _isAfter) && (!_isSame)) {
-			// 		_other.domain[0] = Math.min(_other.domain[0], next.start)
-			// 		_other.domain[1] = Math.max(_other.domain[1], next.end);
-			// 		_isMerged = true;
-			// 		break;
-			// 	}
-			// }
-			// // else, just add
-			// if (!_isMerged) {
-			// 	memo.push({
-			// 		domain: [next.start, next.end],
-			// 		visible: true
-			// 	});
-			// }
-			
-		// 	return memo;
-		// }, []);
-
-		// TODO
-		// add needed summarized segments
-		
-		// segments.forEach( d => {
-		// 	console.log(d.domain[0], d.domain[1])
-		// })
-		// return mergedSegments;
 	}
 };
