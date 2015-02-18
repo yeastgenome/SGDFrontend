5f651170a6db7b8b4919603d00503695b5564028
Travis Sheppard <tshepp@stanford.edu>
2015-02-18 12:38:47 -0800

    disable elastic search

--- client/jsx/lib/search.jsx
+++ client/jsx/lib/search.jsx
@@ -8,9 +8,9 @@
 var $ = require("jquery");
 require("jquery-ui");
 
-var SUGGEST_URL='/autocomplete_results'; 
+var SUGGEST_URL='/cgi-bin/search/searchSuggest.fpl'; 
 
-var SEARCH_URL='/search?query='; 
+var SEARCH_URL='/cgi-bin/search/instantSearch.fpl?query='; 
         
 var fields = ['gene_name',
 	      'headline',
