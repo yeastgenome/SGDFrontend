071ce0e96faa523f3dd739d372244c86276722b3
Travis Sheppard <tshepp@stanford.edu>
2015-05-01 11:15:35 -0700

    play with buildout

--- Gruntfile.js
+++ Gruntfile.js
@@ -228,4 +228,5 @@ module.exports = function(grunt) {
     grunt.registerTask("dev", ["compileDev", "watch"]);
     
     grunt.registerTask("default", ["static", "concurrent:production", "updateAssetVersion"]);
+    grunt.registerTask("deployAssets", ["static", "concurrent:production", "updateAssetVersion", "uploadToS3"]);
 };
