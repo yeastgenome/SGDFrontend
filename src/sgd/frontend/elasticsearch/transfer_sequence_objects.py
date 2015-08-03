b78ca832c9858cf8d67b032e4e98e469886bf5be
Travis Sheppard <tshepp@stanford.edu>
2015-08-03 13:16:29 -0700

    transfer seq objects from one instance to another

---
+++ src/sgd/frontend/elasticsearch/transfer_sequence_objects.py
@@ -0,0 +1,13 @@
+from elasticsearch import helpers
+from elasticsearch import Elasticsearch
+
+SRC_CLIENT_ADDRESS = 'http://localhost:9200'
+TARGET_CLIENT_ADDRESS = 'http://localhost:9200'
+SRC_INDEX = 'sequence_objects'
+TARGET_INDEX = 'sequence_objects2'
+
+src_client = Elasticsearch(SRC_CLIENT_ADDRESS)
+target_client = Elasticsearch(TARGET_CLIENT_ADDRESS)
+
+# transfer sequence objects between clusters or index based on statics
+helpers.reindex(client=src_client, source_index=SRC_INDEX, target_index=TARGET_INDEX, target_client=target_client)
