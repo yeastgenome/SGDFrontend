0387c3c651c5c87057780b7058037f87de3466cc
Travis Sheppard <tshepp@stanford.edu>
2015-09-29 10:25:12 -0700

    start being able to re call vars

--- src/sgd/frontend/elasticsearch/reindex_sequence_objects.py
+++ src/sgd/frontend/elasticsearch/reindex_sequence_objects.py
@@ -1,10 +1,9 @@
 from elasticsearch import helpers
 from elasticsearch import Elasticsearch
-from collections import OrderedDict
 
 CLIENT_ADDRESS = 'http://localhost:9200'
 SRC_INDEX = 'sequence_objects'
-INDEX = 'sequence_objects5'
+INDEX = 'sequence_objects'
 DOC_TYPE = 'sequence_object'
 RESET_INDEX = False
 # TEMP, trigger runscope test, should be ENV var with default to False
@@ -13,26 +12,22 @@ RUNSCOPE_TRIGGER_URL = ''
 
 es = Elasticsearch(CLIENT_ADDRESS)
 
-CONTIG_LENGTHS = OrderedDict([
-	('I', 230218),
-	('II', 813184),
-	('III', 316620),
-	('IV',  1531933),
-	('V', 576874),
-	('VI', 270161),
-	('VII', 1090940),
-	('VIII', 562643),
-	('IX', 439888),
-	('X', 745751),
-	('XI', 666816),
-	('XII', 1078177),
-	('XIII', 924431),
-	('XIV', 784333),
-	('XV', 1091291),
-	('XVI', 94806),
-	('Mito', 85779),
-	('2-micron', 6318)
-])
+codon_table = {'TTT': 'F', 'TTC': 'F', 'TTA': 'L', 'TTG': 'L',
+               'CTT': 'L', 'CTC': 'L', 'CTA': 'L', 'CTG': 'L',
+               'ATT': 'I', 'ATC': 'I', 'ATA': 'I', 'ATG': 'M',
+               'GTT': 'V', 'GTC': 'V', 'GTA': 'V', 'GTG': 'V',
+               'TCT': 'S', 'TCC': 'S', 'TCA': 'S', 'TCG': 'S',
+               'CCT': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+               'ACT': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+               'GCT': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+               'TAT': 'Y', 'TAC': 'Y', 'TAA': '*', 'TAG': '*',
+               'CAT': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+               'AAT': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+               'GAT': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+               'TGT': 'C', 'TGC': 'C', 'TGA': '*', 'TGG': 'W',
+               'CGT': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+               'AGT': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
+               'GGT': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'}
 
 def setup_index():
 	exists = es.indices.exists(INDEX)
@@ -59,23 +54,35 @@ def put_mapping():
 	es.indices.put_mapping(index=INDEX, body=full_settings, doc_type=DOC_TYPE)
 
 def index_locus(old_data):
-	# # add absolute_genetic_start
-	# contig_numeral = old_data['contig_name'].split('_')[1]
-	# contig_index = CONTIG_LENGTHS.keys().index(contig_numeral)
-	# _absolute_genetic_start = 0
-	# for contig in CONTIG_LENGTHS.keys():
+	# calc variant data
+	# get introns in { start, end } format	
+	block_starts = old_data['block_starts']
+	block_sizes = old_data['block_sizes']
 
-	# 	if contig == contig_numeral:
-	# 		break
-	# 	_absolute_genetic_start += CONTIG_LENGTHS[contig]
-	# _absolute_genetic_start += old_data['chrom_start']
+	if len(block_starts) <= 1:
+		introns = []
+	else:
+		introns = []
+		i = 0
+		for b_size in block_sizes:
+			if i == len(block_sizes) - 1:
+				continue
+			b_start = block_starts[i]
+			next_b_start = block_starts[i + 1]
+			_start = b_start + b_size
+			_end = next_b_start
+			obj = { 'start': _start, 'end': _end }
+			introns.append(obj)
+			start = b_start + b_size
+			i += 1
 
-	_contig_name = old_data['contig_name'].replace('_', ' ')
+	_variant_data_dna = calculate_variant_data('DNA', old_data['aligned_dna_sequences'], introns)
+	_variant_data_protein = calculate_variant_data('Protein', old_data['aligned_protein_sequences'], [])
 
-	# assign new data
-	old_data['contig_name'] = _contig_name
+
+	old_data['variant_data_dna'] = _variant_data_dna
+	old_data['variant_data_protein'] = _variant_data_protein
 	es.index(index=INDEX, doc_type=DOC_TYPE, id=old_data['sgdid'], body=old_data)
-	return
 
 # fetch all the loci, index in index_locus
 def index_loci():
@@ -86,10 +93,130 @@ def index_loci():
 		data = hit['_source']
 		index_locus(data)
 
+def translate(codon):
+    if codon in codon_table:
+        return codon_table[codon]
+    else:
+        return None
+
+
+def check_snp_type(index, intron_indices, ref_seq, aligned_seq):
+    #Check if SNP is in intron
+    pre_introns = []
+    post_introns = []
+    aligned_seq_coding = ''
+    ref_seq_coding = ''
+    seq_index = 0
+    for start, end in intron_indices:
+        aligned_seq_coding += aligned_seq[seq_index:start]
+        ref_seq_coding += ref_seq[seq_index:start]
+        seq_index = end+1
+        if index < start:
+            post_introns.append((start, end))
+        elif index > end:
+            pre_introns.append((start, end))
+        else:
+            return 'Intron SNP'
+    aligned_seq_coding += aligned_seq[seq_index:len(aligned_seq)]
+    ref_seq_coding += ref_seq[seq_index:len(ref_seq)]
+    index_coding = index - sum([end-start+1 for start, end in pre_introns])
+
+    #Introns have been removed, now deal with insertions/deletions and find frame
+    index_to_frame = {}
+    codon = []
+    for i, letter in enumerate(ref_seq_coding):
+        if letter == '-':
+            pass
+        else:
+            codon.append(i)
+        if len(codon) == 3:
+            for index in codon:
+                index_to_frame[index] = codon
+            codon = []
+
+    codon = index_to_frame[index_coding]
+    ref_amino_acid = translate(''.join([ref_seq_coding[i] for i in codon]))
+    aligned_amino_acid = translate(''.join([aligned_seq_coding[i] for i in codon]))
+
+    if ref_amino_acid is None or aligned_amino_acid is None:
+        return 'Untranslatable SNP'
+    elif ref_amino_acid == aligned_amino_acid:
+        return 'Synonymous SNP'
+    else:
+        return 'Nonsynonymous SNP'
+
+
+def calculate_variant_data(type, aligned_sequences, introns):
+    intron_indices = [(int(x['start']), int(x['end'])) for x in introns]
+    variants = dict()
+    reference_alignment = [x['sequence'] for x in aligned_sequences if x['strain_id'] == 1]
+    if len(reference_alignment) == 1:
+        reference_alignment = reference_alignment[0]
+
+        for strain in aligned_sequences:
+            aligned_sequence = strain['sequence']
+
+
+            #print aligned_sequence[0:int(introns[0]['start'])] + aligned_sequence[int(introns[0]['end'])+1:len(aligned_sequence)]
+
+            state = 'No difference'
+            state_start_index = 0
+            for i, letter in enumerate(reference_alignment):
+                #Figure out new state
+                new_state = 'No difference'
+                if aligned_sequence[i] != letter:
+                    if letter == '-':
+                        new_state = 'Insertion'
+                    elif aligned_sequence[i] == '-':
+                        new_state = 'Deletion'
+                    else:
+                        if type == 'DNA':
+                            new_state = check_snp_type(i, intron_indices, reference_alignment, aligned_sequence)
+                        else:
+                            new_state = 'SNP'
+
+                if state != new_state:
+                    if state != 'No difference':
+                        variant_key = (state_start_index+1, i+1, state)
+                        if variant_key not in variants:
+                            variants[variant_key] = 0
+                        variants[variant_key] += 1
+
+                    state = new_state
+                    state_start_index = i
+
+            if state != 'No difference':
+                variant_key = (state_start_index+1, i+1, state)
+                if variant_key not in variants:
+                    variants[variant_key] = 0
+                variants[variant_key] += 1
+
+    variant_data = []
+    for variant, score in variants.iteritems():
+        obj_json = {
+            'start': variant[0],
+            'end': variant[1],
+            'score': score,
+            'variant_type': 'SNP' if variant[2].endswith('SNP') else variant[2]
+        }
+        if variant[2].endswith('SNP'):
+            obj_json['snp_type'] = variant[2][0:-4]
+        #obj_json['sequence'] = []
+        #for strain in aligned_sequences:
+        #    obj_json['sequence'].append(strain['sequence'][variant[0]-1:variant[1]-1])
+
+        variant_data.append(obj_json)
+    return variant_data
+
+def index_test_locus():
+	example_id = 'S000001855'
+	res = es.get(index='sequence_objects', id=example_id)['_source']
+	index_locus(res)
+
 def main():
 	setup_index()
-	index_loci()
-	# index_test_locus()
+	# index_loci()
+	index_test_locus()
 
 	if TEST:
 		requests.get(RUNSCOPE_TRIGGER_URL)
