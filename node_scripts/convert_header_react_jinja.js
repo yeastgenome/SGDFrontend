d0e034b020b900733dd7542bcf6f6418a8d763a8
Travis <travissheppard.1988@gmail.com>
2015-11-10 16:02:01 -0800

    start working on jinja writers

---
+++ node_scripts/convert_header_react_jinja.js
@@ -0,0 +1,7 @@
+// run this script with node to write a static version of the react header to jinja2
+require('babel-core/register');
+const React = require('react');
+const Header = require('../client/jsx/containers/layout/header.jsx');
+
+console.log("foo")
+
