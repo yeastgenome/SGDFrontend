886705abc7042f86955f4c95fa859184767fe37b
Pedro Assis <pedroh2306@gmail.com>
2015-11-24 15:08:57 -0800

    Including gevent for prod env

--- src/app.py
+++ src/app.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask_restful import Api
 from flask.ext.sqlalchemy import SQLAlchemy
 
+from gevent.wsgi import WSGIServer
+
+import os
+
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+app.config.from_pyfile('../config/config.py')
 
 api = Api(app)
 db = SQLAlchemy(app)
@@ -13,4 +17,9 @@ from resources.locus import Locus
 api.add_resource(Locus, '/locus', '/locus/<string:id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	if os.environ.get('ENV', False) == 'prod':
+		app.debug = False
+		http_server = WSGIServer(('', 5000), app)
+		http_server.serve_forever()
+	else:
+		app.run(debug=True)
