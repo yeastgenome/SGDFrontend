528d270f94b549a6264bd91fec49112c46a54db0
Pedro Assis <pedroh@stanford.edu>
2015-11-25 16:32:52 -0800

    Returning locus information by id

--- src/resources/locus.py
+++ src/resources/locus.py
@@ -1,6 +1,10 @@
-from common.db_conn import db
-from flask_restful import Resource
+from flask_restful import Resource, abort
+from models.locus import Locus as Locus_
 
 class Locus(Resource):
     def get(self, id):
-        return id
+        locus = Locus_.query.filter_by(systematic_name=id).first()
+        if locus:
+            return locus.to_dict()
+        else:
+            abort(404, message="Locus {} doesn't exist.".format(id))
