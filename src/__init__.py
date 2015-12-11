e976cc3ea2cfb570bd44c2f3121947484e0ef303
Pedro Assis <pedroh@stanford.edu>
2015-12-11 15:46:50 -0800

    Moving the server routes to init

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,13 @@
-import os
-os.environ['CELERY_CONFIG_MODULE'] = 'mypackage.celeryconfig'
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+
+def main(global_config, **settings):
+        config = Configurator(settings=settings)
+        
+        config.add_route('home', '/')
+        config.add_route('upload', '/upload')
+        
+        config.scan()
+        config.add_static_view(name='static', path='../static')
+
+        return config.make_wsgi_app()
