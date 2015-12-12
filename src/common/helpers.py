e9dca17b73c8c4d28c646e35a1db4e3f31ebc46e
Pedro Assis <pedroh@stanford.edu>
2015-12-11 16:01:36 -0800

    Moving general methods to a helper

--- src/common/helpers.py
+++ src/common/helpers.py
@@ -3,7 +3,7 @@ import hashlib
 def md5(fname):
     hash = hashlib.md5()
     with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
+        for chunk in iter(lambda: f.read(4096), ""):
             hash.update(chunk)
     return hash.hexdigest()
 
