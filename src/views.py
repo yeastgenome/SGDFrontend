1b1677d489b182753ebc05a402f36e3fe62c97fb
Pedro Assis <pedroh@stanford.edu>
2015-12-14 14:41:47 -0800

    Refactoring

--- src/views.py
+++ src/views.py
@@ -10,27 +10,29 @@ import shutil
 from celery_tasks import upload_to_s3
 from common.helpers import allowed_file
 
+
 @view_config(route_name='home')
 def home_view(request):
-	return FileResponse('static/index.html', request=request)
+    return FileResponse('static/index.html', request=request)
 
 @view_config(route_name='upload', request_method='POST')
 def upload_file(request):
-	root_dir = os.path.dirname(os.getcwd())
+    root_dir = os.path.dirname(os.getcwd())
 	
-	file = request.POST['file'].file
-	filename = request.POST['file'].filename
+    file = request.POST['file'].file
+    filename = request.POST['file'].filename
 	
-	if file and allowed_file(filename):
-		filename = secure_filename(filename)
-		temp_file_path = os.path.join('/tmp', filename)
+    if file and allowed_file(filename):
+        filename = secure_filename(filename)
+	temp_file_path = os.path.join('/tmp', filename)
 
-		file.seek(0)
-		with open(temp_file_path, 'wb') as output_file:
-			shutil.copyfileobj(file, output_file)
+	file.seek(0)
+	with open(temp_file_path, 'wb') as output_file:
+	    shutil.copyfileobj(file, output_file)
 
-		upload_to_s3.delay(filename, temp_file_path)
-			
-		return Response('Upload complete.')
-	else:
-		return Response('Extension not suported.')
+        settings = request.registry.settings
+	upload_to_s3.delay(filename, temp_file_path, settings['s3_access_key'], settings['s3_secret_key'], settings['s3_bucket'])
+
+	return Response('Upload complete')
+    else:
+	return Response('Extension not suported')
