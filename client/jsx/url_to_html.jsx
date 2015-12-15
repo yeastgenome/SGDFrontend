93b99926d917dc8d7106ac820cc15a49ec4a1c0f
Travis Sheppard <tshepp@stanford.edu>
2015-12-15 13:33:07 -0800

    more node cleanup

--- client/jsx/url_to_html.jsx
+++ client/jsx/url_to_html.jsx
@@ -18,7 +18,7 @@ const TOP_HTML = '<html><head><link href="/static/css/normalize.css" rel="styles
 const BOTTOM_HTML = '</div><script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script><script src="/static/js/application.js"></script><script>views.bundle.render();</script></body></html>';
 
 // takes relative url, call cb with html response
-// cb(err, html)
+// cb(err, html, statusCode)
 module.exports = function(relativeUrl, cb) {
   // create server store
   const reducer = combineReducers({
@@ -32,11 +32,19 @@ module.exports = function(relativeUrl, cb) {
     //   console.log(store.getState())
     // });
 
-    renderProps.store = store;
-    var innerEl = React.createElement(RoutingContext, renderProps);
-    var el = React.createElement(Provider, renderProps, innerEl);
-    var appHtml = ReactDOMServer.renderToStaticMarkup(el);
-    var fullHtml = TOP_HTML + appHtml + BOTTOM_HTML;
-    return cb(err, fullHtml);
+    if (err) {
+      return cb(err, 'ERROR', 500);
+    } else if (redirectLocation) {
+      return cb(err, '', 302);
+    } else if (renderProps) {
+      renderProps.store = store;
+      var innerEl = React.createElement(RoutingContext, renderProps);
+      var el = React.createElement(Provider, renderProps, innerEl);
+      var appHtml = ReactDOMServer.renderToStaticMarkup(el);
+      var fullHtml = TOP_HTML + appHtml + BOTTOM_HTML;
+      return cb(err, fullHtml, 200);
+    } else {
+      return cb(err, '404 - NOT FOUND', 404)
+    }
   });
 };
