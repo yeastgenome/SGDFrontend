5ecb7bdc47652b14def85507eb4b454c6b0f34dc
Pedro Assis <pedroh@stanford.edu>
2015-12-17 11:17:33 -0800

    Initializing sqlalchmey conn

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,15 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
+
+from .models import DBSession, Base
+
 
 def main(global_config, **settings):
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+
     config = Configurator(settings=settings)
 
     config.add_route('home', '/')
