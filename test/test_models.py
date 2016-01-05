997de2795c486e40774c7e9495e32a0a90e7a092
Pedro Assis <pedroh@stanford.edu>
2016-01-04 16:15:48 -0800

    Removing previous bindings from sessions

--- test/test_models.py
+++ test/test_models.py
@@ -1,12 +1,13 @@
 import unittest
 from sqlalchemy import create_engine, Column, String
-from src.models import DBSession, Base, Source
-from fixtures import SourceFactory
+from src.models import DBSession, Base, Source, Colleague
+import fixtures as factory
 
 
 class ModelsTest(unittest.TestCase):
     def setUp(self):
         self.engine = create_engine('sqlite://')
+        DBSession.remove()
         DBSession.configure(bind=self.engine)
 
         Base.metadata.create_all(self.engine)
@@ -14,14 +15,24 @@ class ModelsTest(unittest.TestCase):
     def tearDown(self):
         DBSession.remove()
 
-    def test_source_fields(self):
+    def test_source_model(self):
         instances = DBSession.query(Source).all()
         self.assertEqual(0, len(instances))
-        
-        source = SourceFactory()
+
+        source = factory.SourceFactory()
         instances = DBSession.query(Source).all()
 
         self.assertEqual(1, len(instances))
         self.assertEqual(source, instances[0])
 
-    
+    def test_colleague_model(self):
+        instances = DBSession.query(Colleague).all()
+        self.assertEqual(0, len(instances))
+
+        source = factory.SourceFactory()
+        colleague = factory.ColleagueFactory()
+        instances = DBSession.query(Colleague).all()
+
+        self.assertEqual(1, len(instances))
+        self.assertEqual(colleague, instances[0])
+        self.assertEqual(colleague.source, source)
