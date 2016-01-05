a18fa7b4c6c6312c6f4d81199e38c83aee30db9a
Pedro Assis <pedroh@stanford.edu>
2016-01-04 16:14:59 -0800

    Querying colleagues

--- src/views.py
+++ src/views.py
@@ -2,13 +2,16 @@ from pyramid.httpexceptions import HTTPFound, HTTPBadRequest
 from pyramid.response import Response, FileResponse
 from pyramid.response import FileResponse
 from pyramid.view import view_config
+from pyramid.compat import escape
 from celery_tasks import upload_to_s3
 from common.helpers import allowed_file, secure_save_file
+
+from .models import DBSession, Colleague
 
 import os
 
 
-@view_config(route_name='home')
+@view_config(route_name='home', request_method='GET')
 def home_view(request):
     return FileResponse('static/index.html', request=request)
 
@@ -25,7 +28,16 @@ def upload_file(request):
     if file and allowed_file(filename):
         temp_file_path = secure_save_file(file, filename)
         settings = request.registry.settings
-	upload_to_s3.delay(filename, temp_file_path, settings['s3_access_key'], settings['s3_secret_key'], settings['s3_bucket'])
+	upload_to_s3.delay(filename, temp_file_path, os.environ['S3_ACCESS_KEY'], os.environ['S3_SECRET_KEY'], os.environ['S3_BUCKET'])
 	return Response('Upload complete')
     else:
         return HTTPBadRequest('File extension is invalid')
+
+@view_config(route_name='colleagues', renderer='json', request_method='GET')
+def colleagues_by_last_name(request):
+    if request.params.get('last_name') is None:
+        return HTTPBadRequest('last_name argument is missing')
+
+    last_name = escape(request.params['last_name'])
+    return []
+#    return DBSession.query(Colleague).filter(Colleague.last_name == last_name)
