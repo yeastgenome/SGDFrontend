81513f92a7a9e34cbbdd03eb0b80a384c29322dc
Pedro Assis <pedroh2306@gmail.com>
2016-01-05 16:12:05 -0800

    Token decrypt using Google OpenID

--- src/views.py
+++ src/views.py
@@ -3,12 +3,15 @@ from pyramid.response import Response, FileResponse
 from pyramid.response import FileResponse
 from pyramid.view import view_config
 from pyramid.compat import escape
-from celery_tasks import upload_to_s3
-from common.helpers import allowed_file, secure_save_file
+from oauth2client import client, crypt
+import os
 
 from .models import DBSession, Colleague
+from .common.helpers import allowed_file, secure_save_file
+from .celery_tasks import upload_to_s3
 
-import os
+import logging
+log = logging.getLogger(__name__)
 
 
 @view_config(route_name='home', request_method='GET')
@@ -41,3 +44,22 @@ def colleagues_by_last_name(request):
     last_name = escape(request.params['last_name'])
     return []
 #    return DBSession.query(Colleague).filter(Colleague.last_name == last_name)
+
+@view_config(route_name='authorize', renderer='json', request_method='POST')
+def authorize(request):
+	try:
+		idinfo = client.verify_id_token(request.POST.get('token'), os.environ['GOOGLE_CLIENT_ID'])
+
+		if idinfo.get('iss') not in ['accounts.google.com', 'https://accounts.google.com']:
+			raise crypt.AppIdentityError("Wrong issuer.")
+
+		log.info("User " + idinfo['email'] + " trying to authenticate from " + request.remote_addr)
+
+#		if not is_a_curator(idinfo['email']):
+#			raise crypt.AppIdentityError("User not authorized.")
+
+		log.info("User " + idinfo['email'] + " was successfuly authenticated.")
+		return {'status': 200}
+	except crypt.AppIdentityError:
+		userid = idinfo['sub']
+		return {'status': 433}
