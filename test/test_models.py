74494bc6446502bb4a00099ce0b7167e51cf938c
Pedro Assis <pedroh@stanford.edu>
2016-01-20 15:06:25 -0800

    Tests for model ColleagueUrl

--- test/test_models.py
+++ test/test_models.py
@@ -1,6 +1,6 @@
 import unittest
 from sqlalchemy import create_engine, Column, String
-from src.models import DBSession, Base, Source, Colleague, Dbuser
+from src.models import DBSession, Base, Source, Colleague, ColleagueUrl, Dbuser
 import fixtures as factory
 
 
@@ -37,6 +37,22 @@ class ModelsTest(unittest.TestCase):
         self.assertEqual(colleague, instances[0])
         self.assertEqual(colleague.source, source)
 
+    def test_colleague_model_search_result_dict(self):
+        source = factory.SourceFactory()
+        colleague = factory.ColleagueFactory()
+        instances = DBSession.query(Colleague).all()
+        self.assertEqual(1, len(instances))
+        self.assertEqual(colleague, instances[0])
+        self.assertEqual(colleague.to_search_result_dict(), {
+            'first_name': 'Jimmy',
+            'last_name': 'Page',
+            'organization': 'Stanford Universty',
+            'work_phone': '444-444-4444',
+            'fax': '333-333-3333',
+            'lab_url': '',
+            'research_summary_url': ''
+        })
+
     def test_dbuser_model(self):
         instances = DBSession.query(Dbuser).all()
         self.assertEqual(0, len(instances))
@@ -46,3 +62,17 @@ class ModelsTest(unittest.TestCase):
 
         self.assertEqual(1, len(instances))
         self.assertEqual(dbuser, instances[0])
+
+    def test_colleagueurl_model(self):
+        instances = DBSession.query(ColleagueUrl).all()
+        self.assertEqual(0, len(instances))
+
+        source = factory.SourceFactory()
+        colleague = factory.ColleagueFactory()
+        colleague_url = factory.ColleagueUrlFactory()
+        instances = DBSession.query(ColleagueUrl).all()
+
+        self.assertEqual(1, len(instances))
+        self.assertEqual(colleague_url, instances[0])
+        self.assertEqual(colleague_url.source, source)
+        self.assertEqual(colleague_url.colleague, colleague)
