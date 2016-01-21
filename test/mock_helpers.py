c2797884a2cc5b6047059295428f2aa57340a75b
Pedro Assis <pedroh@stanford.edu>
2016-01-20 16:12:30 -0800

    Improving SQLAlchemy mock model

--- test/mock_helpers.py
+++ test/mock_helpers.py
@@ -1,20 +1,25 @@
 class MockQueryFilter(object):
-    def __init__(self, query_result):
+    def __init__(self, query_params, query_result):
         self._return = query_result
+        self._params = query_params
 
     def one_or_none(self):
         return self._return
 
     def first(self):
         return self._return
+
+    def query_params(self):
+        return self._params
     
 
 class MockQuery(object):
     def __init__(self, query_result):
         self._query_result = query_result
 
-    def filter(self, query_result):
-        return MockQueryFilter(self._query_result)
+    def filter(self, query_params):
+        self._query_filter = MockQueryFilter(query_params, self._query_result)
+        return self._query_filter
 
 
 class MockFileStorage(object):
