b6d31df095a4c849796c1404630c1b9a1ff77de7
Travis Sheppard <tshepp@stanford.edu>
2016-02-04 16:09:16 -0800

    start metadata select

--- src/client/js/containers/files/new_file.js
+++ src/client/js/containers/files/new_file.js
@@ -4,25 +4,46 @@ import Dropzone from 'react-dropzone';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
+import TForm from '../../components/widgets/t_form'
+
 const FilesIndex = React.createClass({
   render() {
     return (
       <div>
         <h1>Dataset Upload</h1>
-        <div className='panel'>
-          <p>File</p>
-          <div style={style.dropZoneContainer}>
-            <Dropzone onDrop={this._onDrop}>
-              <p style={style.dropMessage}>Drop file here or click to select.</p>
-            </Dropzone>
+        <div className='row'>
+          <div className='columns small-6'>
+            {this._renderForm()}
+            <div className='button-group'>
+              <Link to='/dashboard/files' className='button secondary' style={style.formButton}>Cancel</Link>
+              <a className='button disabled'><i className='fa fa-upload'/> Upload</a>
+            </div>
           </div>
-          <div className='button-group'>
-           <Link to='/dashboard/files' className='button secondary' style={style.formButton}>Cancel</Link>
-           <a className='button disabled'><i className='fa fa-upload'/> Upload</a>
+          <div className='columns small-6'>
+            <p>File</p>
+            <div style={style.dropZoneContainer}>
+              <Dropzone onDrop={this._onDrop}>
+                <p style={style.dropMessage}>Drop file here or click to select.</p>
+              </Dropzone>
+            </div>
           </div>
         </div>
       </div>
     );
+  },
+
+  _renderForm () {
+      const datasetSchema = {
+        title: 'Dataset',
+        type: 'object',
+        properties: {
+          filename: { type: 'string' },
+          date: { type: 'string' },
+          file_format: { type: 'string' },
+        },
+        required: ['filename']
+      };
+      return <TForm validationObject={datasetSchema} />;
   },
 
   _onDrop (files) {
