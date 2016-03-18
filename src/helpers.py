9cce28385720e5a22c07e7c7207d31ded8d996ab
Pedro Assis <pedroh@stanford.edu>
2016-03-18 16:47:53 -0700

    Complete upload method with metadata handling

--- src/helpers.py
+++ src/helpers.py
@@ -3,8 +3,12 @@ import werkzeug
 import os
 import shutil
 import tempfile
-from pyramid.httpexceptions import HTTPForbidden
-from .models import DBSession, Dbuser
+from pyramid.httpexceptions import HTTPForbidden, HTTPBadRequest
+from .models import DBSession, Dbuser, Referencedbentity, Keyword, Filepath, Edam, Filedbentity, FileKeyword, ReferenceFile
+
+
+import logging
+log = logging.getLogger(__name__)
 
 def md5(fname):
     hash = hashlib.md5()
@@ -36,3 +40,86 @@ def authenticate(view_callable):
         else:
             return view_callable(request)
     return inner
+
+def extract_references(request):
+    references = []
+    if request.POST.get("pmids") != '':
+        pmids = str(request.POST.get("pmids")).split("|")
+        for pmid in pmids:
+            try:
+                f_pmid = float(pmid)
+            except ValueError:
+                log.info('Upload error: PMIDs must be integer numbers. Sent: ' + pmid)
+                raise HTTPBadRequest('PMIDs must be integer numbers. You sent: ' + pmid)
+            
+            reference = DBSession.query(Referencedbentity).filter(Referencedbentity.pmid == f_pmid).one_or_none()
+            if reference is None:
+                log.info('Upload error: inexistent PMID(s): ' + pmid)
+                raise HTTPBadRequest('Inexistent PMID(s): ' + pmid)
+            else:
+                references.append(reference.dbentity_id)
+    return references
+
+def extract_keywords(request):
+    keywords = []
+    if request.POST.get("keywords") != '':
+        keyword_names = str(request.POST.get("keywords")).split("|")
+        for keyword in keyword_names:
+            keyword_obj = DBSession.query(Keyword).filter(Keyword.display_name == keyword).one_or_none()
+            if keyword_obj is None:
+                log.info('Upload error: invalid or inexistent Keyword: ' + keyword)
+                raise HTTPBadRequest('Invalid or inexistent Keyword: ' + keyword)
+            else:
+                keywords.append(keyword_obj.keyword_id)
+    return keywords
+
+def get_or_create_filepath(request):
+    filepath = DBSession.query(Filepath).filter(Filepath.filepath == request.POST.get("new_filepath")).one_or_none()
+    import pdb; pdb.set_trace()
+    if filepath is None:
+        filepath = Filepath(filepath=request.POST.get("new_filepath"), source_id=339)
+        DBSession.add(filepath)
+        DBSession.flush()
+        DBSession.refresh(filepath)
+    return filepath
+
+def extract_topic(request):
+    topic = DBSession.query(Edam).filter(Edam.edamid == request.POST.get("topic_id")).one_or_none()
+    if topic is None:
+        log.info('Upload error: Topic ID ' + request.POST.get("topic_id") + ' is not registered or is invalid.')
+        raise HTTPBadRequest('Invalid or inexistent Topic ID: ' + request.POST.get("topic_id"))
+    return topic
+
+def extract_format(request):
+    format = DBSession.query(Edam).filter(Edam.edamid == request.POST.get("format_id")).one_or_none()
+    if format is None:
+        log.info('Upload error: Format ID ' + request.POST.get("format_id") + ' is not registered or is invalid.')
+        raise HTTPBadRequest('Invalid or inexistent Format ID: ' + request.POST.get("format_id"))
+    return format
+
+def file_already_uploaded(request):
+    fdb = DBSession.query(Filedbentity).filter(Filedbentity.format_name == request.POST.get("display_name")).one_or_none()
+    if fdb is not None:
+        log.info('Upload error: File ' + request.POST.get("display_name") + ' already exists.')
+        return True
+    return False
+
+def link_references_to_file(references, fdb_dbentity_id):
+    for reference_id in references:
+        rf = ReferenceFile(
+            reference_id=reference_id,
+            file_id=fdb_dbentity_id,
+            source_id=339
+        )
+        DBSession.add(rf)
+    DBSession.commit()
+
+def link_keywords_to_file(keywords, fdb_db_entity_id):
+    for keyword_id in keywords:
+        fk = FileKeyword(
+            keyword_id=keyword_id,
+            file_id=fdb_dbentity_id,
+            source_id=339
+        )
+        DBSession.add(fk)
+    DBSession.commit()
