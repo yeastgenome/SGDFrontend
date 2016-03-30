382332ccb16b2fd35d49cfc6f1c1d58a159f574c
Travis Sheppard <tshepp@stanford.edu>
2016-03-30 15:34:49 -0700

    black header version

--- client/jsx/containers/layout/header.jsx
+++ client/jsx/containers/layout/header.jsx
@@ -13,20 +13,20 @@ const Header = React.createClass({
             <div className="header-left">
               <a href="/" className="logo-link">
                 <div className="sgd-logo"></div>
-                <span><i>Saccharomyces</i> Genome Database</span>
+                <span className="sgd-logo-text"><i>Saccharomyces</i> Genome Database</span>
               </a>
               <div className="site-links">
-                <a href="/suggestion" target="_blank" id="email-header" className="webicon mail small">Email Us</a>
-                <a href="http://twitter.com/#!/yeastgenome" target="_blank" id="twitter" className="webicon twitter small">Twitter</a>
-                <a href="https://www.facebook.com/pages/Saccharomyces-Genome-Database-SGD/139140876128200" target="_blank" className="webicon facebook small" id="facebook">Facebook</a>
-                <a href="https://www.linkedin.com/company/saccharomyces-genome-database" target="_blank" className="webicon linkedin small" id="linkedin">Linkedin</a>
-                <a href="https://www.youtube.com/SaccharomycesGenomeDatabase" target="_blank" id="youtube" className="webicon youtube small">YouTube</a>
+                <a href="/suggestion" target="_blank" id="email-header" className="webicon mail medium">Email Us</a>
+                <a href="http://twitter.com/#!/yeastgenome" target="_blank" id="twitter" className="webicon twitter medium">Twitter</a>
+                <a href="https://www.facebook.com/pages/Saccharomyces-Genome-Database-SGD/139140876128200" target="_blank" className="webicon facebook medium" id="facebook">Facebook</a>
+                <a href="https://www.linkedin.com/company/saccharomyces-genome-database" target="_blank" className="webicon linkedin medium" id="linkedin">Linkedin</a>
+                <a href="https://www.youtube.com/SaccharomycesGenomeDatabase" target="_blank" id="youtube" className="webicon youtube medium">YouTube</a>
               </div>
             </div>
           </div>
           {/* right */}
           <div className="columns medium-4 header-right">
-            <div className="site-links">
+            <div className="site-links site-social-links">
               <div><a href="/about"><i className="fa fa-info-circle"/> About</a></div>
               <div><a href="/blog"><i className="fa fa-exclamation-circle"/> Blog</a></div>
               <div><a href="/download-data"><i className="fa fa-download"/> Download</a></div>
