db7ea524baafc6874874c49eef5e569f68e03c44
Travis Sheppard <tshepp@stanford.edu>
2016-05-02 16:36:35 -0700

    rm readme ref

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
