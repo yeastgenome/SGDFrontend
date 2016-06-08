233c911c8b9a501425ba615fead994bf01fb4ab9
Travis Sheppard <tshepp@stanford.edu>
2016-06-08 09:31:43 -0700

    dash -> curate, start colleague

--- src/client/js/routes.js
+++ src/client/js/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 // import handler containers
 import { requireAuthentication } from './containers/authenticate_component';
 import Layout from './containers/layout';
+import ColleaguesIndex from './containers/colleagues/colleagues_index';
 import Dashboard from './containers/dashboard';
 import DashboardIndex from './containers/dashboard_index';
 import ExampleContainer from './containers/example_container';
@@ -15,11 +16,14 @@ export default (
   <Route path='/' component={Layout}>
   	<IndexRoute component={requireAuthentication(DashboardIndex)} />
     <Route path='login' component={Login} />
-    <Route path='dashboard' component={requireAuthentication(Dashboard)}>
+    <Route path='curate' component={requireAuthentication(Dashboard)}>
       <IndexRoute component={DashboardIndex} />
       <Route path='files'>
         <IndexRoute component={NewFile} />
         <Route path='new' component={NewFile} />
+      </Route>
+      <Route path='colleagues'>
+        <IndexRoute component={ColleaguesIndex}/>
       </Route>
     </Route>
     <Route path='*' component={NotFound} />
