4bc016468f78ecd0cf1d0175e6ce785d7db18e01
Travis Sheppard <tshepp@stanford.edu>
2016-06-08 10:11:50 -0700

    cleanup syntax and eslint

--- src/client/js/containers/colleagues/colleagues_edit.js
+++ src/client/js/containers/colleagues/colleagues_edit.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const ExampleContainer = React.createClass({
+const ColleaguesEdit = React.createClass({
   render() {
-    return <h1>Example</h1>;
+    return <h1>ColleaguesEdit</h1>;
   }
 });
 
@@ -12,4 +12,4 @@ function mapStateToProps(_state) {
   };
 }
 
-export default connect(mapStateToProps)(ExampleContainer);
+export default connect(mapStateToProps)(ColleaguesEdit);
