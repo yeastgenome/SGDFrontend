a9563e12df8633cdb53d6ee31639e8613909c220
Travis Sheppard <tshepp@stanford.edu>
2016-06-08 13:12:24 -0700

    some changes for files

--- src/client/js/containers/colleagues/colleagues_index.js
+++ src/client/js/containers/colleagues/colleagues_index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 const ColleaguesIndex = React.createClass({
   render() {
-    return <h1>ColleaguesIndex</h1>;
+    return (
+      <div>
+        <h1>Colleagues</h1>
+        <hr />
+        <Link to='/curate/colleagues/new' className='button small'>Add New Colleague</Link>
+      </div>
+    );
   }
 });
 
