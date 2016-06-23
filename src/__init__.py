cc3e40612b236fa93220f691791d3896ff4bd12d
Travis Sheppard <tshepp@stanford.edu>
2016-06-23 13:41:19 -0700

    start having the genes

--- src/__init__.py
+++ src/__init__.py
@@ -21,6 +21,7 @@ def main(global_config, **settings):
     config.add_route('colleague', '/colleagues/{format_name}')
     config.add_route('colleagues', '/colleagues')
     config.add_route('research_interests', '/research_interests')
+    config.add_route('genes', '/genes')
     config.add_route('keywords', '/keywords')
     config.add_route('formats', '/formats')
     config.add_route('topics', '/topics')
