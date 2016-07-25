ab16667bcf7f534d216e159295650985e8d2cce9
Travis Sheppard <tshepp@stanford.edu>
2016-07-25 13:23:22 -0700

    more view tests

--- test/view_test.py
+++ test/view_test.py
@@ -16,3 +16,45 @@ class SGDViewTests(unittest.TestCase):
         req.matchdict = { 'identifier': 'rad54' }
         res = locus(req)
         self.assertEqual(res.status_code, 200)
+
+    def test_blast(self):
+        from src.sgd.frontend.views import blast_sgd
+        req = testing.DummyRequest()
+        res = blast_sgd(req)
+        self.assertEqual(res.status_code, 200)
+
+    def test_fungal_blast(self):
+        from src.sgd.frontend.views import blast_fungal
+        req = testing.DummyRequest()
+        res = blast_fungal(req)
+        self.assertEqual(res.status_code, 200)
+
+    def test_interaction_search(self):
+        from src.sgd.frontend.views import interaction_search
+        req = testing.DummyRequest()
+        res = interaction_search(req)
+        self.assertEqual(res.status_code, 200)
+
+    def test_snapshot(self):
+        from src.sgd.frontend.views import snapshot
+        req = testing.DummyRequest()
+        res = snapshot(req)
+        self.assertEqual(res.status_code, 200)
+
+    def test_suggestion(self):
+        from src.sgd.frontend.views import suggestion
+        req = testing.DummyRequest()
+        res = suggestion(req)
+        self.assertEqual(res.status_code, 200)
+
+    def test_variant_viewer(self):
+        from src.sgd.frontend.views import variant_viewer
+        req = testing.DummyRequest()
+        res = variant_viewer(req)
+        self.assertEqual(res.status_code, 200)
+
+    # def test_search(self):
+    #     from src.sgd.frontend.views import search
+    #     req = testing.DummyRequest()
+    #     res = search(req)
+    #     self.assertEqual(res.status_code, 200)
