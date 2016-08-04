761d7c020a1f4987138a1a5f3a2dc6ab6799340f
Travis Sheppard <tshepp@stanford.edu>
2016-08-04 16:08:53 -0700

    change header wording and add fasolt style to css

--- client/jsx/containers/layout/header.jsx
+++ client/jsx/containers/layout/header.jsx
@@ -10,7 +10,7 @@ const Header = React.createClass({
         <div className="hide-for-small">
           <div className="top-header">
             <div>
-              <span className="header-notice">Thanks for using our beta search. <a href="https://www.surveymonkey.com/r/5M2QQSW" target="_blank">Tell us what you think.</a></span>
+              <span className="header-notice">Thanks for using our new search. <a href="https://www.surveymonkey.com/r/5M2QQSW" target="_blank">Tell us what you think.</a></span>
             </div>
             <div className="site-links">
               <a href="/about">About</a>
