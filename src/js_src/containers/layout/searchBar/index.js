a1b0333bee86b2c4a0aaba8b1825d64a38076d44
Travis Sheppard <tshepp@stanford.edu>
2016-11-15 14:33:38 -0800

    more search base

--- src/js_src/containers/layout/searchBar/index.js
+++ src/js_src/containers/layout/searchBar/index.js
@@ -3,14 +3,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Autosuggest from 'react-autosuggest';
 import { push } from 'react-router-redux';
-import { DropdownButton, MenuItem } from 'react-bootstrap';
 
 import style from './style.css';
 import CategoryLabel from '../../search/categoryLabel';
-import fetchData from '../../../lib/fetchData';
+// import fetchData from '../../../lib/fetchData';
 
 import { CATEGORIES } from '../../../constants';
-const AUTO_BASE_URL = '/api/search_autocomplete';
+// const AUTO_BASE_URL = '/api/search_autocomplete';
 const DEFAULT_CAT = CATEGORIES[0];
 
 class SearchBarComponent extends Component {
@@ -51,29 +50,17 @@ class SearchBarComponent extends Component {
     this.setState({ value: newValue });
   }
 
-  handleFetchData({ value }) {
-    let query = value;
-    let cat = this.state.catOption.name;
-    let catSegment = cat === DEFAULT_CAT.name ? '' : ('&category=' + cat);
-    let url = AUTO_BASE_URL + '?q=' + query + catSegment;
-    fetchData(url)
-      .then( (data) => {
-        let newOptions = data.results || [];
-        this.setState({ autoOptions: newOptions });
-      });
-  }
-
-  renderDropdown() {
-    let _title = this.state.catOption.displayName;
-    let nodes = CATEGORIES.map( d => {
-      let labelNode = (d.name === DEFAULT_CAT.name) ? 'All' : <CategoryLabel category={d.name} />;
-      return <MenuItem className={style.dropdownItem} eventKey={d.name} key={d.name}>{labelNode}</MenuItem>;
-    });
-    return (
-      <DropdownButton className={style.dropdown} id='bg-nested-dropdown' onSelect={this.handleSelect.bind(this)} title={_title}>
-        {nodes}
-      </DropdownButton>
-    );
+  handleFetchData() {
+    return;
+    // let query = value;
+    // let cat = this.state.catOption.name;
+    // let catSegment = cat === DEFAULT_CAT.name ? '' : ('&category=' + cat);
+    // let url = AUTO_BASE_URL + '?q=' + query + catSegment;
+    // fetchData(url)
+    //   .then( (data) => {
+    //     let newOptions = data.results || [];
+    //     this.setState({ autoOptions: newOptions });
+    //   });
   }
 
   renderSuggestion(d) {
@@ -90,7 +77,7 @@ class SearchBarComponent extends Component {
   render() {
     let _getSuggestionValue = ( d => d.name );
     let _inputProps = {
-      placeholder: 'search a gene, GO term, or disease',
+      placeholder: 'search',
       value: this.state.value,
       onChange: this.handleTyping.bind(this)
     };
@@ -106,19 +93,17 @@ class SearchBarComponent extends Component {
     return (
       <div className={style.container}>
         <form onSubmit={this.handleSubmit.bind(this)} ref='form'>
-          {this.renderDropdown()}
-            <Autosuggest
-              getSuggestionValue={_getSuggestionValue}
-              inputProps={_inputProps}
-              onSuggestionsClearRequested={this.handleClear.bind(this)}
-              onSuggestionsFetchRequested={this.handleFetchData.bind(this)}
-              renderSuggestion={this.renderSuggestion}
-              suggestions={this.state.autoOptions}
-              theme={_theme}
-            />
-          <a className={`btn btn-primary ${style.searchBtn}`} href='#' onClick={this.handleSubmit.bind(this)}><i className='fa fa-search' /></a>
+          <Autosuggest
+            getSuggestionValue={_getSuggestionValue}
+            inputProps={_inputProps}
+            onSuggestionsClearRequested={this.handleClear.bind(this)}
+            onSuggestionsFetchRequested={this.handleFetchData.bind(this)}
+            renderSuggestion={this.renderSuggestion}
+            suggestions={this.state.autoOptions}
+            theme={_theme}
+          />
         </form>
-        
+        <i className={`fa fa-search ${style.searchIcon}`} /> 
       </div>
     );
   }
--- src/js_src/routes.js
+++ src/js_src/routes.js
@@ -4,11 +4,13 @@ import { IndexRoute, Route  } from 'react-router';
 import { requireAuthentication } from './containers/authenticateComponent';
 import Layout from './containers/layout';
 import Home from './containers/home';
+import Search from './containers/search';
 import Login from './containers/login';
 
 export default (
   <Route component={Layout} path='/'>
     <IndexRoute component={requireAuthentication(Home)} />
+    <Route compoment={requireAuthentication(Search)} path='/search' />
     <Route component={Login} path='login' />
   </Route>
 );
