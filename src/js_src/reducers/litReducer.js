fb58fa5426c45ab22d973c72b924702ff414112b
Travis Sheppard <tshepp@stanford.edu>
2016-12-20 12:43:52 -0800

    allow tagging

--- src/js_src/reducers/litReducer.js
+++ src/js_src/reducers/litReducer.js
@@ -1,3 +1,4 @@
+/*eslint-disable no-case-declarations */
 import { fromJS } from 'immutable';
 
 // temp fixture
@@ -52,12 +53,23 @@ const DEFAULT_STATE = fromJS({
     citation: 'Kang MS, et al. (2013) Yeast RAD2, a homolog of human XPG, plays a key role in the regulation of the cell cycle and actin dynamics. Biol Open',
     journal: 'Nucleic Acids Research',
     abstract: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-    status: 'reviewing'
+    status: 'reviewing',
+    tags: ['Pathways', 'Phenotype needs review'],
+    assignees: [
+      {
+        username: 'user2',
+        name: 'Harriet Tubman'
+      }
+    ]
   }
 });
 
 export default function litReducer(state = DEFAULT_STATE, action) {
   switch (action.type) {
+  case 'UPDATE_TAGS':
+    let updatedLitEntry = state.get('activeLitEntry').toJS();
+    updatedLitEntry.tags = action.payload;
+    return state.set('activeLitEntry', fromJS(updatedLitEntry));
   default:
     return state;
   }
