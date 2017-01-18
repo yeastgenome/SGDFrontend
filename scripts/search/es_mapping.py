8e03b6209df52e76b1bc6f6be39d998bf15a8822
Pedro Assis <pedroh@stanford.edu>
2017-01-18 13:06:21 -0800

    Search index scripts for nex2

---
+++ scripts/search/es_mapping.py
@@ -0,0 +1,249 @@
+es_mapping = {
+    "settings": {
+        "index": {
+            "max_result_window": 15000,
+            "analysis": {
+                "analyzer": {
+                    "default": {
+                        "type": "standard"
+                    },
+                    "autocomplete": {
+                        "type": "custom",
+                        "filter": ["lowercase", "autocomplete_filter"],
+                        "tokenizer": "standard"
+                    },
+                    "raw": {
+                        "type": "custom",
+                        "filter": ["lowercase"],
+                        "tokenizer": "keyword"
+                    }
+                },
+                "filter": {
+                    "autocomplete_filter": {
+                        "min_gram": "1",
+                        "type": "edge_ngram",
+                        "max_gram": "20"
+                    }
+                }
+            },
+            "number_of_replicas": "1",
+            "number_of_shards": "5"
+        }
+    },
+    "mappings": {
+        "searchable_item": {
+            "properties": {
+                "biological_process": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "category": {
+                    "type": "string"
+                },
+                "observable": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "qualifier": {
+                    "type": "string",
+                    "fields": {"raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "references": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "phenotype_loci": {
+                    "type": "string",
+                    "fields": {"raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "keys": {
+                    "type": "string"
+                },
+                "secondary_sgdid": {
+                    "type": "string"
+                },
+                "chemical": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "mutant_type": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "go_loci": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "strain": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "author": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "journal": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "year": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "first_name": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"},
+                        "autocomplete": {"type": "string", "analyzer": "autocomplete"}
+                    }
+                },
+                "last_name": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"},
+                        "autocomplete": {"type": "string", "analyzer": "autocomplete"}
+                    }
+                },
+                "institution": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"},
+                        "autocomplete": {"type": "string", "analyzer": "autocomplete"}
+                    }
+                },
+                "position": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "country": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"},
+                        "autocomplete": {"type": "string", "analyzer": "autocomplete"}
+                    }
+                },
+                "state": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"},
+                        "autocomplete": {"type": "string", "analyzer": "autocomplete"}
+                    }
+                },
+                "colleague_loci": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "keywords": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"},
+                        "autocomplete": {"type": "string", "analyzer": "autocomplete"}
+                    }
+                },
+                "reference_loci": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "ec_number": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "tc_number": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "cellular_component": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "description": {
+                    "type": "string"
+                },
+                "sequence_history": {
+                    "type": "string"
+                },
+                "gene_history": {
+                    "type": "string"
+                },
+                "summary": {
+                    "type":"string"
+                },
+                "feature_type": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "href": {
+                    "type": "string"
+                },
+                "molecular_function": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "name": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string","analyzer": "raw"},
+                        "autocomplete": {"type": "string", "analyzer": "autocomplete"},
+                        "search": {"type": "string","analyzer": "default"},
+                    }
+                },
+                "go_id": {
+                    "type": "string"
+                },
+                "number_annotations": {
+                    "type": "integer"
+                },
+                "name_description": {
+                    "type": "string"
+                },
+                "synonyms": {
+                    "type": "string"
+                },
+                "phenotypes": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                }
+            }
+        }
+    }
+}
