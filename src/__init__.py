72892f94769a84519ab4f5b1164414ffa5b64aa6
Travis Sheppard <tshepp@stanford.edu>
2017-01-27 15:05:01 -0800

    fix a merge bug

--- src/__init__.py
+++ src/__init__.py
@@ -13,8 +13,6 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
 
     config.add_route('home', '/')
-    config.add_route('upload', '/upload')
-    config.add_route('upload_spreadsheet', '/upload_spreadsheet')
     #search
     config.add_route('search', '/get_search_results')
     config.add_route('autocomplete_results', '/autocomplete_results')
