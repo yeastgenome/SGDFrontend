c2978d976429462d76e124548d5956ca115eb6b9
Pedro Assis <pedroh@stanford.edu>
2017-02-02 13:16:57 -0800

    Adding phenotype/locus_details endpoint

--- src/views.py
+++ src/views.py
@@ -486,3 +486,18 @@ def phenotype(request):
     except:
         log.error("Database failure querying phenotype.")
         return HTTPNotFound()
+
+@view_config(route_name='phenotype_locus_details', renderer='json', request_method='GET')
+def phenotype_locus_details(request):
+    id = request.matchdict['id']
+
+    try:
+        phenotype = DBSession.query(Phenotype).filter_by(phenotype_id=id).one_or_none()
+        if phenotype:
+            return phenotype.annotations_to_dict()
+        else:
+            return HTTPNotFound()
+    except:
+        log.error("Database failure querying phenotype.")
+        return HTTPNotFound()
+
