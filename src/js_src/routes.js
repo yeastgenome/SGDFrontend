37bf840c3462cc3648badb9c7004281d1769fc61
Travis Sheppard <tshepp@stanford.edu>
2017-02-15 10:47:29 -0800

    remove a curate route

--- src/js_src/routes.js
+++ src/js_src/routes.js
@@ -39,7 +39,6 @@ export default (
     <Route component={requireAuthentication(TriageLit)} path='triage_literature/:id' />
     <Route component={requireAuthentication(LocusShow)} path='locus/:id/overview' />
     <Route component={requireAuthentication(RefShow)} path='reference/:id/overview' />
-    <Route component={requireAuthentication(LitIndex)} path='literature' />
     <Route component={requireAuthentication(CurateLit)} path='annotate/reference/:id'>
       <IndexRoute component={requireAuthentication(CurateLitBasic)} />
       <Route component={requireAuthentication(CurateLitOverview)} path='loci' />
