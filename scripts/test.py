23af8c95a55e1b0bbdd82827ee14677ae9ca7510
Shuai Weng <sweng@stanford.edu>
2017-02-17 11:35:08 -0800

    updated to use csv

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,12 @@
 import json
+import csv
 import loading.load_summaries as ls
 
 filename = 'loading/data/16-11phenoSummaries.txt'
 datatype = 'Phenotype'
 
-data = ls.load_summaries(datatype, filename)
+f = open(filename, 'r')
+datareader = csv.reader(f, delimiter='\t')
+data = ls.load_summaries(datatype, datareader)
 
 print json.dumps(data, sort_keys=True, indent=4)
