ce49c9a86ec9ed8ac9415040e70091e7227f70e1
Travis Sheppard <tshepp@stanford.edu>
2017-02-21 11:11:36 -0800

    remove item from triage

--- src/js_src/containers/triage/index.js
+++ src/js_src/containers/triage/index.js
@@ -4,9 +4,10 @@ import { connect } from 'react-redux';
 import style from './style.css';
 import fetchData from '../../lib/fetchData';
 import { selectTriageEntries } from '../../selectors/litSelectors';
-import { updateTriageEntries } from './triageActions';
+import { promoteEntry, updateTriageEntries } from './triageActions';
 
 const TRIAGE_URL = '/reference/triage';
+// const PROMOTE_URL_SUFFIX = 'promote';
 
 class LitTriageIndex extends Component {
   componentDidMount() {
@@ -19,12 +20,33 @@ class LitTriageIndex extends Component {
     });
   }
 
+  promoteEntry(id) {
+    this.props.dispatch(promoteEntry(id));
+    // let url = `${TRIAGE_URL}/${id}/${PROMOTE_URL_SUFFIX}`;
+    // let fetchOptions = {
+    //   type: 'PUT',
+    //   headers: {
+    //     'X-CSRF-Token': window.CSRF_TOKEN,        
+    //   }
+    // };
+    // fetchData(url, fetchOptions).then( () => {
+    //   this.props.dispatch(promoteEntry(id));
+    // });
+  }
+
   renderEntries() {
     let nodes = this.props.triageEntries.map( (d) => {
+      let handlePromoteClick = (e) => {
+        e.preventDefault();
+        this.promoteEntry(d.curation_id);
+      };
       return (
         <div key={'te' + d.curation_id}>
           <h5>{d.basic.citation}</h5>
-          <a className='button secondary small'><i className='fa fa-trash' /> Discard</a>
+          <div>
+            <a className='button secondary small'><i className='fa fa-trash' /> Discard</a>
+            <a className='button small' onClick={handlePromoteClick}><i className='fa fa-check' /> Add to Database</a>
+          </div>
           <p dangerouslySetInnerHTML={{ __html: d.basic.abstract }} />
         </div>
       );
