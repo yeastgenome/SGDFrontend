0e97db9fba63890b6fbe4a3637e367416c72f8c3
Kalpana Karra <kkarra@stanford.edu>
2017-02-21 16:27:47 -0800

    save

--- test/mock_helpers.py
+++ test/mock_helpers.py
@@ -1,3 +1,5 @@
+import fixtures as factory
+
 class MockQueryFilter(object):
     def __init__(self, query_params, query_result):
         self._return = query_result
@@ -44,3 +46,48 @@ class MockQuery(object):
 
 class MockFileStorage(object):
     pass
+
+
+def side_effect(*args, **kwargs):
+    #import pdb;
+    #pdb.set_trace()
+    if len(args) == 1 and str(args[0]) == "<class 'src.models.Straindbentity'>":
+        s_name = factory.StraindbentityFactory()
+        return MockQuery(s_name)
+    if len(args) == 3 and str(args[0]) == 'StrainUrl.display_name' and str(args[1]) == 'StrainUrl.url_type' and str(
+            args[2]) == 'StrainUrl.obj_url':
+        strain_url = factory.StrainUrlFactory()
+        return MockQuery((strain_url.display_name, strain_url.url_type, strain_url.obj_url))
+    elif len(args) == 2 and str(args[0]) == 'Strainsummary.summary_id' and str(args[1]) == 'Strainsummary.html':
+        strain_summary = factory.StrainsummaryFactory()
+        return MockQuery((strain_summary.summary_id, strain_summary.html))
+    elif len(args) == 1 and str(args[0]) == 'StrainsummaryReference.reference_id':
+        # source = factory.SourceFactory()
+        # taxonomy = factory.TaxonomyFactory()
+        # journal = factory.JournalFactory()
+        # book = factory.BookFactory()
+        # reference = factory.ReferencedbentityFactory(dbentity_id=2, sgdid='S99999')
+        #import pdb;
+        #pdb.set_trace()
+        strain_ref = factory.StrainsummaryReferenceFactory()
+        return MockQuery([(strain_ref.reference_id,)])
+    elif len(args) == 1 and str(args[0]) == 'ReferenceUrl.reference_id':
+        refurl = factory.ReferenceUrlFactory()
+        return MockQuery(refurl.obj_url)
+    elif len(args) == 1 and str(args[0]) == "<class 'src.models.Referencedbentity'>":
+        source = factory.SourceFactory()
+        journal = factory.JournalFactory()
+        book = factory.BookFactory()
+        refdbentity = factory.ReferencedbentityFactory()
+        return MockQuery(refdbentity)
+    elif len(args) == 1 and str(args[0]) == "<class 'src.models.ReferenceUrl'>":
+        refurl = factory.ReferenceUrlFactory()
+        return MockQuery(refurl)
+    elif len(args) == 1 and str(args[0]) == "<class 'src.models.Contig'>":
+        c_name = factory.ContigFactory()
+        return MockQuery(c_name)
+    elif len(args) == 2 and str(args[0]) == 'Contig.format_name' and str(args[1]) == 'Contig.obj_url':
+        c_name = factory.ContigFactory()
+        return MockQuery((c_name.format_name, c_name.obj_url))
+
+
