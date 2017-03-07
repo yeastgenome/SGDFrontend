43d93ceb26be9bce19c9bc2bb767f6b82ff1e4d8
Travis Sheppard <tshepp@stanford.edu>
2017-03-06 16:04:36 -0800

    login chage

--- src/js_src/containers/publicHome/index.js
+++ src/js_src/containers/publicHome/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+import style from './style.css';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home</h1>
+      <div className={`callout ${style.loginContainer}`}>
+        <Link to='/login'>Click here to begin login.</Link>
       </div>
     );
   }
