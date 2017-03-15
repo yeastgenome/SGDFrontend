8dd769bf7a8739ed3d51f4c9a47f45ea94ce7229
Travis Sheppard <tshepp@stanford.edu>
2017-03-15 16:04:22 -0700

    change the config and add err catch

--- src/sgd/frontend/yeastgenome/views/cms_helpers.py
+++ src/sgd/frontend/yeastgenome/views/cms_helpers.py
@@ -4,6 +4,7 @@ import requests
 from datetime import datetime
 import os
 from dateutil import parser
+from src.sgd.frontend import config
 
 BLOG_BASE_URL = 'https://public-api.wordpress.com/rest/v1.1/sites/sgdblogtest.wordpress.com/posts'
 BLOG_PAGE_SIZE = 10
@@ -82,32 +83,32 @@ def get_recent_blog_posts():
     return blog_posts
 
 def get_meetings():
-    # try:
-    calendar_url = os.environ['GOOGLE_CALENDAR_API_URL']
-    response = requests.get(calendar_url, timeout=HOMEPAGE_REQUEST_TIMEOUT)
-    meetings = json.loads(response.text)['items']
-    for meeting in meetings:
-        # get URL from description and remove URLs from description
-        urls = re.findall(URL_REGEX, meeting['description'])
-        if len(urls) > 0:
-            url = urls[0]
-        else:
-            url = None
-        meeting['url'] = url
-        meeting['description'] = re.sub(URL_REGEX, '', meeting['description'])
-        # format date as a string which is either a single day or range of dates
-        start_date = datetime.strptime(meeting['start']['date'], '%Y-%m-%d')
-        end_date = datetime.strptime(meeting['end']['date'], '%Y-%m-%d')
-        days_delta = (end_date - start_date).days
-        if (days_delta > 1):
-            start_desc = start_date.strftime('%B %d')
-            end_desc = end_date.strftime('%B %d, %Y')
-            date_description = start_desc + ' to ' + end_desc
-        else:
-            date_description = start_date.strftime('%B %d, %Y')
-        meeting['date_description'] = date_description
-    # except Exception, e:
-    #     meetings = []
+    try:
+        calendar_url = config.google_calendar_api_url
+        response = requests.get(calendar_url, timeout=HOMEPAGE_REQUEST_TIMEOUT)
+        meetings = json.loads(response.text)['items']
+        for meeting in meetings:
+            # get URL from description and remove URLs from description
+            urls = re.findall(URL_REGEX, meeting['description'])
+            if len(urls) > 0:
+                url = urls[0]
+            else:
+                url = None
+            meeting['url'] = url
+            meeting['description'] = re.sub(URL_REGEX, '', meeting['description'])
+            # format date as a string which is either a single day or range of dates
+            start_date = datetime.strptime(meeting['start']['date'], '%Y-%m-%d')
+            end_date = datetime.strptime(meeting['end']['date'], '%Y-%m-%d')
+            days_delta = (end_date - start_date).days
+            if (days_delta > 1):
+                start_desc = start_date.strftime('%B %d')
+                end_desc = end_date.strftime('%B %d, %Y')
+                date_description = start_desc + ' to ' + end_desc
+            else:
+                date_description = start_date.strftime('%B %d, %Y')
+            meeting['date_description'] = date_description
+    except Exception, e:
+        meetings = []
     return meetings
 
 def add_simple_date_to_post(raw):
