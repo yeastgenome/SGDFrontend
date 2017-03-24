4b8d031d8365e0c0342d80d1e77b4c4b12c7c49e
Travis Sheppard <tshepp@stanford.edu>
2017-03-24 13:53:55 -0700

    debug headers

--- src/sgd/frontend/yeastgenome/views/misc_views.py
+++ src/sgd/frontend/yeastgenome/views/misc_views.py
@@ -119,6 +119,7 @@ def phenotype(request):
 # If is_quick, try to redirect to gene page.  If not, or no suitable response, then just show results in script tag and let client js do the rest.
 @view_config(route_name='search') 
 def search(request):
+    print request.headers.keys()
     # get limit, default to 25
     limit = '25' if request.params.get('page_size') is None else request.params.get('page_size')
     # get search results
