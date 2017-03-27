b5b5758d4bb0548900f80549ba0eef55ba1ebb62
Kalpana Karra <kkarra@stanford.edu>
2017-03-27 11:46:29 -0700

    save

--- test/test_views/test_observable.py
+++ test/test_views/test_observable.py
@@ -29,38 +29,78 @@ class ObservableTest(unittest.TestCase):
         response = observable(request)
         self.assertEqual(response, obs.to_dict())
 
-    # @mock.patch('src.models.DBSession.query')
-    # def test_should_return_valid_observable_locus_details(self, mock_search):
-    #     mock_search.side_effect = observable_side_effect
-    #
-    #     obs = factory.ApoFactory()
-    #     request = testing.DummyRequest()
-    #     request.context = testing.DummyResource()
-    #     request.matchdict['id'] = "169841"
-    #
-    #     response = observable_locus_details(request)
-    #     self.assertEqual(response, obs.to_dict())
-    #
-    # @mock.patch('src.models.DBSession.query')
-    # def test_should_return_valid_observable_locus_details_all(self, mock_search):
-    #     mock_search.side_effect = observable_side_effect
-    #
-    #     obs = factory.ApoFactory()
-    #     request = testing.DummyRequest()
-    #     request.context = testing.DummyResource()
-    #     request.matchdict['id'] = "169841"
-    #
-    #     response = observable_locus_details_all(request)
-    #     self.assertEqual(response, obs.to_dict())
-    #
-    # @mock.patch('src.models.DBSession.query')
-    # def test_should_return_valid_observable_ontology_graph(self, mock_search):
-    #     mock_search.side_effect = observable_side_effect
-    #
-    #     obs = factory.ApoFactory()
-    #     request = testing.DummyRequest()
-    #     request.context = testing.DummyResource()
-    #     request.matchdict['id'] = "169841"
-    #
-    #     response = observable_ontology_graph(request)
-    #     self.assertEqual(response, obs.to_dict())
+    @mock.patch('src.models.DBSession.query')
+    def test_should_return_valid_observable_locus_details(self, mock_search):
+        mock_search.side_effect = observable_side_effect
+
+        obs = factory.ApoFactory()
+        request = testing.DummyRequest()
+        request.context = testing.DummyResource()
+        request.matchdict['id'] = "169841"
+
+        response = observable_locus_details(request)
+        self.assertEqual(response, obs.annotations_to_dict())
+
+    @mock.patch('src.models.DBSession.query')
+    def test_should_return_valid_observable_locus_details_all(self, mock_search):
+        mock_search.side_effect = observable_side_effect
+
+        obs = factory.ApoFactory()
+        request = testing.DummyRequest()
+        request.context = testing.DummyResource()
+        request.matchdict['id'] = "169841"
+
+        response = observable_locus_details_all(request)
+        self.assertEqual(response, obs.annotations_and_children_to_dict())
+
+    @mock.patch('src.models.DBSession.query')
+    def test_should_return_valid_observable_ontology_graph(self, mock_search):
+        mock_search.side_effect = observable_side_effect
+
+        obs = factory.ApoFactory()
+        request = testing.DummyRequest()
+        request.context = testing.DummyResource()
+        request.matchdict['id'] = "169841"
+
+        response = observable_ontology_graph(request)
+        self.assertEqual(response, obs.ontology_graph())
+
+    @mock.patch('src.models.DBSession.query')
+    def test_should_return_non_existent_observable(self, mock_search):
+        mock_search.return_value = MockQuery(None)
+
+        request = testing.DummyRequest()
+        request.context = testing.DummyResource()
+        request.matchdict['format_name'] = 'nonexistent_id'
+        response = observable(request)
+        self.assertEqual(response.status_code, 404)
+
+    @mock.patch('src.models.DBSession.query')
+    def test_should_return_non_existent_observable_locus_details(self, mock_search):
+        mock_search.return_value = MockQuery(None)
+
+        request = testing.DummyRequest()
+        request.context = testing.DummyResource()
+        request.matchdict['id'] = "169841"
+        response = observable_locus_details(request)
+        self.assertEqual(response.status_code, 404)
+
+    @mock.patch('src.models.DBSession.query')
+    def test_should_return_non_existent_observable_locus_details_all(self, mock_search):
+        mock_search.return_value = MockQuery(None)
+
+        request = testing.DummyRequest()
+        request.context = testing.DummyResource()
+        request.matchdict['id'] = "169841"
+        response = observable_locus_details_all(request)
+        self.assertEqual(response.status_code, 404)
+
+    @mock.patch('src.models.DBSession.query')
+    def test_should_return_non_existent_observable_ontology_graph(self, mock_search):
+        mock_search.return_value = MockQuery(None)
+
+        request = testing.DummyRequest()
+        request.context = testing.DummyResource()
+        request.matchdict['id'] = "169841"
+        response = observable_ontology_graph(request)
+        self.assertEqual(response.status_code, 404)
