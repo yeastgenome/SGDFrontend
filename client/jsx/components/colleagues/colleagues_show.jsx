afb8fe8ff0f2e6121f5629153108538cc764a15d
Travis Sheppard <tshepp@stanford.edu>
2017-03-29 07:36:50 -0700

    colleague show style

--- client/jsx/components/colleagues/colleagues_show.jsx
+++ client/jsx/components/colleagues/colleagues_show.jsx
@@ -1,39 +1,32 @@
 import React from 'react';
+import Radium from 'radium';
 import { connect } from 'react-redux';
 
-import apiRequest from '../../lib/api_request.jsx';
 import ColleaguesFormShow from './colleagues_form_show.jsx';
 
 const ColleaguesShow = React.createClass({
-  componentDidMount() {
-    this.fetchData();
-  },
-
-  fetchData() {
-    let formatName = this.props.routeParams.formatName;
-    let url = `/backend/colleagues/${formatName}`;
-    apiRequest(url).then( json => {
-      console.log(json);
-      // let _data = _.findWhere(json, { format_name: name });
-      // this.setState({ data: _data, isLoadPending: false });
-    });
-  },
-
   render () {
     return (
-      <div>
+      <div style={[style.container]}>
         <ColleaguesFormShow
           isReadOnly={true} isCurator={false} 
           colleagueDisplayName={this.props.routeParams.formatName}
         />
+        <a className='button secondary small disabled'>Update Colleague</a>
       </div>
     );
   }
 });
+
+const style = {
+  container: {
+    marginBottom: '2rem'
+  }
+};
 
 function mapStateToProps(_state) {
   return {
   };
 }
 
-export default connect(mapStateToProps)(ColleaguesShow);
+export default connect(mapStateToProps)(Radium(ColleaguesShow));
