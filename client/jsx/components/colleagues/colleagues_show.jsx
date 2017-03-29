9f1bcc2027845d1f970167fdfa5bbc02b2f06eb4
Travis Sheppard <tshepp@stanford.edu>
2017-03-28 17:08:30 -0700

    display colleague

--- client/jsx/components/colleagues/colleagues_show.jsx
+++ client/jsx/components/colleagues/colleagues_show.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import apiRequest from '../../lib/api_request.jsx';
 import ColleaguesFormShow from './colleagues_form_show.jsx';
 
 const ColleaguesShow = React.createClass({
+  componentDidMount() {
+    this.fetchData();
+  },
+
+  fetchData() {
+    let formatName = this.props.routeParams.formatName;
+    let url = `/backend/colleagues/${formatName}`;
+    apiRequest(url).then( json => {
+      console.log(json);
+      // let _data = _.findWhere(json, { format_name: name });
+      // this.setState({ data: _data, isLoadPending: false });
+    });
+  },
+
   render () {
     return (
       <div>
