0809ee22dcdbecf4ac931ff0bbb469f13cd7b88b
Travis Sheppard <tshepp@stanford.edu>
2017-05-24 19:03:38 -0700

    perform an update

--- src/loading/load_summaries_sync.py
+++ src/loading/load_summaries_sync.py
@@ -32,6 +32,7 @@ logger.setLevel(level=logging.INFO)
 def validate_file_content(file_content, nex_session):
     header_literal = ['# Feature', 'Summary Type (phenotype, regulation, protein, or sequence)', 'Summary', 'PMIDs']
     file_gene_ids = []
+    copied = []
     for i, val in enumerate(file_content):
         # match header
         if i is 0:
@@ -43,13 +44,37 @@ def validate_file_content(file_content, nex_session):
         # match length of each row
         if len(val) != len(header_literal):
             raise ValueError('Row has incorrect number of columns.')
+        copied.append(val)
     # check that gene names are valid
     matching_genes = nex_session.query(Locusdbentity).filter(Locusdbentity.format_name.in_(file_gene_ids)).count()
     is_correct_match_num = matching_genes == len(file_gene_ids)
     if not is_correct_match_num:
         raise ValueError('Invalid gene identifier in ', str(file_gene_ids))
+    # update 
+    for i, val in enumerate(copied):
+        if i != 0:
+            file_id = val[0]
+            gene = nex_session.query(Locusdbentity).filter(Locusdbentity.format_name.match(file_id)).all()[0]
+            summaries = DBSession.query(Locussummary.summary_id, Locussummary.html, Locussummary.date_created).filter_by(locus_id=gene.dbentity_id, summary_type='Gene').all()
+            new_summary_val = val[2]
+            # update
+            if len(summaries):
+                summary = summaries[0]
+                nex_session.query(Locussummary).filter_by(summary_id=summary.summary_id).update({ 'text': new_summary_val, 'html': new_summary_val })
+                # nex_session.query(summary.update({ 'text': new_summary_val, 'html': new_summary_val }))
+            # TODO create
+            # else:
+
 def load_summaries(nex_session, file_content, username, summary_type=None):
-    validate_file_content(file_content, nex_session)
+    print str(enumerate(file_content))
+    # print val
+    # # update each gene summary
+    # file_id = val[0]
+    # gene = nex_session.query(Locusdbentity).filter(Locusdbentity.format_name.match(file_id))
+    # print gene
+    # summary = DBSession.query(Locussummary.summary_id, Locussummary.html, Locussummary.date_created).filter_by(locus_id=self.dbentity_id, summary_type="Gene").all()
+
+    # nex_session.query(Locussummary).filter_by(summary_id=key_to_summary[key].summary_id).update({'text': x['text'], 'html': x['html']})
     annotation_summary = []
     return annotation_summary
 
@@ -58,4 +83,4 @@ if __name__ == '__main__':
     DBSession.configure(bind=engine)
     with open('test/example_files/example_summary_upload.tsv') as tsvfile:
         tsvreader = csv.reader(tsvfile, delimiter="\t")
-        load_summaries(DBSession, tsvreader, 'USER')
+        validate_file_content(tsvreader, DBSession)
