44423b08e55b925e0332c001f083d8846192510c
Travis Sheppard <tshepp@stanford.edu>
2017-05-24 19:16:14 -0700

    try to really parse

--- src/tsv_parser.py
+++ src/tsv_parser.py
@@ -1,8 +1,8 @@
 import csv
-from loading.load_summaries_sync import load_summaries_sync
+from loading.load_summaries_sync import load_summaries
 
 # takes a TSV file and returns an array of annotations
 def parse_tsv_annotations(db_session, tsv_file, template_type, username):
     raw_file_content = csv.reader(tsv_file, delimiter='\t')
-    annotations = load_summaries_sync(db_session, raw_file_content, username)
+    annotations = load_summaries(db_session, raw_file_content, username)
     return annotations
