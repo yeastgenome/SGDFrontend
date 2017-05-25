8dfadede6bfa81fce3daf6a404196a77fea5621e
Travis Sheppard <tshepp@stanford.edu>
2017-05-25 13:50:44 -0700

    show recent annotations

--- src/js_src/containers/curateHome/index.js
+++ src/js_src/containers/curateHome/index.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
+import fetchData from '../../lib/fetchData';
+import AnnotationSummary from '../../components/annotationSummary';
+import LoadingPage from '../../components/loadingPage';
+
+const ANNOTATION_URL = '/annotations';
+
 class CurateHome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPending: true,
+      annotationData: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetchData(ANNOTATION_URL).then( (data) => {
+      this.setState({ annotationData: data, isPending: false });
+    });
+  }
+
   render() {
+    if (this.state.isPending) return <LoadingPage />;
     return (
       <div>
-        <h1>Curation Home</h1>
+        <AnnotationSummary annotations={this.state.annotationData} />
       </div>
     );
   }
