103971b5c7cae6b5e5a8e511092084bdeefe8ad9
Travis Sheppard <tshepp@stanford.edu>
2017-05-30 16:12:17 -0700

    only upload after successful processing

--- src/tsv_parser.py
+++ src/tsv_parser.py
@@ -9,9 +9,9 @@ S3_BUCKET = os.environ['S3_BUCKET']
 
 # takes a TSV file and returns an array of annotations
 def parse_tsv_annotations(db_session, tsv_file, filename, template_type, username):
+    raw_file_content = csv.reader(tsv_file, delimiter='\t')
+    annotations = load_summaries(db_session, raw_file_content, username)
     conn = tinys3.Connection(S3_ACCESS_KEY,S3_SECRET_KEY,tls=True)
     s3_file_path = username + '/' + filename
     conn.upload(s3_file_path, tsv_file, S3_BUCKET)
-    raw_file_content = csv.reader(tsv_file, delimiter='\t')
-    annotations = load_summaries(db_session, raw_file_content, username)
     return annotations
