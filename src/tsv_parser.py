eb8c67a6ff741d9d485f9ae6cbd6ac2895adfb72
Travis Sheppard <tshepp@stanford.edu>
2017-06-02 14:12:29 -0700

    restore upload

--- src/tsv_parser.py
+++ src/tsv_parser.py
@@ -10,9 +10,9 @@ S3_BUCKET = os.environ['S3_BUCKET']
 
 # takes a TSV file and returns an array of annotations
 def parse_tsv_annotations(db_session, tsv_file, filename, template_type, username):
-    # TEMP
-    upload_file(username, tsv_file, filename=filename)
-    return []
+    # # TEMP
+    # upload_file(username, tsv_file, filename=filename)
+    # return []
 
     raw_file_content = csv.reader(tsv_file, delimiter='\t')
     annotations = load_summaries(db_session, raw_file_content, username)
