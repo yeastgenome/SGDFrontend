fc132ab9ebbc5c18cca4249c01452a25da0d6586
Travis Sheppard <tshepp@stanford.edu>
2017-06-05 13:50:45 -0700

    better spreadsheet error checking

--- src/tsv_parser.py
+++ src/tsv_parser.py
@@ -7,18 +7,24 @@ from helpers import upload_file
 
 # takes a TSV file and returns an array of annotations
 def parse_tsv_annotations(db_session, tsv_file, filename, template_type, username):
-    raw_file_content = csv.reader(tsv_file, delimiter='\t')
-    annotations = load_summaries(db_session, raw_file_content, username)
-    upload_file(
-    	username, tsv_file,
-    	filename=filename,
-    	data_id=248375,
-        description='summary upload',
-        display_name=filename,
-        format_id=248824,
-        format_name='TSV',
-        file_extension='tsv',
-        json=json.dumps(annotations),
-		topic_id=250482
-    )
-    return annotations
+    try:
+        print tsv_file
+        dialect = csv.Sniffer().sniff(tsv_file.read(1024), delimiters='\t')
+        tsv_file.seek(0)
+        raw_file_content = csv.reader(tsv_file, delimiter='\t')
+        annotations = load_summaries(db_session, raw_file_content, username)
+        upload_file(
+            username, tsv_file,
+            filename=filename,
+            data_id=248375,
+            description='summary upload',
+            display_name=filename,
+            format_id=248824,
+            format_name='TSV',
+            file_extension='tsv',
+            json=json.dumps(annotations),
+            topic_id=250482
+        )
+        return annotations
+    except:
+        raise ValueError('File format not accepted. Please upload a valid TSV file.')
