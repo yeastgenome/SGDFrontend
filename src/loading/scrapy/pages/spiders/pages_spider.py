2c31f6f0d5d1e127b54d311acf70826500ebbb0f
Travis Sheppard <tshepp@stanford.edu>
2017-06-09 14:33:03 -0700

    attempt to raise the threads

--- src/loading/scrapy/pages/spiders/pages_spider.py
+++ src/loading/scrapy/pages/spiders/pages_spider.py
@@ -23,8 +23,8 @@ logger.setLevel(level=logging.INFO)
 # init spiders
 class BaseSpider(scrapy.Spider):
     custom_settings = {
-        'CONCURRENT_REQUESTS_PER_DOMAIN': 4,
-        'CONCURRENT_REQUESTS': 8
+        'CONCURRENT_REQUESTS_PER_DOMAIN': 8,
+        'CONCURRENT_REQUESTS': 16
     }
     
     def get_entities(self):
