c4c20126beb8b3d11fa5e92cc8c142bdfc10b23d
Travis Sheppard <tshepp@stanford.edu>
2017-07-10 10:47:24 -0700

    supress warnings from restart script

--- system_config/restart.sh
+++ system_config/restart.sh
@@ -3,5 +3,5 @@
 cd /data/www/SGDBackend-NEX2/current
 source /data/envs/sgd/bin/activate
 . prod_variables.sh
-/usr/bin/make stop-prod
-/usr/bin/make run-prod
+/usr/bin/make stop-prod >/dev/null
+/usr/bin/make run-prod >/dev/null
