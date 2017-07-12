1d4583378d92e957a682246b490c3516044ae1e0
Pedro Assis <pedroh@stanford.edu>
2017-07-12 10:20:25 -0700

    Setting encode utf-8 for display names with special characters

--- src/sgd/frontend/yeastgenome/__init__.py
+++ src/sgd/frontend/yeastgenome/__init__.py
@@ -253,8 +253,8 @@ class YeastgenomeFrontend(FrontendInterface):
         
         response.text = '\n' + '\n\n'.join([ref['text'] for ref in references])
         
-        headers['Content-Type'] = 'text/plain'        
-        headers['Content-Disposition'] = str('attachment; filename=' + display_name + '.nbib')
+        headers['Content-Type'] = 'text/plain'
+        headers['Content-Disposition'] = str('attachment; filename=' + display_name.encode('utf-8') + '.nbib')
         headers['Content-Description'] = 'File Transfer'
         return response
 
