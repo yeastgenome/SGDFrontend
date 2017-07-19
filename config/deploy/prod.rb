a71063831700959b0d54644e7ae369daad8a11bb
Travis Sheppard <tshepp@stanford.edu>
2017-07-19 11:48:24 -0700

    parallel prod deploy

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,3 +1,4 @@
 set :stage, :prod
 
-server ENV['AWS_SERVER'], user: 'deploy', port: 22, roles: %w{app}
+server ENV['PROD_SERVER_A'], user: 'deploy', port: 22, roles: %w{app}
+server ENV['PROD_SERVER_B'], user: 'deploy', port: 22, roles: %w{app}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,4 @@
 set :stage, :prod
 
-server ENV['STAGING_SERVER_A'], ENV['STAGING_SERVER_B']
-user: 'deploy',
-port: 22,
-roles: %w{app}
+server ENV['STAGING_SERVER_A'], user: 'deploy', port: 22, roles: %w{app}
+server ENV['STAGING_SERVER_B'], user: 'deploy', port: 22, roles: %w{app}
