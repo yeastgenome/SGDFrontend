e922fbdd11d38877b83fcf836d25fc3714dc2bb0
felixgondwe <gondwefelix8@gmail.com>
2017-07-20 16:07:32 -0700

    changed deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ lock '3.4.0'
 
 set :application, 'SGDBackend-NEX2'
 
-set :repo_url, 'git@github.com:yeastgenome/SGDBackend-Nex2.git'
+set :repo_url, 'git://github.com/yeastgenome/SGDBackend-Nex2.git'
 set :branch, ENV['BRANCH'] || $1 if `git branch` =~ /\* (\S+)\s/m
 set :deploy_to, '/data/www/' + fetch(:application)
 set :pty, true
