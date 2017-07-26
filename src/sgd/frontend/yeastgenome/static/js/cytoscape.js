d47a0f1ccb196c89eda38ff6a05dee16a8be49ec
Travis Sheppard <tshepp@stanford.edu>
2017-07-26 11:05:49 -0700

    remove debugger

--- src/sgd/frontend/yeastgenome/static/js/cytoscape.js
+++ src/sgd/frontend/yeastgenome/static/js/cytoscape.js
@@ -293,7 +293,6 @@ function create_slider_ticks(slider_tick_id, min, max, stop) {
 }
 
 function create_discrete_filter(radio_id, graph, slider, target_filter, max_value) {
-	debugger
     var radio = $("#" + radio_id);
     radio.click(function() {
         if(slider != null) {
