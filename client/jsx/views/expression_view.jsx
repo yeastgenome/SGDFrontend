82feb2897cc829799cd59d7ccefd902b88f852a9
Travis Sheppard <tshepp@stanford.edu>
2017-08-18 13:53:47 -0700

    read expression details from s3

--- client/jsx/views/expression_view.jsx
+++ client/jsx/views/expression_view.jsx
@@ -6,25 +6,23 @@ var $ = require("jquery");
 var ExpressionChart = require("../components/viz/expression_chart.jsx");
 
 var expressionView = {};
-expressionView.render = function () {
-  $.getJSON('/backend/locus/' + locus['id'] + '/expression_details', (data) => {
-    if (data.datasets.length) {
+expressionView.render = function (data) {
+  if (data.datasets.length) {
 
-      // use filter_table(minValue, maxValue) method from expression_details.js
-      var _onClick = null;
-      if (filter_table) {
-        _onClick = (minValue, maxValue) => { filter_table(minValue, maxValue); };
-      }
-
-      ReactDOM.render(
-        <ExpressionChart data={data.overview} minValue={data.min_value} maxValue={data.max_value} hasScaleToggler={true} onClick={_onClick} hasHelpIcon={true}/>,
-        document.getElementById("j-expression-chart-target")
-      );
-    } else {
-      $("#expression_overview_panel").hide();
-          $("#expression_message").show();
+    // use filter_table(minValue, maxValue) method from expression_details.js
+    var _onClick = null;
+    if (filter_table) {
+      _onClick = (minValue, maxValue) => { filter_table(minValue, maxValue); };
     }
-  });
+
+    ReactDOM.render(
+      <ExpressionChart data={data.overview} minValue={data.min_value} maxValue={data.max_value} hasScaleToggler={true} onClick={_onClick} hasHelpIcon={true}/>,
+      document.getElementById("j-expression-chart-target")
+    );
+  } else {
+    $("#expression_overview_panel").hide();
+        $("#expression_message").show();
+  }
 };
 
 module.exports = expressionView;
