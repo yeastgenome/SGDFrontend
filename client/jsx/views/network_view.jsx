abd3b0524332f6779133469d6d6a710ecaeaea59
Travis Sheppard <tshepp@stanford.edu>
2017-10-10 14:10:37 -0700

    start adding label and click events

--- client/jsx/views/network_view.jsx
+++ client/jsx/views/network_view.jsx
@@ -1,16 +1,18 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const _ = require('underscore');
+import d3 from 'd3';
 
 const Graph = require("../components/viz/graph.jsx");
 
 var networkView = {};
 networkView.render = function renderNetworkView (graphData) {
-    var _colorScale = function(d) {
-      if (d.sub_type == 'TARGET') return "#9F75B8";
-      if (d.sub_type == 'REGULATOR') return "#6CB665";
-      return "#1660A7";
-    }
+    // var _colorScale = function(d) {
+    //   if (d.sub_type == 'TARGET') return "#9F75B8";
+    //   if (d.sub_type == 'REGULATOR') return "#6CB665";
+    //   return "#1660A7";
+    // }
+    var _colorScale = d3.scale.ordinal().domain(['TARGET', 'REGULATOR']).range(["#9F75B8", "#6CB665"]);
     ReactDOM.render(<Graph colorScale={_colorScale} data={graphData} stage={0} />, document.getElementById('j-network'));
 };
 
