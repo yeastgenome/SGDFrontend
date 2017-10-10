7982d92c62f60c2d0c87f0815c051879d4382420
Travis Sheppard <tshepp@stanford.edu>
2017-10-10 14:30:11 -0700

    footer and finish header

--- client/jsx/views/network_view.jsx
+++ client/jsx/views/network_view.jsx
@@ -7,13 +7,10 @@ const Graph = require("../components/viz/graph.jsx");
 
 var networkView = {};
 networkView.render = function renderNetworkView (graphData) {
-    // var _colorScale = function(d) {
-    //   if (d.sub_type == 'TARGET') return "#9F75B8";
-    //   if (d.sub_type == 'REGULATOR') return "#6CB665";
-    //   return "#1660A7";
-    // }
-    var _colorScale = d3.scale.ordinal().domain(['TARGET', 'REGULATOR']).range(["#9F75B8", "#6CB665"]);
-    ReactDOM.render(<Graph colorScale={_colorScale} data={graphData} stage={0} />, document.getElementById('j-network'));
+    var _colorScale = d3.scale.ordinal().domain(['REGULATOR', 'TARGET', 'FOCUS']).range(["#9F75B8", "#6CB665", "#1f77b4"]);
+    var tempDate = new Date();
+    var _footerText = `SGD ${tempDate.getFullYear()}-${tempDate.getMonth() + 1}-${tempDate.getDate()}`; 
+    ReactDOM.render(<Graph colorScale={_colorScale} data={graphData} footerText={_footerText} stage={0} />, document.getElementById('j-network'));
 };
 
 module.exports = networkView;
