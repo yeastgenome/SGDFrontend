e20e4b31a9f7e59f54ded3a744ab6d2ddb54e9f3
Travis Sheppard <tshepp@stanford.edu>
2017-10-13 14:01:11 -0700

    system script for reference triage loading

---
+++ system_config/cron/load_reference_triage.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+cd /data/www/SGDBackend-NEX2/current
+source /data/envs/sgd/bin/activate && source prod_variables.sh && python scripts/loading/load_reference_triage.py &>> /data/www/logs/triage_worker.log
