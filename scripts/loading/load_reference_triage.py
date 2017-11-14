ee580d149b9bef37d73820da14327d352e7081e2
Travis Sheppard <tshepp@stanford.edu>
2017-11-14 09:26:03 -0800

    turn back crontab

--- scripts/loading/load_reference_triage.py
+++ scripts/loading/load_reference_triage.py
@@ -19,8 +19,7 @@ __author__ = 'sweng66'
 
 TERMS = ['yeast', 'cerevisiae']
 URL = 'http://www.ncbi.nlm.nih.gov/pubmed/'
-# TEMP get backlog of triage
-DAY = 86#14 
+DAY = 14 
 RETMAX = 10000
 
 logging.basicConfig()
