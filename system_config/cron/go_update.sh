78d7d84939f90d4c0e9b2410ff5cff1278d1fa53
Shuai Weng <sweng@stanford.edu>
2017-11-22 14:09:29 -0800

    minor name fixes

--- system_config/cron/go_update.sh
+++ system_config/cron/go_update.sh
@@ -2,7 +2,7 @@
 
 cd /data/www/SGDBackend-NEX2/current
 source /data/envs/sgd/bin/activate 
-source source_variables-CURATE.sh 
+source dev_variables.sh 
 python scripts/loading/ontology/go.py
 python scripts/loading/go/load_gpad.py 'manually curated'
 python scripts/loading/go/load_gpad.py computational
