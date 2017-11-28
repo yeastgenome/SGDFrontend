b0580c4daeddeb178ae0169db10cd9801f4077dc
Shuai Weng <sweng@stanford.edu>
2017-11-27 19:39:54 -0800

    minor fix

--- system_config/cron/reference_update.sh
+++ system_config/cron/reference_update.sh
@@ -3,5 +3,5 @@
 
 cd /data/www/SGDBackend-NEX2/current
 source /data/envs/sgd/bin/activate 
-source dev_variables.sh 
+source prod_variables.sh 
 python scripts/loading/reference/reference_update.py
