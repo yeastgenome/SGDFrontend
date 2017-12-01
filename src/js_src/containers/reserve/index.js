78c5987a1733df7e78b0b01565767b6bc0f4cbda
Travis Sheppard <tshepp@stanford.edu>
2017-12-01 13:14:04 -0800

    better form style

--- src/js_src/containers/reserve/index.js
+++ src/js_src/containers/reserve/index.js
@@ -16,28 +16,63 @@ class GeneNameReservation extends Component {
     });
     let reserveSchema = t.struct({
       new_gene_name: t.String,
-      description: t.String,
       orf_name: t.String,
+      description: t.String,
       first_name: t.String,
       last_name: t.String,
       email: t.String,
-      institution: t.String,
+      phone_number: t.String,
       position: t.String,
-      title: t.String,
+      institution: t.String,
+      profession: t.String,
+      publication_title: t.String,
       journal: t.String,
       year: t.Number,
       authors: t.list(Author)
     });
     let authorLayout = locals => {
       return (
-        <div>
+        <div className='row'>
           <div className='column small-5'>{locals.inputs.first_name}</div>
           <div className='column small-5'>{locals.inputs.last_name}</div>
           <div className='column small-2'>{locals.inputs.orcid}</div>
         </div>
       );
     };
+    let formLayout = locals => {
+      return (
+        <div>
+          <p>* indicates required field</p>
+          <p><b>Gene Name Information</b></p>
+          <div className='row'>
+            <div className='column small-6'>{locals.inputs.new_gene_name}</div>
+            <div className='column small-6'>{locals.inputs.orf_name}</div>
+          </div>
+          <div>{locals.inputs.description}</div>
+          <p><b>Your Information</b></p>
+          <div className='row'>
+            <div className='column small-3'>{locals.inputs.first_name}</div>
+            <div className='column small-3'>{locals.inputs.last_name}</div>
+            <div className='column small-3'>{locals.inputs.email}</div>
+            <div className='column small-3'>{locals.inputs.phone_number}</div>
+          </div>
+          <div className='row'>
+            <div className='column small-4'>{locals.inputs.position}</div>
+            <div className='column small-4'>{locals.inputs.institution}</div>
+            <div className='column small-4'>{locals.inputs.profession}</div>
+          </div>
+          <p><b>Publication Information</b></p>
+          <div className='row'>
+            <div className='column small-6'>{locals.inputs.publication_title}</div>
+            <div className='column small-4'>{locals.inputs.journal}</div>
+            <div className='column small-2'>{locals.inputs.year}</div>
+          </div>
+          <div>{locals.inputs.authors}</div>
+        </div>
+      );
+    };
     let reserveOptions = {
+      template: formLayout,
       fields: {
         new_gene_name: {
           label: 'Proposed Gene Name *'
@@ -57,8 +92,8 @@ class GeneNameReservation extends Component {
         email: {
           label: 'Email *'
         },
-        title: {
-          label: 'Publication Title'
+        publication_title: {
+          label: 'Publication Title *'
         },
         authors: {
           disableOrder: true,
@@ -75,7 +110,7 @@ class GeneNameReservation extends Component {
       <div>
         <h1>Reserve a Gene Name</h1>
         <FlexiForm tFormOptions={reserveOptions} tFormSchema={reserveSchema} onSuccess={_onSuccess} submitText='Send gene name reservation' updateUrl={TARGET_URL} />
-        <p>* indicates required field</p>
+        
       </div>
     );
   }
