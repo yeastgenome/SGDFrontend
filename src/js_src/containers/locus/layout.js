9d11613110fe93c92be9646208a8a8aec04aea32
Travis Sheppard <tshepp@stanford.edu>
2018-01-11 11:16:20 -0800

    integrate UI

--- src/js_src/containers/locus/layout.js
+++ src/js_src/containers/locus/layout.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 import t from 'tcomb-form';
 
+import style from './style.css';
 import CategoryLabel from '../../components/categoryLabel';
 import fetchData from '../../lib/fetchData';
 import Loader from '../../components/loader';
@@ -10,7 +12,13 @@ import { updateData, setPending, clearPending} from './locusActions';
 import { setMessage, setError, clearError } from '../../actions/metaActions';
 import { PREVIEW_URL } from '../../constants.js';
 
-class LocusShow extends Component {
+const BASE_CURATE_URL = '/curate/locus';
+const SECTIONS = [
+  'basic',
+  'summaries'
+];
+
+class LocusLayout extends Component {
   componentDidMount() {
     this.fetchData();
   }
@@ -83,6 +91,24 @@ class LocusShow extends Component {
     );
   }
 
+  renderNav() {
+    let baseUrl = `${BASE_CURATE_URL}/${this.props.params.id}`;
+    let current = this.props.pathname.replace(baseUrl, '');
+    let nodes = SECTIONS.map( (d) => {
+      let relative;
+      if (d === 'basic') {
+        relative = '';
+      } else {
+        relative = `/${d}`;
+      }
+      let isActive = (current === relative);
+      let url = `${baseUrl}${relative}`;
+      let _className = isActive ? style.activeNavLink : style.navLink;
+      return <li key={`lit${d}`}><Link className={_className} to={url}>{d}</Link></li>;
+    });
+    return <ul className={`vertical menu ${style.menu}`}>{nodes}</ul>;
+  }
+
   render() {
     let data = this.props.data;
     if (!data || this.props.isPending) return <Loader />;
@@ -92,16 +118,25 @@ class LocusShow extends Component {
         <h3 style={{ display: 'inline-block', marginRight: '0.5rem' }}><CategoryLabel category='locus' hideLabel isPageTitle /> {data.name}</h3>
         <span><a href={previewUrl} target='_new'><i className='fa fa-file-image-o' aria-hidden='true'></i> preview</a></span>
         <hr style={{ margin: 0 }} />
-        {this.renderForms()}
+        <div className='row'>
+          <div className='columns small-2'>
+            {this.renderNav()}
+          </div>
+          <div className='columns small-10'>
+            {this.renderForms()}
+          </div>  
+        </div>
+        
       </div>
     );
   }
 }
 
-LocusShow.propTypes = {
+LocusLayout.propTypes = {
   data: React.PropTypes.object,
   dispatch: React.PropTypes.func,
   isPending: React.PropTypes.bool,
+  pathname: React.PropTypes.string,
   params: React.PropTypes.object
 };
 
@@ -109,9 +144,10 @@ function mapStateToProps(state) {
   let _data = state.locus.get('data') ? state.locus.get('data').toJS() : null;
   return {
     data: _data,
-    isPending: state.locus.get('isPending')
+    isPending: state.locus.get('isPending'),
+    pathname: state.routing.locationBeforeTransitions.pathname
   };
 }
 
-export { LocusShow as LocusShow };
-export default connect(mapStateToProps)(LocusShow);
+export { LocusLayout as LocusLayout };
+export default connect(mapStateToProps)(LocusLayout);
