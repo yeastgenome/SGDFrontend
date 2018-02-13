c4d31c20973ea2d1c3d59901c45632a538de86a3
sweng66 <sweng@stanford.edu>
2018-02-13 11:03:58 -0800

    updated for working with patmatch server

--- src/sgd/tools/patmatch/__init__.py
+++ src/sgd/tools/patmatch/__init__.py
@@ -5,29 +5,19 @@ from urllib2 import Request, urlopen, URLError
 
 def do_patmatch(request):
 
-    
     p = dict(request.params)
-
-    if p.get('name'):
-        data = _get_seq(p.get('name'), p.get('type'))
-        return Response(body=json.dumps(data), content_type='application/json')
-
-    if p.get('conf'):
-        data = _get_config(p.get('conf')) 
-        return Response(body=json.dumps(data), content_type='application/json')
         
-    data = _run_blast(p)
+    data = _run_patmatch(p)
 
     return Response(body=json.dumps(data), content_type='application/json')
 
-
-def _run_blast(p):
+def _run_patmatch(p):
 
     from urllib2 import Request, urlopen, URLError
 
-    paramData = _construct_blast_parameters(p)
+    paramData = _construct_patmatch_parameters(p)
 
-    url = config.compute_url + "cgi-bin/aws-blast"
+    url = config.patmatch_url + "cgi-bin/aws-patmatch"
 
     req = Request(url=url, data=paramData)
 
@@ -50,115 +40,33 @@ def _run_blast(p):
 
     return data
 
-def _construct_blast_parameters(p):
+def _construct_patmatch_parameters(p):
 
-    program = p.get('program')
-    database = p.get('database')
-    seq = p.get('seq')
-    seqname = p.get('seqname')
-    if seqname == None:
-        seqname = ''
-    confFile = 'blast-sgd.conf'
-    if p.get('blastType') == 'fungal':
-        confFile = 'blast-fungal.conf'
-    filtering = 0
-    if p.get(filter) == 'on':
-        filtering = 1
-
-    blastOptions = _get_blast_options(p)
-
-    database = database.replace(',', ' ')
-    database = database.replace('  ', ' ')
-    database = database.replace(' ', '+')
-
-    blastOptions = blastOptions.replace(' ', '+')
+    insertion = p.get('insertion') if p.get('insertion') else 0 
+    deletion = p.get('deletion') if p.get('deletion') else 0
+    substitution = p.get('substitution') p.get('substitution') else 0
+    
+    if strand.startswith('Both'):
+        strand = 'both'
+    elif strand.startswith('Reverse'):
+        strand = 'crick'
+    else:
+        strand = 'watson'
 
     import urllib
     
-    paramData = urllib.urlencode({ 'program': program,
-                                   'dataset': database,
-                                   'sequence': seq,
-                                   'seqname': seqname,
-                                   'config': confFile,
-                                   'options': blastOptions,
-                                   'filtering': filtering,
-                                   'alignToShow': p['alignToShow'] })
+    paramData = urllib.urlencode({ 'pattern': p.get('pattern'),
+                                   'strain': p.get('strain'),
+                                   'seqtype': p.get('seqtype'),
+                                   'dataset': p.get('dataset'),
+                                   'maxhits': p.get('maxhits'),
+                                   'strand': strand,
+                                   'mismatch': p.get('mismatch'),
+                                   'insertion': insertion,
+                                   'deletion': deletion,
+                                   'substitution': substitution})
     return paramData
 
-    
-def _get_seq(name, type):
-
-    data = {}
-    if type == None or type == '' or type == 'undefined':
-        type = 'dna'
-
-    url = config.backend_url + "/locus/" + name + "/sequence_details"
-    res = _get_json_from_server(url)
-    if type in ('nuc', 'dna'):
-        data['seq'] = res['genomic_dna'][0]['residues']
-    else:
-        data['seq'] = res['protein'][0]['residues']
-        
-    return data
-
-
-def _get_config(conf):
-
-    url = config.compute_url + "blast/" + conf + ".json"
-    data = _get_json_from_server(url)
-    
-    return data
-
-
-def _get_json_from_server(url):
-
-    req = Request(url)
-    res = urlopen(req)
-    data = json.loads(res.read())
-    return data
-
-def _get_blast_options(p):
-    
-    seq = p.get('seq')
-    program = p.get('program')
-    database = p.get('database')
-    outFormat = p.get('outFormat')
-    matrix = p.get('matrix')
-    threshold = p.get('threshold')
-    cutoffScore = p.get('cutoffScore')
-    alignToShow = p.get('alignToShow')
-    wordLength = p.get('wordLength')
-    filter = p.get('filter')
-    
-    options = "-evalue " + cutoffScore + " -num_alignments " + alignToShow 
-
-    if (database == 'Sc_mito_chr' and (program == 'blastn' or program == 'tblastx')):
-        # default is 1                      
-        options = options + " -query_genetic_code 3"
-
-    if program != 'blastn' and threshold and threshold != 'default':
-        options = options + " -threshold " + threshold
-
-    if program != 'blastn' and matrix != "BLOSUM62":
-        options = options + " -matrix " + matrix
-                
-    if wordLength != 'default':
-        options = options + " -word_size " + wordLength
-     
-    # options = options + " -outfmt 0 -html"
-
-    options = options + " -outfmt 0"
-
-    if outFormat.startswith("ungapped"):
-        options = options + " -ungapped"
-
-    if filter == 'on':
-        if program != 'blastn':
-            options = options + " -seg yes"
-        else:
-            options = options + " -dust '20 64 1'"
-    
-    return options;
 
              
 
