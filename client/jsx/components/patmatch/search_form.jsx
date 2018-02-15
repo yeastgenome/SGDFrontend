fb6c1fcda608480e27c5fbbdecd8379ddcac6dd7
sweng66 <sweng@stanford.edu>
2018-02-14 19:56:27 -0800

    working in progress

---
+++ client/jsx/components/patmatch/search_form.jsx
@@ -0,0 +1,269 @@
+"use strict";
+
+var React = require("react");
+var _ = require("underscore");
+var $ = require("jquery");
+
+var Params = require("../mixins/parse_url_params.jsx");
+
+// var RadioSelector = require("./radio_selector.jsx");
+// var BlastBarChart = require("./blast_bar_chart.jsx");
+
+var PATMATCH_URL = "/run_patmatch";
+
+var SearchForm = React.createClass({
+
+	getInitialState: function () {
+	        
+		var param = Params.getParams();
+		
+		var submitted = '';
+		if (param['submit']) {
+		     submitted = 1;
+		}
+
+		this._getConfigData();
+
+		// submitted: submitted,
+		return {
+			isComplete: false,
+			isPending: false,
+			userError: null,
+			configData: {},
+			strain: null,
+			seqtype: null,
+			dataset: null,
+			pattern: null,
+			maxHits: null,
+			strand: null,
+			mismatch: null,
+			deletion: null,
+			insersion: null,
+			substitution: null,
+			resultData: {},
+			param: param,
+			didPatmatch: 0
+		};
+	},
+
+	render: function () {		
+		var formNode = this._getFormNode();
+		return (<div>
+			<span style={{ textAlign: "center" }}><h1>Yeast Genome Pattern Matching <a target="_blank" href="https://sites.google.com/view/yeastgenome-help/analyze-help/pattern-matching?authuser=0"><img src="https://d1x6jdqbvd5dr.cloudfront.net/legacy_img/icon_help_circle_dark.png"></img></a></h1>
+			<hr /></span>
+			{formNode}
+		</div>);
+	},
+
+	componentDidMount: function () {
+	        if (this.state.submitted) {
+	              this._doPatmatch();
+	        }
+	},
+
+	_getFormNode: function () {
+				
+	        if (this.state.isComplete) {
+		        if (this.state.resultData.hits == '') {
+			     var errorReport = this.state.resultData.result;
+			     return (<div dangerouslySetInnerHTML={{ __html: errorReport }} />);
+
+			}
+
+			<p>DISPLAY RESULT TABLE HERE</p>
+
+		} 
+		else if (this.state.isPending) {
+
+		        return (<div>
+			       <div className="row">
+			       	    <p><b>Something wrong with your patmatch search</b></p>
+			       </div>
+			</div>);
+			
+		}
+		else {
+
+		        if (this.state.submitted) {
+			     return <p>Please wait... The search may take a while to run.</p>; 
+
+			}
+			var genomeBoxNode = this._getGenomeBoxNode();
+			var seqtypeNode = this._getSeqtypeNode(); 
+			var patternBoxNode = this._getPatternBoxNode();
+			var datsetNode = this._getDatasetNode();
+                	var submitNode = this._getSubmitNode();
+                	var optionNode = this._getOptionsNode();
+			var patternExampleNode = this._getPatternExampleNote();
+
+			// need to put the date in a config file
+			
+			var descText = "<p>Pattern Matching allows you to search for short (<20 residues) nucleotide or peptide sequences, or ambiguous/degenerate patterns. It uses the same dataset as SGD's BLAST program. If you are searching for a sequence >20 bp or aa with no degenerate positions, please use BLAST, which is much faster. Pattern Matching allows for ambiguous characters, mismatches, insertions and deletions, but does not do alignments and so is not a replacement for <a target='_blank' href='/blast-sgd'>BLAST</a>. Please note, also, that PatMatch will not find overlapping hits.</p><p>Your comments and suggestions are appreciated: <a target='_blank' href='/suggestion'>Send a Message to SGD</a></p>";
+							
+			return (<div>
+			        <div dangerouslySetInnerHTML={{ __html: descText}} />
+				<form onSubmit={this._onSubmit} target="search_result">
+					<div className="row">
+                        		     <div className="large-12 columns">
+                               		     	  { genomeBoxNode }
+						  { seqtypeNode }
+						  { patternBoxNode }
+						  { datasetNode }
+                               			  { submitNode }
+						  { optionNode }
+						  { patternExampleNode }
+                   		             </div>
+                                       </div>
+				</form>
+			</div>);
+		}
+	},
+	
+	_getgetGenomeBoxNode: function() {
+	
+                return(<div><p>GENOME BOX SESSION</p></dev>);
+			      
+	},
+		
+	_getSeqtypeNode: function() {
+
+                var format = ['peptide', 'nucleotide'];
+
+                var _elements = [];
+                format.forEach ( function(f) {
+                     if (f == 'peptide') {
+                          _elements.push(<option value={f} selected="selected">{f}</option>);
+                     }
+                     else {
+                          _elements.push(<option value={f}>{f}</option>);
+                     }
+                });
+
+                return(<div>
+		      <p><h3>Enter a</h3></p>
+		      <p><select ref='seqtype' onChange={this._onChange}>{_elements}</select></p>
+		</div>);
+
+        },
+
+	_getPatternBoxNode: function() {
+
+                return (<div>
+                        <p><h3>sequence or pattern (<a href='#examples'>syntax</a>)</h3></p>
+                        <textfield ref='pattern' onChange={this._onChange} size='50'></textfield><p></p>
+                </div>);
+
+        },
+
+	_getDatasetNode: function() {
+
+	        return(<div><p>DATASET SECTION</p></dev>);
+
+	},
+
+	_getSubmitNode: function() {
+               
+                return(<div>
+                      <p><input type="submit" value="START PATTERN SEARCH" className="button secondary"></input> OR  <input type="reset" value="RESET FORM" className="button secondary"></input>
+		      </p>
+                </div>);
+
+        },
+
+	_getOptionsNode: function() {
+
+	        return(<div><p>OPTIONS SECTION</p></dev>);
+
+	},	
+
+	_getPatternExampleNote: function() {
+
+                return(<div><p>PATTERN EXAMPLES SECTION</p></dev>);
+
+        },
+
+	_getDropdownList: function(elementList, activeVal) {
+		var _elements = [];
+		elementList.forEach ( function(m) {
+                     if (m == activeVal) {
+                     	  _elements.push(<option value={m} selected='selected'>{m}</option>);
+                     }
+                     else {
+                          _elements.push(<option value={m}>{m}</option>);
+                     }
+		});
+       		return _elements;
+	},
+
+        _onChange: function(e) {
+                this.setState({ text: e.target.value});
+        },
+
+	_getConfigData: function() {
+                var jsonUrl = PATMATCH_URL + "?conf=patmatch.conf";
+                $.ajax({
+                      url: jsonUrl,
+                      dataType: 'json',
+                      success: function(data) {
+                            this.setState({configData: data});
+                      }.bind(this),
+                      error: function(xhr, status, err) {
+                            console.error(jsonUrl, status, err.toString());
+                      }.bind(this)
+                });
+        },
+
+	_onSubmit: function (e) {
+
+		var strain = this.refs.strain.value.trim();
+		var seqtype = this.refs.seqtype.value.trim();
+		var pattern = this.refs.pattern.value.trim();
+		var dataset = this.refs.dataset.value.trim();
+		// more here		
+                		
+		if (pattern) {
+		    window.localStorage.clear();
+		    window.localStorage.setItem("seqtype", seqtype);
+		    window.localStorage.setItem("pattern", pattern);
+		    window.localStorage.setItem("dataset", dataset);
+		    // more here
+		}
+		else {
+		    e.preventDefault();
+		    return 1; 
+		}
+
+	},
+
+	_doPatmatch: function() {
+
+		var seqtype = window.localStorage.getItem("seqtype");
+		var pattern = window.localStorage.getItem("pattern");
+		var dataset = window.localStorage.getItem("dataset");
+		// more here
+
+		$.ajax({
+			url: PATMATCH_URL,
+			data_type: 'json',
+			type: 'POST',
+
+			// add more to data: eg, insertion, deletion, ....
+			data: { 'seqtype':     seqtype,
+			        'pattern':     pattern,
+				'dataset':     dataset
+                        },
+			success: function(data) {
+			      this.setState({isComplete: true,
+			                     resultData: data,
+			         	     filter: filter});
+			}.bind(this),
+			error: function(xhr, status, err) {
+			      this.setState({isPending: true}); 
+			}.bind(this) 
+		});
+
+	},
+
+});
+
+module.exports = SearchForm;
---
+++ src/sgd/tools/patmatch/__init__.py
@@ -0,0 +1,90 @@
+import json
+from pyramid.response import Response
+from src.sgd.frontend import config
+from urllib2 import Request, urlopen, URLError
+
+def do_patmatch(request):
+
+    p = dict(request.params)
+
+    if p.get('conf'):
+        data = _get_config(p.get('conf'))
+        return Response(body=json.dumps(data), content_type='application/json')
+    
+    data = _run_patmatch(p)
+
+    return Response(body=json.dumps(data), content_type='application/json')
+
+def _run_patmatch(p):
+
+    from urllib2 import Request, urlopen, URLError
+
+    paramData = _construct_patmatch_parameters(p)
+
+    url = config.patmatch_url + "cgi-bin/aws-patmatch"
+
+    req = Request(url=url, data=paramData)
+
+    res = urlopen(req)
+
+    result = res.read()
+
+    dataSet = result.split("\t")
+    
+    if dataSet[1]:
+        data = { "result": dataSet[0],
+                 "hits":   json.loads(dataSet[1]),
+                 "totalHits": dataSet[2],
+                 "showHits": dataSet[3]}
+    else:
+        data = { "result": dataSet[0],
+                 "hits": "",
+                 "totalHits": 0,
+                 "showHits": 0}
+
+    return data
+
+def _construct_patmatch_parameters(p):
+
+    insertion = p.get('insertion') if p.get('insertion') else 0 
+    deletion = p.get('deletion') if p.get('deletion') else 0
+    substitution = p.get('substitution') p.get('substitution') else 0
+    
+    if strand.startswith('Both'):
+        strand = 'both'
+    elif strand.startswith('Reverse'):
+        strand = 'crick'
+    else:
+        strand = 'watson'
+
+    import urllib
+    
+    paramData = urllib.urlencode({ 'pattern': p.get('pattern'),
+                                   'strain': p.get('strain'),
+                                   'seqtype': p.get('seqtype'),
+                                   'dataset': p.get('dataset'),
+                                   'maxhits': p.get('maxhits'),
+                                   'strand': strand,
+                                   'mismatch': p.get('mismatch'),
+                                   'insertion': insertion,
+                                   'deletion': deletion,
+                                   'substitution': substitution })
+    return paramData
+
+
+def _get_config(conf):
+
+    url = config.patmatch_url + "patmatch/" + conf
+    data = _get_json_from_server(url)
+
+    return data
+
+
+def _get_json_from_server(url):
+
+    req = Request(url)
+    res = urlopen(req)
+    data = json.loads(res.read())
+    return data
+             
+
