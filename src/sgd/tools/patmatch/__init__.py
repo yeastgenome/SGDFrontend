9e146951626b57d8088bd754492d759d34ac4bb8
sweng66 <sweng@stanford.edu>
2018-02-20 14:50:38 -0800

    test

--- src/sgd/tools/patmatch/__init__.py
+++ src/sgd/tools/patmatch/__init__.py
@@ -13,33 +13,36 @@ def do_patmatch(request):
         data = _get_config(p.get('conf'))
         return Response(body=json.dumps(data), content_type='application/json')
         
-    # data = _run_patmatch(p)
-    data = _get_config()
+    data = _run_patmatch(p)
     return Response(body=json.dumps(data), content_type='application/json')
 
 def _run_patmatch(p):
 
-    from urllib2 import Request, urlopen, URLError
-
-    paramData = _construct_patmatch_parameters(p)
+    # paramData = _construct_patmatch_parameters(p)
 
     url = patmatch_url + "cgi-bin/aws-patmatch" 
-    req = Request(url=url, data=paramData)
-    res = urlopen(req)
-    result = res.read()
+   
+    # url = "http://patmatch.dev.yeastgenome.org/cgi-bin/aws-patmatch"
 
-    dataSet = result.split("\t")
+    # req = Request(url=url, data=paramData)
+    req = Request(url)
+    res = urlopen(req)
+    data = json.loads(res.read())
     
-    if dataSet[1]:
-        data = { "result": dataSet[0],
-                 "hits":   json.loads(dataSet[1]) }
-                 # "totalHits": dataSet[2],
-                 # "showHits": dataSet[3]}
-    else:
-        data = { "result": dataSet[0],
-                 "hits": "",
-                 "totalHits": 0,
-                 "showHits": 0}
+    # result = res.read()
+
+    # dataSet = result.split("\t")
+    
+    # if dataSet[1]:
+    #    data = { "result": dataSet[0],
+    #             "hits":   json.loads(dataSet[1]) }
+    #             # "totalHits": dataSet[2],
+    #             # "showHits": dataSet[3]}
+    # else:
+    #    data = { "result": dataSet[0],
+    #             "hits": "",
+    #             "totalHits": 0,
+    #             "showHits": 0}
 
     return data
 
