8a6cdc7d9dcee3e3aad6db1a41644074627ab38e
Travis Sheppard <tshepp@stanford.edu>
2018-02-21 16:40:20 -0800

    pick some changes from original 4446files branch

--- scripts/loading/files/load_filedbentities.py
+++ scripts/loading/files/load_filedbentities.py
@@ -2,8 +2,10 @@ import csv
 import os
 import logging
 from datetime import datetime
+import getpass
+import paramiko
 from src.helpers import upload_file
-from src.models import DBSession, Edam, Filedbentity, FilePath, Path, Referencedbentity, ReferenceFile, Source
+from src.models import DBSession, Edam, Filedbentity, FileKeyword, FilePath, Keyword, Path, Referencedbentity, ReferenceFile, Source
 from sqlalchemy import create_engine, and_
 from sqlalchemy.orm import sessionmaker, scoped_session
 from zope.sqlalchemy import ZopeTransactionExtension
@@ -12,12 +14,14 @@ import traceback
 
 '''
     Process a CSV file of downloads, create filedbentity entries, file_path entries, and 
-    finds the file on a local directory, then uploads to s3 and updates s3_path.
+    finds the file on the remote download server via SSH, then uploads to s3 and updates s3_path.
 
     example
-        $ source dev_variables.sh && CREATED_BY=TSHEPP LOCAL_FILE_DIRECTORY=/Users/travis/Documents/bun_downloads/html INPUT_FILE_NAME=/Users/travis/Desktop/meta_csvs/literature_file_metadata.csv python scripts/loading/files/load_filedbentities.py
+        $ source dev_variables.sh && CREATED_BY=TSHEPP INPUT_FILE_NAME=/Users/travis/Desktop/meta_csvs/literature_file_metadata.csv python scripts/loading/files/load_filedbentities.py
 '''
 
+DATA_DIR = '/data.s3/html'
+HOSTNAME = 'bun.stanford.edu'
 INPUT_FILE_NAME = os.environ.get('INPUT_FILE_NAME')
 LOCAL_FILE_DIRECTORY = os.environ.get('LOCAL_FILE_DIRECTORY')
 NEX2_URI = os.environ.get('NEX2_URI')
@@ -26,13 +30,13 @@ SGD_SOURCE_ID = 834
 
 logging.basicConfig(level=logging.INFO)
 
-def create_and_upload_file(obj, row_num):
+def create_and_upload_file(obj, row_num, sftp_client):
     try:
         # find on local system
-        local_file_path = LOCAL_FILE_DIRECTORY + '/' + obj['bun_path']
+        remote_file_path = DATA_DIR + '/' + obj['bun_path']
         # special transformations
-        local_file_path = local_file_path.replace('feature/', 'features/')
-        local_file = open(local_file_path)
+        remote_file_path = remote_file_path.replace('feature/', 'features/')
+        remote_file = sftp_client.open(remote_file_path)
     except IOError:
         logging.error('error opening file ' + str(row_num))
         traceback.print_exc()
@@ -61,7 +65,7 @@ def create_and_upload_file(obj, row_num):
             except TypeError:
                 logging.error('invalid EDAM id or source in row ' + str(row_num) + ' val in ' + obj['data_edam_id'] + ', ' + obj['format_edam_id'] + ', ' + obj['topic_edam_id'])
                 return
-            upload_file(CREATED_BY, local_file,
+            upload_file(CREATED_BY, remote_file,
                 filename=obj['display_name'],
                 file_extension=obj['file_extension'],
                 description=obj['description'],
@@ -79,21 +83,29 @@ def create_and_upload_file(obj, row_num):
             )
             db_session.flush()
         else:
+            existing.display_name = obj['display_name']
             existing.description = obj['description']
             existing.status = obj['status']
             existing.is_public = obj['is_public']
             existing.is_in_spell = obj['is_in_spell']
             existing.is_in_browser = obj['is_in_browser']
             existing.source_id = source_id
+            # update file size
+            if not existing.file_size and existing.s3_url:
+                remote_file.seek(0, os.SEEK_END)
+                file_size = remote_file.tell()
+                remote_file.seek(0)
+                existing.file_size = file_size
             if obj['file_date']:
                 existing.file_date = obj['file_date']
                 existing.year = obj['file_date'].year
             existing.readme_file_id = readme_file_id
+            remote_file.seek(0, os.SEEK_END)
             transaction.commit()
             existing = db_session.query(Filedbentity).filter(Filedbentity.display_name == obj['display_name']).one_or_none()
             # only upload s3 file if not defined
             if existing.s3_url is None:
-                existing.upload_file_to_s3(local_file, obj['display_name'])
+                existing.upload_file_to_s3(remote_file, obj['display_name'])
             db_session.flush()
         # add path entries
         existing = db_session.query(Filedbentity).filter(Filedbentity.display_name == obj['display_name']).one_or_none()
@@ -123,6 +135,20 @@ def create_and_upload_file(obj, row_num):
                     db_session.add(new_ref_file)
                 transaction.commit()
                 db_session.flush()
+        # maybe add keywords
+        if len(obj['keywords']):
+            existing = db_session.query(Filedbentity).filter(Filedbentity.display_name == obj['display_name']).one_or_none()
+            keywords = obj['keywords'].split('|')
+            for x in keywords:
+                x = x.strip()
+                keyword = db_session.query(Keyword).filter(Keyword.display_name==x).one_or_none()
+                existing_file_keyword = db_session.query(FileKeyword).filter(and_(FileKeyword.file_id==existing.dbentity_id, FileKeyword.keyword_id==keyword.keyword_id)).one_or_none()
+                if not existing_file_keyword:
+                    new_file_keyword = FileKeyword(created_by=CREATED_BY, file_id=existing.dbentity_id, keyword_id=keyword.keyword_id, source_id=SGD_SOURCE_ID)
+                    db_session.add(new_file_keyword)
+                transaction.commit()
+                db_session.flush()
+        remote_file.close()
         logging.info('finished ' + obj['display_name'] + ', line ' + str(row_num))
     except:
         logging.error('error with ' + obj['display_name']+ ' in row ' + str(row_num))
@@ -133,6 +159,14 @@ def create_and_upload_file(obj, row_num):
 def load_csv_filedbentities():
     engine = create_engine(NEX2_URI, pool_recycle=3600)
     DBSession.configure(bind=engine)
+
+    # open ssh connection to download server
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    username = raw_input('Username for legacy download server: ')
+    password =  getpass.getpass('Password for %s@%s: ' % (username, HOSTNAME))
+    client.connect(HOSTNAME, 22, username, password, gss_auth=False, gss_kex=False)
+    sftp_client = client.open_sftp()
 
     o = open(INPUT_FILE_NAME,'rU')
     reader = csv.reader(o)
@@ -146,25 +180,37 @@ def load_csv_filedbentities():
                 raw_date = datetime.strptime(val[13], '%Y-%m-%d')
             else:
                 raw_date = None
+            raw_status = val[4].strip()
+            if raw_status == 'Archive':
+                raw_status = 'Archived'
+
+            bun_path = val[0].strip()
+            new_path = val[1].strip()
+            if bun_path[0] != '/':
+                bun_path = bun_path.replace('genome-sequences/', '/genome-sequences/')
+            if new_path[0] != '/':
+                new_path = new_path.replace('genome-sequences/', '/genome-sequences/')
             obj = {
-                'bun_path': val[0],
-                'new_path': val[1],
-                'display_name': val[3],
-                'status': val[4].replace('Archive', 'Archived'),
-                'source': val[5],
-                'topic_edam_id': val[7].upper().replace('TOPIC', 'EDAM'),
-                'data_edam_id': val[9].upper().replace('DATA', 'EDAM'),
-                'format_edam_id': val[11].upper().replace('FORMAT', 'EDAM'),
-                'file_extension': val[12],
+                'bun_path': bun_path,
+                'new_path': new_path,
+                'display_name': val[3].strip(),
+                'status': raw_status,
+                'source': val[5].strip(),
+                'topic_edam_id': val[7].upper().replace('TOPIC', 'EDAM').strip(),
+                'data_edam_id': val[9].upper().replace('DATA', 'EDAM').strip(),
+                'format_edam_id': val[11].upper().replace('FORMAT', 'EDAM').strip(),
+                'file_extension': val[12].strip(),
                 'file_date': raw_date,
                 'is_public': (val[15] == '1'),
                 'is_in_spell': (val[16] == '1'),
                 'is_in_browser': (val[17] == '1'),
                 'readme_name': val[18],
-                'description': val[19],
-                'pmids': val[20]
+                'description': val[19].decode('utf-8', 'ignore'),
+                'pmids': val[20],
+                'keywords': val[21]
             }
-            create_and_upload_file(obj, i)
+            create_and_upload_file(obj, i, sftp_client)
+    client.close()
 
 if __name__ == '__main__':
     load_csv_filedbentities()
