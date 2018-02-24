71cc57db395ae00015eb629bc4a9c006880d2a68
sweng66 <sweng@stanford.edu>
2018-02-23 23:45:31 -0800

    test

--- client/jsx/components/patmatch/example_table.jsx
+++ client/jsx/components/patmatch/example_table.jsx
@@ -18,21 +18,20 @@ module.exports = {
 		rows.push(["Peptide Searches", "O", "Any hydrophilic residue (TSHEDQNKR)", <span><a href='/nph-patmatch?pattern=GLFGO'>GLFGO</a></span>]);
 		rows.push(["Peptide Searches", "B", "D or N", <span><a href='/nph-patmatch?pattern=FLGB'>FLGB</a></span>]);
 	        rows.push(["Peptide Searches", "Z", "E or Q", <span><a href='/nph-patmatch?pattern=GLFGZ'>GLFGZ</a></span>]);
-
 		rows.push(["Peptide Searches", "X or .", "Any amino acid", <span><a href='/nph-patmatch?pattern=DTXXDN..RQS'>DTXXDN..RQS</a></span>]);
-
 		rows.push(["Nucleotide Searches", "ACTGU", "Exact match", <span><a href='/nph-patmatch?seqtype=nuc&pattern=ACGGCGTA'>ACGGCGTA</a></span>]);
 		rows.push(["Nucleotide Searches", "R", "Any purine base (AG)", <span><a href='/nph-patmatch?seqtype=nuc&pattern=AATTTGGRGGR'>AATTTGGRGGR</a></span>]);
 		rows.push(["Nucleotide Searches", "Y", "Any pyrimidine base (CT)", <span><a href='/nph-patmatch?seqtype=nuc&pattern=CCCATAYYGGYY'>CCCATAYYGGYY</a></span>]);
-
-		rows.push(["Nucleotide Searches", "S", "G or C", <span><a href='/nph-patmatch?seqtype=nuc&pattern=YGGTWCAMWTGTY'>YGGTWCAMWTGTY</a></span>]);
+		rows.push(["Nucleotide Searches", "S", "G or C", <span><a href='/nph-patmatch?seqtype=nuc&pattern=YGSTWCAMWTGTY'>YGSTWCAMWTGTY</a></span>]);
 		rows.push(["Nucleotide Searches", "W", "A or T", <span><a href='/nph-patmatch?seqtype=nuc&pattern=YGGTWCAMWTGTY'>YGGTWCAMWTGTY</a></span>]);
 		rows.push(["Nucleotide Searches", "M", "A or C", <span><a href='/nph-patmatch?seqtype=nuc&pattern=YGGTWCAMWTGTY'>YGGTWCAMWTGTY</a></span>]);
-		rows.push(["Nucleotide Searches", "K", "G or T", <span><a href='/nph-patmatch?seqtype=nuc&pattern=YGGTWCAMWTGTY'>YGGTWCAMWTGTY</a></span>]);
-		rows.push(["Nucleotide Searches", "V", "A or C or G", <span><a href='/nph-patmatch?seqtype=nuc&pattern=CGG...WH.{3,5}HW...CCG'>CGG...WH.{3,5}HW...CCG</a></span>]);
-		rows.push(["Nucleotide Searches", "H", "A or C or T", <span><a href='/nph-patmatch?seqtype=nuc&pattern=CGG...WH.{3,5}HW...CCG'>CGG...WH.{3,5}HW...CCG</a></span>]);
-		rows.push(["Nucleotide Searches", "D", "A or G or T", <span><a href='/nph-patmatch?seqtype=nuc&pattern=CGG...WH.{3,5}HW...CCG'>CGG...WH.{3,5}HW...CCG</a></span>]);		
-		rows.push(["Nucleotide Searches", "B", "C or G or T", <span><a href='/nph-patmatch?seqtype=nuc&pattern=CGG...WH.{3,5}HW...CCG'>CGG...WH.{3,5}HW...CCG</a></span>]);
+		rows.push(["Nucleotide Searches", "K", "G or T", <span><a href='/nph-patmatch?seqtype=nuc&pattern=YGGTWCAMWTKTY'>YGGTWCAMWTKTY</a></span>]);
+
+		rows.push(["Nucleotide Searches", "V", "A or C or G", <span><a href='/nph-patmatch?seqtype=nuc&pattern=CGV...WH.{3,5}HW...CCG'>CGV...WH.{3,5}HW...CCG</a></span>]);
+		rows.push(["Nucleotide Searches", "H", "A or C or T", <span><a href='/nph-patmatch?seqtype=nuc&pattern=CGH...WH.{3,5}HW...CCG'>CGH...WH.{3,5}HW...CCG</a></span>]);
+		rows.push(["Nucleotide Searches", "D", "A or G or T", <span><a href='/nph-patmatch?seqtype=nuc&pattern=CGD...WH.{3,5}HW...CCG'>CGD...WH.{3,5}HW...CCG</a></span>]);		
+		rows.push(["Nucleotide Searches", "B", "C or G or T", <span><a href='/nph-patmatch?seqtype=nuc&pattern=CGB...WH.{3,5}HW...CCG'>CGB...WH.{3,5}HW...CCG</a></span>]);
+
 		rows.push(["Nucleotide Searches", "N or X or .", "Any base", <span><a href='/nph-patmatch?seqtype=nuc&pattern=ATGCNNNNNATCG'>ATGCNNNNNATCG</a></span>]);
 		rows.push(["All Searches", "[]", "A subset of elements", <span><a href='/nph-patmatch?seqtype=pep&pattern=[WFY]XXXDN[RK][ST]'>[WFY]XXXDN[RK][ST]</a></span>]);
 		rows.push(["All Searches", "[^]", "An excluded subset of elements", <span><a href='/nph-patmatch?seqtype=pep&pattern=NDBB...[VILM]Z[DE]...[^PG]'>NDBB...[VILM]Z[DE]...[^PG]</a></span>]);
