f4614e93c8aec9c534c52d86155c894ac97b4494
sweng66 <sweng@stanford.edu>
2018-02-25 16:25:38 -0800

    test

--- client/jsx/components/patmatch/search_form.jsx
+++ client/jsx/components/patmatch/search_form.jsx
@@ -87,7 +87,7 @@ var SearchForm = React.createClass({
                         var seq = this.state.resultData.seq;
 			var text = this.state.resultData.defline;
 			
-			innerNode = (<SequenceDisplay sequence={seq} text={text} />);
+			var innerNode = (<SequenceDisplay sequence={seq} text={text} />);
 				 
 			return <div className="panel sgd-viz">{innerNode}</div>;
 
