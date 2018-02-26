4d6f83ffe7d7532b596dc823d13e26695f5225bc
felixgondwe <gondwefelix8@gmail.com>
2018-02-26 11:40:34 -0800

    tree css

---
+++ client/jsx/components/widgets/tree_menu/menu_list.jsx
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+class MenuList extends Component{
+
+    render(){
+        return (<ul className="mtree sgd">
+            <li className="mtree-node mtree-open">
+              <a href="#">Africa</a>
+              <ul>
+                <li className="mtree-node mtree-open">
+                  <a href="#">Malawi</a>
+                </li>
+                <li className="mtree-node mtree-open">
+                  <a href="#">Zambia</a>
+                </li>
+                <li className="mtree-node mtree-open">
+                  <a href="#">Kenya</a>
+                </li>
+              </ul>
+            </li>
+            <li className="mtree-node mtree-open">
+              <a href="#">America</a>
+              <ul>
+                <li className="mtree-node mtree-open">
+                  <a href="#">North America</a>
+                  <ul>
+                    <li className="mtree-node mtree-open">
+                      <a href="#">USA</a>
+                    </li>
+                    <li className="mtree-node mtree-open">
+                      <a href="#">Canada</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>);
+    }
+}
+
+export default MenuList;
