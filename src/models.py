171d1f65ae264ebe1eee85eb29eb5ee9439c3bce
Travis Sheppard <tshepp@stanford.edu>
2018-03-14 13:51:59 -0700

    read publication status from form

--- src/models.py
+++ src/models.py
@@ -8403,7 +8403,7 @@ class ReservednameTriage(Base):
                     subclass = 'REFERENCE',
                     dbentity_status = 'Active',
                     method_obtained = 'Gene registry',
-                    publication_status = 'Unpublished',
+                    publication_status = obj['publication_status'],
                     fulltext_status = 'NAP',
                     citation = citation,
                     year = int(obj['year']),
