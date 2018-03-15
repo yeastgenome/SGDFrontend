50d7c22b48e1f74d39dfb5ca10d68a036b584aac
Travis Sheppard <tshepp@stanford.edu>
2018-03-15 15:25:34 -0700

    show warning for adding alias without PMID

--- src/js_src/containers/locus/basic.js
+++ src/js_src/containers/locus/basic.js
@@ -1,3 +1,4 @@
+/*eslint-disable no-undef */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import t from 'tcomb-form';
@@ -9,11 +10,11 @@ import { updateData } from './locusActions';
 
 const DESCRIPTION_LENGTH = 500;
 const HEADLINE_LENGTH = 70;
+const ALIAS_WARNING_ID = 'sgdAliasWarning';
 
 class LocusBasic extends Component {
-  // give some feedback for changes to description, such as updated headline
+  // sidesteps react give some feedback while changing form inputs
   handleChange(data) {
-    // TODO properly get headline and reflect
     let description = data['description'];
     if (description.length > DESCRIPTION_LENGTH) {
       this.props.dispatch(setError(`Description cannot be greater than ${DESCRIPTION_LENGTH} characters.`));
@@ -22,11 +23,31 @@ class LocusBasic extends Component {
     headline = headline.slice(0, headline.indexOf(';'));
     let el = document.getElementsByClassName('field-headline')[0];
     el.innerHTML = `<label>Headline</label>${headline}`;
+    // manually hide/show warning for 
+    for (var i = data.aliases.length - 1; i >= 0; i--) {
+      let d = data.aliases[i];
+      if (!d) continue;
+      let pmids = d.pmids;
+      if (!pmids) {
+        $(`#${ALIAS_WARNING_ID}`).css('display', 'block');
+        return;
+      }
+    }
+    $(`#${ALIAS_WARNING_ID}`).css('display', 'none');
   }
 
   handleSuccess(data) {
     this.props.dispatch(updateData(data));
     this.props.dispatch(setMessage('Locus updated.'));
+  }
+
+  // dislay a hidden warning about having alias without PMIDS, manually show when there is no PMID for an alias to avoid setting state, re-rendering form
+  renderAliasWarning() {
+    return (
+      <div className='callout warning' id={ALIAS_WARNING_ID} style={{ display: 'none' }}>
+        <p><i className='fa fa-exclamation-circle' /> You entered an alias without a PMID</p>
+      </div>
+    );
   }
 
   render() {
@@ -62,6 +83,7 @@ class LocusBasic extends Component {
     let aliasLayout = locals => {
       return (
         <div className='row'>
+          {this.renderAliasWarning()}
           <div className='columns small-2'>{locals.inputs.alias}</div>
           <div className='columns small-3'>{locals.inputs.type}</div>
           <div className='columns small-5'>{locals.inputs.pmids}</div>
