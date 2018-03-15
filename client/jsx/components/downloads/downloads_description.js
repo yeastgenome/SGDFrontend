53d68e7f2256c068fbfa01f6906eae76905eca5d
felixgondwe <gondwefelix8@gmail.com>
2018-03-15 00:50:40 -0700

    added download files end point to UI and other bug fixes

---
+++ client/jsx/components/downloads/downloads_description.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Component} from 'react';
+
+class DownloadsDescription extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (<p>{this.props.title} - {this.props.description}</p>);
+    }
+}
+
+export default DownloadsDescription;
