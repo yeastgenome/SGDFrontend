6ab88a4fca4f921faac4b4f17b3f0112db9563b0
Travis Sheppard <tshepp@stanford.edu>
2018-04-26 10:11:04 -0700

    TEMP disable paused CIs

--- src/js_src/routes.js
+++ src/js_src/routes.js
@@ -7,16 +7,16 @@ import PublicHome from './containers/publicHome';
 import Help from './containers/help';
 import GoogleLogin from './containers/googleLogin';
 import PublicLayout from './containers/layout/PublicLayout';
-import NewGeneNameReservation from './containers/reserve/new';
+// import NewGeneNameReservation from './containers/reserve/new';
 // authenticated curate inputs
 import { requireAuthentication } from './containers/authenticateComponent';
 // import CurateLayout from './containers/curateHome/layout';
 import CurateHome from './containers/curateHome';
 import Search from './containers/search';
-import LocusLayout from './containers/locus/layout';
-import LocusBasic from './containers/locus/basic';
-import LocusName from './containers/locus/name';
-import LocusSummaries from './containers/locus/summaries';
+// import LocusLayout from './containers/locus/layout';
+// import LocusBasic from './containers/locus/basic';
+// import LocusName from './containers/locus/name';
+// import LocusSummaries from './containers/locus/summaries';
 import TriageIndex from './containers/triage';
 import SpreadsheetUpload from './containers/spreadsheetUpload/index';
 import Settings from './containers/settings/index';
@@ -27,12 +27,12 @@ import NewReference from './containers/newReference';
 import CurateLit from './containers/curateLit/layout';
 import CurateLitBasic from './containers/curateLit/basic';
 import CurateLitPhenotype from './containers/curateLit/phenotype';
-import GeneNameReservationIndex from './containers/reserve/index';
-import GeneNameReservation from './containers/reserve/show';
-import GeneNameReservationEdit from './containers/reserve/edit';
-import GeneNameReservationStandardize from './containers/reserve/standardize';
-import ColleaguesIndex from './containers/colleagues/index';
-import ColleaguesShow from './containers/colleagues/show';
+// import GeneNameReservationIndex from './containers/reserve/index';
+// import GeneNameReservation from './containers/reserve/show';
+// import GeneNameReservationEdit from './containers/reserve/edit';
+// import GeneNameReservationStandardize from './containers/reserve/standardize';
+// import ColleaguesIndex from './containers/colleagues/index';
+// import ColleaguesShow from './containers/colleagues/show';
 // import AuthorResponse from './containers/authorResponse/index';
 import NewColleague from './containers/colleagues/new';
 // import EditColleague from './containers/colleagues/edit';
@@ -43,24 +43,25 @@ export default (
     <Route component={Layout} path='/'>
       <IndexRoute component={requireAuthentication(CurateHome)} />
       <Route component={requireAuthentication(TriageIndex)} path='triage' />
-      <Route component={requireAuthentication(GeneNameReservationIndex)} path='reservations' />
+{/*      <Route component={requireAuthentication(GeneNameReservationIndex)} path='reservations' />
       <Route component={requireAuthentication(GeneNameReservation)} path='reservations/:id' />
       <Route component={requireAuthentication(GeneNameReservationEdit)} path='reservations/:id/edit' />
       <Route component={requireAuthentication(GeneNameReservationStandardize)} path='reservations/:id/standardize' />
       <Route component={requireAuthentication(ColleaguesIndex)} path='colleagues/triage' />
       <Route component={requireAuthentication(ColleaguesShow)} path='colleagues/triage/:id' />
+*/}   
       <Route component={requireAuthentication(SpreadsheetUpload)} path='spreadsheet_upload' />
       <Route component={requireAuthentication(Settings)} path='settings' />
       <Route component={Help} path='help' />
       <Route component={requireAuthentication(Search)} path='search' />
       <Route component={PublicHome} path='login' />
       <Route component={GoogleLogin} path='google_login' />
-      <Route component={requireAuthentication(LocusLayout)} path='curate/locus/:id'>
+{/*      <Route component={requireAuthentication(LocusLayout)} path='curate/locus/:id'>
         <IndexRoute component={requireAuthentication(LocusBasic)} />
         <Route component={requireAuthentication(LocusName)} path='gene_name' />
         <Route component={requireAuthentication(LocusSummaries)} path='summaries' />
       </Route>
-      <Route component={requireAuthentication(NewReference)} path='curate/reference/new' />
+*/}      <Route component={requireAuthentication(NewReference)} path='curate/reference/new' />
       <Route component={requireAuthentication(CurateLit)} path='curate/reference/:id'>
         <IndexRoute component={requireAuthentication(CurateLitBasic)} />
         <Route component={requireAuthentication(Blank)} path='protein' />
@@ -75,7 +76,7 @@ export default (
       {/*<Route component={AuthorResponse} path='author_response' />*/}
       <Route component={NewColleague} path='new_colleague' />
       {/*<Route component={EditColleague} path='colleague/:formatName/edit' />*/}
-      <Route component={NewGeneNameReservation} path='new_reservation' />
+      {/*<Route component={NewGeneNameReservation} path='new_reservation' />*/}
     </Route>
     <Route component={NotFound} path='*' />
   </Route>
