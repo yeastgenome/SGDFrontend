5c7d573c0b50634c1f7ae8701f5e931c3126a980
sweng66 <sweng@stanford.edu>
2018-05-06 17:46:08 -0700

    testing

--- client/jsx/components/seqtools/search_form.jsx
+++ client/jsx/components/seqtools/search_form.jsx
@@ -223,9 +223,14 @@ var SearchForm = React.createClass({
 		    } 
 		
 	        });
-		
 		rows.push(seqDLRow);
 		
+		var seqAnalRow = [<span style={{ fontSize: 20}}>Sequence Analysis</span>];
+		_.map(genes, gene => {
+		    seqAnalRow.push(this._getStrainPulldown(strains));
+		});		
+
+
 		return this._display_gene_table(headerRow, rows);		
 		
 	},
@@ -533,20 +538,48 @@ var SearchForm = React.createClass({
 
         },
 
+	_getStrainMapping: function() {
+			   
+                return { 'S288C':      'S. cerevisiae Reference Strain S288C',
+                         'CEN.PK':     'S. cerevisiae Strain CEN.PK2-1Ca_JRIV01000000',
+                         'D273-10B':   'S. cerevisiae Strain D273-10B_JRIY00000000',
+                         'FL100':      'S. cerevisiae Strain FL100_JRIT00000000',
+                         'JK9-3d':     'S. cerevisiae Strain JK9-3d_JRIZ00000000',
+                         'RM11-1a':    'S. cerevisiae Strain RM11-1A_JRIP00000000',
+                         'SEY6210':    'S. cerevisiae Strain SEY6210_JRIW00000000',
+                         'Sigma1278b': 'S. cerevisiae Strain Sigma1278b-10560-6B_JRIQ00000000',
+                         'W303':       'S. cerevisiae Strain W303_JRIU00000000',
+                         'X2180-1A':   'S. cerevisiae Strain X2180-1A_JRIX00000000',
+                         'Y55':        'S. cerevisiae Strain Y55_JRIF00000000'
+                }; 
+	
+	},
+
+	_getStrainPulldown: function(strains) {
+
+		var strainMapping = this._getStrainMapping();
+		var strain2label = {};
+		var strainList = strains.split("|");
+		var _elements = _.map(strainsList, s => {
+		      var label = strainMapping[s]; 
+		      if (s == 'S288C') {
+		      	    return <option value={s} selected='selected'>{label}</option>;
+	              }
+		      else {
+		      	    return <option value={s}>{label}</option>;
+	              }
+		});
+		
+		return(<div>
+                       <p>Select a strain:
+                       <select ref='strain' name='strain' id='strain' onChange={this._onChange}>{_elements}</select></p>
+                </div>);
+
+	},
+
 	_getStrainNode: function() {
 
-                var strain2label = { 'S288C':      'S. cerevisiae Reference Strain S288C',
-		    		     'CEN.PK':     'S. cerevisiae Strain CEN.PK2-1Ca_JRIV01000000',
-				     'D273-10B':   'S. cerevisiae Strain D273-10B_JRIY00000000', 
-				     'FL100':      'S. cerevisiae Strain FL100_JRIT00000000',
-				     'JK9-3d':     'S. cerevisiae Strain JK9-3d_JRIZ00000000',
-				     'RM11-1a':    'S. cerevisiae Strain RM11-1A_JRIP00000000',
-				     'SEY6210':    'S. cerevisiae Strain SEY6210_JRIW00000000', 
-				     'Sigma1278b': 'S. cerevisiae Strain Sigma1278b-10560-6B_JRIQ00000000', 
-				     'W303':       'S. cerevisiae Strain W303_JRIU00000000', 
-				     'X2180-1A':   'S. cerevisiae Strain X2180-1A_JRIX00000000', 
-				     'Y55':	   'S. cerevisiae Strain Y55_JRIF00000000' 
-		    		   };
+	        var strain2label = this._getStrainMapping();
 
 		var strains = Object.keys(strain2label)
 
