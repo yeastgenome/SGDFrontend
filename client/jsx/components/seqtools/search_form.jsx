35abaf3b28eeb7d22234bffaae509f113fe3e14d
sweng66 <sweng@stanford.edu>
2018-05-10 13:40:00 -0700

    testing

--- client/jsx/components/seqtools/search_form.jsx
+++ client/jsx/components/seqtools/search_form.jsx
@@ -260,9 +260,9 @@ const SearchForm = React.createClass({
 	_getToolsLinks: function(gene, strains) {
 
 		var strainPulldown = this._getStrainPulldown(strains);
-		var blastButton = this._getToolButton(gene, '/blast-sgd', 'BLAST');
-		var fungalBlastButton = this._getToolButton(gene, '/blast-fungal', 'Fungal BLAST');		
-		var primerButton = this._getToolButton(gene, '/primer3', 'Design Primers');
+		var blastButton = this._getToolButton(gene, '/blast-sgd', 'BLAST         ');
+		var fungalBlastButton = this._getToolButton(gene, '/blast-fungal', 'Fungal BLAST    ');		
+		var primerButton = this._getToolButton(gene, '/primer3', 'Design Primers       ');
 		var restrictionButton = this._getToolButton(gene, '/cgi-bin/PATMATCH/RestrictionMapper', 'Genome Restriction Map');
 		return(<div className="row">
                             <div className="large-12 columns">	
