1912d17b709d94c94b7a0724d401bea8a22dc330
felixgondwe <gondwefelix8@gmail.com>
2018-05-09 19:40:22 -0700

    es-bug-fix

--- scripts/search/es_mapping.py
+++ scripts/search/es_mapping.py
@@ -81,24 +81,7 @@ es_mapping = {
                         }
                     }
                 },
-                "phenotype_loci_list": {
-                    "type": "string",
-                    "fields": {
-                        "raw": {
-                            "type": "string",
-                            "index": "not_analyzed"
-                        }
-                    }
-                },
-                "pheno": {
-                    "type": "string",
-                    "fields": {
-                        "raw": {
-                            "type": "string",
-                            "index": "not_analyzed"
-                        }
-                    }
-                },
+                
                 "keys": {
                     "type": "string"
                 },
