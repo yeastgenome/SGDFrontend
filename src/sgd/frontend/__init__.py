050385e056803c559780d685788bf9d4926b104a
sweng66 <sweng@stanford.edu>
2018-05-14 16:07:33 -0700

    testing

--- src/sgd/frontend/__init__.py
+++ src/sgd/frontend/__init__.py
@@ -20,7 +20,6 @@ def prep_views(chosen_frontend, config):
     config.add_route('blast_sgd', '/blast-sgd')
     config.add_route('patmatch', '/nph-patmatch')
     config.add_route('seq_tools', '/seqTools')
-    config.add_route('get_seq', '/getSeq')
     config.add_route('blog_post', '/blog/{slug}')
     config.add_route('blog_index', '/blog')
     config.add_route('blog_archive', '/blog/archive/{year}')
