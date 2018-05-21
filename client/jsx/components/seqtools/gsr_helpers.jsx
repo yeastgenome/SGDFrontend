b7f4f5ecf5082c296c3e9ade98ec3de02844ab03
sweng66 <sweng@stanford.edu>
2018-05-20 23:00:11 -0700

    testing

--- client/jsx/components/seqtools/gsr_helpers.jsx
+++ client/jsx/components/seqtools/gsr_helpers.jsx
@@ -20,7 +20,7 @@ module.exports = {
                 return  { 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI',
                           7: 'VII', 8: 'VIII', 9: 'IX', 10: 'X', 11: 'XI',
                           12: 'XII', 13: 'XIII', 14: 'XIV', 15: 'XV', 16: 'XVI',
-                          '17': 'Mito' 
+                          17: 'Mito' 
 	        };
 
        },
