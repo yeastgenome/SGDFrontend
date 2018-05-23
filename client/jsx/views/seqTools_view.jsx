c853074e17ade5f9133fa48d989bcf947e993441
sweng66 <sweng@stanford.edu>
2018-05-23 09:37:52 -0700

    ready for merging to QA

---
+++ client/jsx/views/seqTools_view.jsx
@@ -0,0 +1,13 @@
+
+"use strict";
+
+const React = require("react");
+const SearchForm = require("../components/seqtools/search_form.jsx");
+
+var seqToolsView = {};
+
+seqToolsView.render = function () {
+	React.render(<SearchForm />,  document.getElementById("j-main"));
+};
+
+module.exports = seqToolsView;
