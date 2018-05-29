261500e05f38b50b71ff409ee2021a3aa5f88890
Kalpana Karra <kkarra@stanford.edu>
2018-05-29 14:36:42 -0700

    save

--- client/jsx/components/primer3/primer3.jsx
+++ client/jsx/components/primer3/primer3.jsx
@@ -44,15 +44,6 @@ const Primer3 = React.createClass({
         console.log(geneName)
         this.setState({value: tempVal});
     }
-    if(this.props.queryParams.sequence_id){
-       let seqId = this.props.queryParams.sequence_id;
-       console.log(seqId)
-       let seq = window.localStorage.getItem(seqId);
-       console.log(seq)
-       let tempVal = this.state.value;
-       tempVal.sequence = seq;
-       this.setState({value: tempVal});
-    }
   },
 
   onChange(value) {
@@ -195,6 +186,16 @@ const Primer3 = React.createClass({
   },
 
   renderForm() {
+
+    if(this.props.queryParams.sequence_id){
+       let seqId = this.props.queryParams.sequence_id;
+       console.log(seqId)
+       let seq = window.localStorage.getItem(seqId);
+       console.log(seq)
+       let tempVal = this.state.value;
+       tempVal.sequence = seq;
+       this.setState({value: tempVal});
+    }
 
     const Endpoint = t.enums.of(['NO', 'YES'], 'Endpoint');
 
