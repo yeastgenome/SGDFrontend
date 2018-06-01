70cda485df0d0b362b6cb562352f5e4719e24a42
sweng66 <sweng@stanford.edu>
2018-06-01 16:09:39 -0700

    in progress

--- client/jsx/components/gotools/gotermfinder_form.jsx
+++ client/jsx/components/gotools/gotermfinder_form.jsx
@@ -194,7 +194,7 @@ const GoTermFinder = React.createClass({
 
 	getPvalueList() {
 		
-		var _elements = [<option value='0.01' selected='selected'><0.01</option>)>];
+		var _elements = [<option value='0.01' selected='selected'><0.01</option>];
  		_elements.push(<option value='0.05' selected='selected'><0.05</option>);
 		_elements.push(<option value='0.1' selected='selected'><0.1</option>);
 	
