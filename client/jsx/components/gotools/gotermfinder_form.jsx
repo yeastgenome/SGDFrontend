dbe3e868ea7ec34f71b351eead4be7089b14cb05
sweng66 <sweng@stanford.edu>
2018-06-12 12:27:48 -0700

    in progress

--- client/jsx/components/gotools/gotermfinder_form.jsx
+++ client/jsx/components/gotools/gotermfinder_form.jsx
@@ -233,6 +233,9 @@ const GoTermFinder = React.createClass({
 		if (genes == '') {
 		     return '';
 		}	 	        
+		console.log("genes before="+genes);
+		genes = genes.replace(/%0D%0A/g, ' ');
+		console.log("genes after="+genes);
                 genes = genes.replace(/[^A-Za-z:\-0-9]/g, ' ');
                 var re = /\+/g;
                 genes = genes.replace(re, " ");
