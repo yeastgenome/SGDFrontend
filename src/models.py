3b1e59206e5d7134de892e8c12b8b5194163151c
sweng66 <sweng@stanford.edu>
2018-07-13 00:01:50 -0700

    testing

--- src/models.py
+++ src/models.py
@@ -7738,10 +7738,9 @@ class Complexdbentity(Dbentity):
         if ref_objs:
             for cr in ref_objs:
                 reference = cr.reference
-                refs.append({ "pmid": reference.pmid })
-
-                #              "year", reference.year,
-                #              "citation": reference.citation })
+                refs.append({ "pmid": reference.pmid,
+                              "year", reference.year,
+                              "citation": reference.citation })
 
         data['reference'] = refs
 
