96ea079833c9f94ec9f13f028333cdfb766a03f4
Kalpana Karra <kkarra@stanford.edu>
2018-07-17 14:46:34 -0700

    mock helper stuff

--- src/models.py
+++ src/models.py
@@ -4782,6 +4782,8 @@ class Diseaseannotation(Base):
     taxonomy = relationship(u'Taxonomy')
 
     def to_dict_lsp(self):
+        import pdb
+        pdb.set_trace()
         obj = {
             "annotation_type": self.annotation_type,
             "qualifiers": [self.disease_qualifier],
