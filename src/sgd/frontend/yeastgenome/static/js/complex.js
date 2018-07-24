6f1aee478a537a2a1a7315e52e52bcf0dec6da56
sweng66 <sweng@stanford.edu>
2018-07-23 22:47:32 -0700

    in progress

--- src/sgd/frontend/yeastgenome/static/js/complex.js
+++ src/sgd/frontend/yeastgenome/static/js/complex.js
@@ -12,7 +12,6 @@ function create_complex_table(data) {
     var datatable = [];
     var subunits = {};
     for (var i = 0; i < evidence.length; i++) {
-	console.log("evidence="+evidence[i]['display_name']);
 	datatable.push(complex_subunit_data_to_table(evidence[i]));
 	subunits[evidence[i]["display_name"]] = true;
     }
@@ -41,5 +40,6 @@ function create_complex_table(data) {
   };
 
   return create_table("complex_table", options);
+
 }
 
