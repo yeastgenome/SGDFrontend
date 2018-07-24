2d9c87036a35adb00bb7a5da12b408d289cc9fc9
sweng66 <sweng@stanford.edu>
2018-07-23 21:36:42 -0700

    in progress

---
+++ src/sgd/frontend/yeastgenome/static/js/complex.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function() {
+
+	$.getJSON('/backend/complex/' + complex['id'], function(data) {
+	  	var complex_table = create_complex_table(data);
+	});
+
+});
+
+function create_complex_table(data) {
+    var evidence = data['subunit'];
+    var datatable = [];
+    var subunits = {};
+    for (var i = 0; i < evidence.length; i++) {
+	datatable.push(complex_subunit_data_to_table(data[i]));
+	subunits[evidence[i]["display_name"]] = true;
+    }
+
+    set_up_header(
+		  "complex_table",
+		  datatable.length,
+		  "entry",
+		  "entries",
+		  Object.keys(subunits).length,
+		  "subunit",
+		  "subunits"
+		  );
+
+    var options = {};
+    options["bPaginate"] = false;
+    options["aaSorting"] = true;
+    options["bDestroy"] = true;
+    options["aoColumns"] = [
+			  null,
+			  null,
+			  null
+			  ];
+  options["aaData"] = datatable;
+  options["oLanguage"] = {
+      sEmptyTable: "No subunits for this complex???."
+  };
+
+  return create_table("complex_table", options);
+}
+
