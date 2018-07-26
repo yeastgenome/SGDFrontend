60315bc21e4878a4983b25409eaa2ae4a9944796
Travis Sheppard <tshepp@stanford.edu>
2018-07-25 17:12:09 -0700

    start using network viz library

--- client/jsx/views/network_view.jsx
+++ client/jsx/views/network_view.jsx
@@ -3,7 +3,9 @@ const ReactDOM = require('react-dom');
 const _ = require('underscore');
 import d3 from 'd3';
 
-const Graph = require("../components/viz/graph.jsx");
+import Graph from 'react-sigma-graph';
+
+// const Graph = require("../components/viz/graph.jsx");
 
 var networkView = {};
 networkView.render = function renderNetworkView (graphData) {
@@ -13,8 +15,13 @@ networkView.render = function renderNetworkView (graphData) {
     var formattedMonth = ('0' + month).slice(-2);
     var date = tempDate.getDate();
     var formattedDate = ('0' + date).slice(-2);
-    var _HeaderText = `SGD ${tempDate.getFullYear()}-${formattedMonth}-${formattedDate}`; 
-    ReactDOM.render(<Graph colorScale={_colorScale} data={graphData} headerText={_HeaderText} stage={0} />, document.getElementById('j-network'));
+    var _HeaderText = `SGD ${tempDate.getFullYear()}-${formattedMonth}-${formattedDate}`;
+    var _categoryColors = {
+    	'REGULATOR': '#6CB665',
+    	'TARGET': '#9F75B8',
+    	'FOCUS': '#1f77b4'
+    };
+    ReactDOM.render(<Graph categoryColors={_categoryColors} data={graphData} headerText={_HeaderText} stage={0} />, document.getElementById('j-network'));
 };
 
 module.exports = networkView;
