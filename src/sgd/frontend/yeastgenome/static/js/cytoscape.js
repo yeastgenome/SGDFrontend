79edc08d444305f62cd1d2b5805ff8c2656c2ddd
sweng66 <sweng@stanford.edu>
2018-07-26 18:15:03 -0700

    in progress

--- src/sgd/frontend/yeastgenome/static/js/cytoscape.js
+++ src/sgd/frontend/yeastgenome/static/js/cytoscape.js
@@ -109,8 +109,9 @@ function create_cytoscape_vis(div_id, layout, style, data, f, hide_singletons, l
 	var startX = (legendType === "phenotypeOntology" || legendType === "observable") ? 74 : 53;
 
 
-	drawLegendNode(ctx, mainText, startX, legendY, '#F9DA56', true, false);
-	    
+	if (legendType != "complex") {
+	    drawLegendNode(ctx, mainText, startX, legendY, '#F9DA56', true, false);
+	}    
 
         var secondX = (legendType === "phenotypeOntology" || legendType === "observable") ? 230 : 160;
 	    
