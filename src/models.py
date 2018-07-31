ca2daac51b0575d36e568e6813b40a2ca8c2f473
sweng66 <sweng@stanford.edu>
2018-07-31 16:57:45 -0700

    add code for complex network data retrieval

--- src/models.py
+++ src/models.py
@@ -7861,32 +7861,32 @@ class Complexdbentity(Dbentity):
                                      "id": interactor.format_name,
                                      "link": link,
                                      "type": type } })
-            # network_nodes.append({ "data": { "name": display_name,
-            #                                 "id": interactor.format_name,
-            #                                 "link": link,
-            #                                 "type": "Gene" } })
+            network_nodes.append({ "data": { "name": display_name,
+                                             "id": interactor.format_name,
+                                             "link": link,
+                                             "type": "Gene" } })
 
-            # network_edges.append( { "data": { "source": self.format_name,
-            #                                  "class_type": "complex_gene",
-            #                                  "target": interactor.format_name } })
+            network_edges.append( { "data": { "source": self.format_name,
+                                              "class_type": "complex_gene",
+                                              "target": interactor.format_name } })
 
-            # annot_objs2 = DBSession.query(Complexbindingannotation).filter_by(interactor_id=interactor.interactor_id).all()
-            # found = {}
-            # for annot in annot_objs2:
-            #    complex = annot.complex
-            #    if complex.format_name == self.format_name:
-            #        continue
-            #    if complex.format_name in found:
-            #        continue
-            #    found[complex.format_name] = 1
+            annot_objs2 = DBSession.query(Complexbindingannotation).filter_by(interactor_id=interactor.interactor_id).all()
+            found = {}
+            for annot in annot_objs2:
+                complex = annot.complex
+                if complex.format_name == self.format_name:
+                    continue
+                if complex.format_name in found:
+                    continue
+                found[complex.format_name] = 1
 
-            #    network_nodes.append({ "data": { "name": complex.display_name,
-            #                                     "id": complex.format_name,
-            #                                     "link": complex.obj_url,
-            #                                     "type": "Gomplex" } })
-            #    network_edges.append( { "data": { "source": complex.format_name,
-            #                                      "class_type": "complex_gene",
-            #                                      "target": interactor.format_name } })
+                network_nodes.append({ "data": { "name": complex.format_name,
+                                                 "id": complex.format_name,
+                                                 "link": complex.obj_url,
+                                                 "type": "Gomplex" } })
+                network_edges.append( { "data": { "source": complex.format_name,
+                                                  "class_type": "complex_gene",
+                                                  "target": interactor.format_name } })
             
                  
         data['subunit'] = sorted(subunits, key=lambda a: a['display_name'])
