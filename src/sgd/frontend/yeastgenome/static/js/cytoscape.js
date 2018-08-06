8d1ced97d0683315f75473278540b3476fd531ea
sweng66 <sweng@stanford.edu>
2018-08-06 11:26:32 -0700

    in progress

--- src/sgd/frontend/yeastgenome/static/js/cytoscape.js
+++ src/sgd/frontend/yeastgenome/static/js/cytoscape.js
@@ -115,6 +115,8 @@ function create_cytoscape_vis(div_id, layout, style, data, f, hide_singletons, l
 
 	if (legendType === "complex_diagram") {
 	    drawLegendNode(ctx, "protein", startX, legendY, '#86908C', true, false);   
+	    drawLegendNode(ctx, "small-molecule", startX+80, legendY, '#86908C', true, false);
+	    drawLegendNode(ctx, "sub-complex", startX+220, legendY, '#FF8933', true, false);
 	}
 	else if (legendType === "complex_network") {
 	    drawLegendNode(ctx, "complex", startX, legendY, '#86908C', true, false);
@@ -122,13 +124,6 @@ function create_cytoscape_vis(div_id, layout, style, data, f, hide_singletons, l
 	else {
 	    drawLegendNode(ctx, mainText, startX, legendY, '#F9DA56', true, false);
 	}
-
-	// else if (legendType === "complex_diagram") {
-	//    drawLegendNode(ctx, "protein", startX, legendY, '#86908C', true, true);
-	//    drawLegendNode(ctx, "small-molecule", startX+80, legendY, '#86908C', true, true);
-	//    drawLegendNode(ctx, "sub-complex", startX+220, legendY, '#FF8933', true, true);
-	// }
-
 
         var secondX = (legendType === "phenotypeOntology" || legendType === "observable") ? 230 : 160;
 	    
