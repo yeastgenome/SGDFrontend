f5161e185c9bc73c438d3722a67c64710f5ee0da
sweng66 <sweng@stanford.edu>
2018-08-08 11:43:11 -0700

    testing

--- src/sgd/frontend/yeastgenome/static/js/complex.js
+++ src/sgd/frontend/yeastgenome/static/js/complex.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
 		if(data != null && data["graph"]["nodes"].length > 1) {
 
-		    var graph = create_cytoscape_vis("cy", layout, graph_style, data["graph"], null, true, "complex_diagram");
+		    var graph = create_cytoscape_vis("cy", layout, graph_style, data["graph"], null, true, "complex");
 		    create_cy_download_button(graph, "cy_download", complex['complex_accession'] + '_subunit_graph')
 
 		}
@@ -19,16 +19,6 @@ $(document).ready(function() {
 		    
 		    var graph2 = create_cytoscape_vis("cy2", layout, graph_style, data["network_graph"], null, true, "complex_network");
 		    create_cy_download_button(graph2, "cy2_download", complex['complex_accession'] + '_complex_network_graph')
-		    
-		    if (true) {	
-			create_discrete_filter_bar("union_radio", graph, all_filter);
-			create_discrete_filter_bar("subunit_radio", graph, subunit_filter);
-			create_discrete_filter_bar("go_radio", graph, go_filter);
-			$("#discrete_filter").show();
-		    }
-		    else {
-			$("#discrete_filter").hide();
-		    }
 		}
 		else {
 		    hide_section("network");
@@ -73,25 +63,6 @@ function create_complex_table(data) {
 
 }
 
-function create_discrete_filter_bar(radio_id, graph, target_filter) {
-    var radio = $("#" + radio_id);
-    radio.click(function() {
-	    graph.filters['discrete'] = target_filter();
-	    graph.applyFilters();
-	});
-}
-
-function all_filter() {
-    return "node, edge";
-}
-
-function subunit_filter() {
-    return "node, edge[class_type = 'complex_gene']";
-}
-
-function go_filter() {
-    return "node, edge[class_type = 'complex_go']";
-}
 
 var graph_style = cytoscape.stylesheet()
     .selector('node')
@@ -107,47 +78,31 @@ var graph_style = cytoscape.stylesheet()
 	    'height': 30,
 	    'border-color': '#fff'
         })
-    .selector("node[type='smallmolecule']")
+    .selector("node[type='small-molecule']")
     .css({
-	    'background-color': "#0D85F3",
+	    'text-outline-color': "#338AFF",
 	})
-    .selector("node[type='subcomplex']")
-    .css({
-	    'background-color': "#F3750D",
-        })
-    .selector("node[type='othersubunit']")
-    .css({
-	    'background-color': "#35F30D",
-        })
-
     .selector("node[type='Gene']")
     .css({
-	    'background-color': "#FF8933",
+            'text-outline-color': "#FF8933",
         })
     .selector("node[type='Go']")
     .css({
-	    'background-color': "#A133FF",
+            'text-outline-color': "#A133FF",
         })
     .selector("node[type='Complex']")
     .css({
-            'background-color': "#86908C",
-        })
-    .selector("node[type='Current_complex']")
-    .css({
-	    'background-color': "#0E9F36",
+            'text-outline-color': "#33FFD4",
         })
     .selector('edge')
     .css({
-	    'width': 2,
-	    'target-arrow-shape': 'triangle',
-	    'line-color': '#848484',
-	    'target-arrow-color': '#848484'
+	    'width': 2
         })
     .selector("node[sub_type='FOCUS']")
     .css({
-	    'background-color': "#0E9F36",
+	    'background-color': "#FF3391",
 	    'text-outline-color': '#fff',
-	    'color': '#888'
+	    'color': '#fff'
         })
     .selector("edge[class_type = 'complex_go']")
     .css({
@@ -155,7 +110,7 @@ var graph_style = cytoscape.stylesheet()
         })
     .selector("edge[class_type = 'complex_gene']")
     .css({
-            'line-color': "#FF8933"
+            'line-color': "#74FF33"
         })
     .selector("edge[class_type = 'complex']")
     .css({
