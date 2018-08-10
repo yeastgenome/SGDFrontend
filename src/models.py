428e441a405611b776a690bf99dfabcb27121b12
sweng66 <sweng@stanford.edu>
2018-08-10 16:12:29 -0700

    added evidence count for complex network graph

--- src/models.py
+++ src/models.py
@@ -7966,12 +7966,12 @@ class Complexdbentity(Dbentity):
         min_evidence_cutoff = 2;
         max_evidence_cutoff = 2;
 
-        for x in network_nodes:
-            data = x['data']
-            node_id = data['id']
-            if evidenceCount[node_id] > max_evidence_cutoff:
-                max_evidence_cutoff = evidenceCount[node_id]
-            data['evidence'] = evidenceCount[node_id]
+        # for x in network_nodes:
+        #    data = x['data']
+        #    node_id = data['id']
+        #    if evidenceCount[node_id] > max_evidence_cutoff:
+        #        max_evidence_cutoff = evidenceCount[node_id]
+        #    data['evidence'] = evidenceCount[node_id]
         
         data['subunit'] = sorted(subunits, key=lambda a: a['display_name'])
         data['graph'] = { "edges": edges, "nodes": nodes }
