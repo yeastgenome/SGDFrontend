5f3cc2649f0fffd2eee2927f5445ae35d9130a5d
Travis Sheppard <tshepp@stanford.edu>
2018-08-14 16:00:40 -0700

    change cap of complex network cats

--- src/models.py
+++ src/models.py
@@ -8582,7 +8582,7 @@ class Complexdbentity(Dbentity):
                                 "name": complex.display_name,
                                 "id": complex.format_name,
                                 "href": "/complex/" + complex.format_name,
-                                "category": "Complex"
+                                "category": "complex"
                             })
                             network_nodes_ids[complex.format_name] = True
                         network_edges.append( foundComplex[complex.format_name] )
