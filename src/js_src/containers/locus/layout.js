0fc23836a8da88e9c4ea22e21e14786479bfe19a
felixgondwe <gondwefelix8@gmail.com>
2018-10-18 13:49:46 -0700

    navigation fix.

--- src/js_src/containers/locus/layout.js
+++ src/js_src/containers/locus/layout.js
@@ -1,3 +1,4 @@
+/*eslint-disable */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
@@ -38,12 +39,8 @@ class LocusLayout extends Component {
     let baseUrl = `${BASE_CURATE_URL}/${this.props.params.id}`;
     let current = this.props.pathname.replace(baseUrl, '');
     let nodes = SECTIONS.map( (d) => {
-      let relative;
-      if (d === 'summaries') {
-        relative = '';
-      } else {
-        relative = `/${d}`;
-      }
+      let relative = d ? `/${d}` : '';
+      current = !current? relative : current; 
       let isActive = (current === relative);
       let url = `${baseUrl}${relative}`;
       let _className = isActive ? style.activeNavLink : style.navLink;
