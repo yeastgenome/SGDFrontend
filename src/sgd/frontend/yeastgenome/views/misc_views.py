d58584fb2a26c3334097d6201db7e82bfe44800b
felixgondwe <gondwefelix8@gmail.com>
2019-01-28 17:12:25 -0800

    minor change

--- src/sgd/frontend/yeastgenome/views/misc_views.py
+++ src/sgd/frontend/yeastgenome/views/misc_views.py
@@ -218,15 +218,19 @@ def variant_viewer(request):
 
 @view_config(route_name='new_gene_name_reservation')
 def new_gene_name_reservation(request):
-    ci_base = config.backend_url if config.environment == 'dev' else 'https://curate.yeastgenome.org'
-    
+    #TODO: run this line in dev to avoid updating curate.* box
+    #ci_base = config.backend_url if config.environment == 'dev' else 'https://curate.yeastgenome.org'
+    ci_base = 'https://curate.yeastgenome.org'
+
     return render_to_response(TEMPLATE_ROOT + 'iframe.jinja2', { 'ci_url': 'new_reservation', 'ci_base': ci_base }, request=request)
 
 
 @view_config(route_name='new_colleague')
 def new_colleague(request):
-    ci_base = config.backend_url if config.environment == 'dev' else 'https://curate.yeastgenome.org'
-  
+    #TODO: run this line in dev to avoid updating curate.* box
+    #ci_base = config.backend_url if config.environment == 'dev' else 'https://curate.yeastgenome.org'
+    ci_base = 'https://curate.yeastgenome.org'
+
     return render_to_response(TEMPLATE_ROOT + 'iframe.jinja2', { 'ci_url': 'new_colleague', 'ci_base': ci_base }, request=request)
 
 @view_config(route_name='primer3')
