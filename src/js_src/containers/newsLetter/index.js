9080480b57b266b5e9c15a799f3167ea5d92eea0
Sagar <sagar.jhaa@gmail.com>
2019-03-05 11:16:22 -0800

    Added newsLetter container

---
+++ src/js_src/containers/newsLetter/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { CurateLayout } from '../curateHome/layout';
+
+class NewsLetter extends Component {constructor(props) {super(props);}
+  render() {
+    return (<CurateLayout>
+        <h1>Newsletter</h1>
+    </CurateLayout>);
+  }
+}
+
+export default NewsLetter;
