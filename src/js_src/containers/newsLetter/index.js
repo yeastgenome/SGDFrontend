b0ab6831352049d5363b19f3c1b2f5746cf3ff7c
Sagar <sagar.jhaa@gmail.com>
2019-03-08 15:30:57 -0800

    Add state and props to construtor.

--- src/js_src/containers/newsLetter/index.js
+++ src/js_src/containers/newsLetter/index.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import { CurateLayout } from '../curateHome/layout';
+import CurateLayout from '../curateHome/layout';
 
-class NewsLetter extends Component {constructor(props) {super(props);}
+// import fetchData from '../../lib/fetchData';
+// const DATA_URL = '/colleagues_subscriptions';
+
+class NewsLetter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data:null
+    };
+  }
+
   render() {
-    return (<CurateLayout>
-        <h1>Newsletter</h1>
-    </CurateLayout>);
+    return (
+      <CurateLayout>
+        {<h1>NewsLetter</h1>}
+      </CurateLayout>);
   }
 }
 
+// export default connect(mapStateToProps)(NewsLetter);
 export default NewsLetter;
