f9fcb1fefd298ba88276c9fc14dd86cfe5424a31
Sagar <sagar.jhaa@gmail.com>
2019-03-13 16:37:25 -0700

    Written email function to send newsletter in email

--- src/helpers.py
+++ src/helpers.py
@@ -472,3 +472,43 @@ def tsv_file_to_dict(tsv_file):
         return list_dictionary
     else:
         return list_dictionary
+
+
+def send_email(msg):
+    try:
+        import smtplib, ssl
+        from email.mime.text import MIMEText
+        from email.mime.multipart import MIMEMultipart
+
+        sender_email = ""
+        receiver_email = ""
+        password = ''
+
+        message = MIMEMultipart("alternative")        
+        message["Subject"] = "multipart test"
+        message["From"] = sender_email
+        message["To"] = receiver_email
+
+        ## Create the plain-text and HTML version of your message
+        ## Turn these into plain/html MIMEText objects
+        # part1 = MIMEText(text, "plain")
+
+        part2 = MIMEText(msg.encode('utf8'), "html")
+        
+        ## Add HTML/plain-text parts to MIMEMultipart message
+        ## The email client will try to render the last part first
+        # message.attach(part1)
+        message.attach(part2)
+
+        # Create secure connection with server and send email
+        context = ssl.create_default_context()
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server.set_debuglevel(1)
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+        server.quit()
+        
+        return {"success":"Email sent"}
+
+    except Exception as e:
+        return {"error":"Major error"}
