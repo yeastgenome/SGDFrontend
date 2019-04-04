2e17b3b51415088ba04c22429868ec35d6e0983b
Sagar <sagar.jhaa@gmail.com>
2019-04-04 13:26:40 -0700

    Updated to add route to PTM

--- src/js_src/containers/curateHome/layout.js
+++ src/js_src/containers/curateHome/layout.js
@@ -22,6 +22,7 @@ class CurateLayout extends Component {
             <li><Link className={(location === '/settings') ? style.activeLink : null} to='/settings'><span><i className='fa fa-cog' /> Settings</span></Link></li>
             <li><Link className={(location === '/curate/reference/new') ? style.activeLink : null} to='/curate/reference/new'><span><i className='fa fa-plus' /> Add References</span></Link></li>
             <li><Link className={(location === '/newsletter') ? style.activeLink : null} to='/newsletter'><span><i className="fa fa-envelope" /> Newsletter</span></Link></li>
+            <li><Link className={(location === '/ptm') ? style.activeLink : null} to='/ptm'><span><i className="fa fa-question" /> PTM</span></Link></li>
           </ul>
         </div>
         <div className={LARGE_COL_CLASS}>
