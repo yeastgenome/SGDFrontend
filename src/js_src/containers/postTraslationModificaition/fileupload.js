5ffb33270a783a19c8bdc49c93ed192a6ae9cfaa
Sagar <sagar.jhaa@gmail.com>
2019-04-08 16:28:40 -0700

    file upload interface for PTM file

--- src/js_src/containers/postTraslationModificaition/fileupload.js
+++ src/js_src/containers/postTraslationModificaition/fileupload.js
@@ -1,24 +1,41 @@
 import React,{Component} from 'react';
+import { connect } from 'react-redux';
 import Dropzone from 'react-dropzone';
 import style from './style.css';
-import fetchData from '../../lib/fetchData';
 
+import fetchData from '../../lib/fetchData';
 const FILE_INSERT = '/ptm_file';
+const TIMEOUT = 120000;
 
 class FileUpload extends Component{
   constructor(props){
     super(props);
+    this.state = {
+      file:''
+    };
+
     this.handleDrop = this.handleDrop.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleDrop(file){
-    console.log(file);
+  handleDrop(files){
+    this.setState({file:files[0]});
   }
 
   handleSubmit(){
+    let formData = new FormData(this.refs.form);
+    formData.append('file', this.state.file);
+    
     fetchData(FILE_INSERT,{
-      type:'GET'
+      type:'POST',
+      credentials: 'same-origin',
+      headers: {
+        'X-CSRF-Token': this.props.csrfToken
+      },
+      data:formData,
+      processData: false,
+      contentType: false,
+      timeout:TIMEOUT
     })
     .then((data) => {
       console.log(data);
@@ -29,17 +46,28 @@ class FileUpload extends Component{
 
   render(){
     return(
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} ref='form'>
             <Dropzone multiple={false} onDrop={this.handleDrop}>
               <p className={style.uploadMsg}>Drop file here or click to select.</p>
               <h3 className={style.uploadIcon}><i className='fa fa-upload' /></h3>
             </Dropzone>
             <button type='submit' className='button'>Submit</button>
       </form>
-      
     );
   }
 }
 
 
-export default FileUpload;
+FileUpload.propTypes = {
+  csrfToken: React.PropTypes.string
+};
+
+function mapStateToProps(state) {
+  return {
+    csrfToken: state.auth.csrfToken
+  };
+}
+
+export default connect(mapStateToProps)(FileUpload);
+
+// export default FileUpload;
