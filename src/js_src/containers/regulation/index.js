a17aca11b091c7f6c92daa6b171f737e8a053fce
Sagar <sagar.jhaa@gmail.com>
2019-05-02 12:58:11 -0700

    Added fileUpload and form interface for regulation

--- src/js_src/containers/regulation/index.js
+++ src/js_src/containers/regulation/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CurateLayout from '../curateHome/layout';
+import FileUpload from './fileUpload';
+import RegulationForm from './regulationForm';
 
 class Regulation extends Component {
   constructor(props) {
@@ -10,8 +12,15 @@ class Regulation extends Component {
   render() {
     return (
       <CurateLayout>
-        <div>
-          <p>Component template </p>
+        <div className='row'>
+          <div className='columns small-12 medium-12 large-5'>
+            <h1>Regulation file upload</h1>
+            <FileUpload/>
+          </div>
+          <div className='columns small-12 medium-12 large-5 end'>
+            <h1>Add/Update regulation</h1>
+            <RegulationForm />
+          </div>
         </div>
       </CurateLayout>
     );
