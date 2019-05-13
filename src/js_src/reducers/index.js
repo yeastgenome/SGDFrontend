6d5b9c0cb3e756fde20ef4ff2de44468f8b8b443
Sagar <sagar.jhaa@gmail.com>
2019-05-13 13:05:38 -0700

    Add reducer to index js

--- src/js_src/reducers/index.js
+++ src/js_src/reducers/index.js
@@ -5,6 +5,7 @@ import locusReducer from './locusReducer';
 import searchReducer from './searchReducer';
 import ptmReducer from './ptmReducer';
 import newsLetterReducer from './newsLetterReducer';
+import regulationReducer from './regulationReducer';
 
 export default {
   auth: authReducer,
@@ -13,5 +14,6 @@ export default {
   locus: locusReducer,
   search: searchReducer,
   ptm:ptmReducer,
-  newsLetter:newsLetterReducer
+  newsLetter:newsLetterReducer,
+  regulation:regulationReducer
 };
