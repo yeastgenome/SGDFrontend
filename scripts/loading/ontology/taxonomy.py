7f00d11c7b0e818670c659d932e4fbb7a907a99b
sweng66 <sweng@stanford.edu>
2019-08-02 11:47:13 -0700

    fixed the URL for downloading the ncbitaxon.owl file

--- scripts/loading/ontology/taxonomy.py
+++ scripts/loading/ontology/taxonomy.py
@@ -310,9 +310,9 @@ def write_summary_and_send_email(fw, update_log, to_delete_list):
 
 
 if __name__ == "__main__":
-        
-    url_path = 'http://ontologies.berkeleybop.org/'
-    owl_file = 'goncbitaxon.owl'
+            
+    url_path = 'http://purl.obolibrary.org/obo/'
+    owl_file = 'ncbitaxon.owl'
     urllib.request.urlretrieve(url_path + owl_file, owl_file)
 
     load_ontology(owl_file)
