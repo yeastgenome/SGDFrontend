c6479cf9b8de50ee77918fe033c9246bb73d53ee
Sagar <sagar.jhaa@gmail.com>
2019-08-22 10:06:10 -0700

    FIxed ReactApp to use connected-react-router

--- src/js_src/reactApplication.js
+++ src/js_src/reactApplication.js
@@ -1,50 +1,23 @@
-// import React, { Component } from 'react';
-// import { Router, hashHistory, createMemoryHistory } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import { syncHistoryWithStore } from 'connected-react-router';
-// import configureStore from './lib/configureStore';
-
-// import routes from './routes';
-
-// class ReactApp extends Component {
-//   render() {
-//     let isBrowser = typeof window === 'object';
-//     let historyObj = isBrowser ? hashHistory : createMemoryHistory('/');
-//     let store = configureStore(historyObj);
-//     let history = syncHistoryWithStore(historyObj, store);
-//     return (
-//       <Provider store={store}>
-//         <Router history={history}>
-//           {routes}
-//         </Router>
-//       </Provider>
-//     );
-//   }
-// }
-
-// export default ReactApp;
-
-
 import React, { Component } from 'react';
-import { HashRouter as Router, createMemoryHistory } from 'react-router-dom';
+import { createMemoryHistory } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import { syncHistoryWithStore } from 'connected-react-router';
-import configureStore from './lib/configureStore';
+import { ConnectedRouter } from 'connected-react-router';
+import { createHashHistory } from 'history';
 
+import configureStore from './lib/configureStore';
 import routes from './routes';
 
-import { createHashHistory } from 'history';
+
 class ReactApp extends Component {
   render() {
     let isBrowser = typeof window === 'object';
-    let historyObj = isBrowser ? createHashHistory() : createMemoryHistory('/');
-    let store = configureStore(historyObj);
-    // let history = syncHistoryWithStore(historyObj, store);
+    let history = isBrowser ? createHashHistory() : createMemoryHistory('/');
+    let store = configureStore(history);
     return (
       <Provider store={store}>
-        <Router>
+        <ConnectedRouter history={history}>
           {routes}
-        </Router>
+        </ConnectedRouter>
       </Provider>
     );
   }
