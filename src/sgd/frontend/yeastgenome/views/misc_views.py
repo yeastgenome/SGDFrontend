43a863fe70d23601de6d4719f7f4524512904e3e
Sagar <sagar.jhaa@gmail.com>
2019-09-05 13:39:47 -0700

    Update misc_views to use curation server url

--- src/sgd/frontend/yeastgenome/views/misc_views.py
+++ src/sgd/frontend/yeastgenome/views/misc_views.py
@@ -230,12 +230,13 @@ def variant_viewer(request):
 
 @view_config(route_name='new_gene_name_reservation')
 def new_gene_name_reservation(request):
-    if ENV == 'dev':
-        ci_base = config.backend_url
-    elif ENV == 'prod':
-        ci_base = 'https://curate.yeastgenome.org'
-    else:
-        ci_base = 'https://curate.qa.yeastgenome.org'
+    ci_base = config.curate_server
+    # if ENV == 'dev':
+    #     ci_base = config.backend_url
+    # elif ENV == 'prod':
+    #     ci_base = 'https://curate.yeastgenome.org'
+    # else:
+    #     ci_base = 'https://curate.qa.yeastgenome.org'
 
     return render_to_response(
         TEMPLATE_ROOT + 'iframe.jinja2', {
@@ -248,12 +249,13 @@ def new_gene_name_reservation(request):
 
 @view_config(route_name='new_colleague')
 def new_colleague(request):
-    if ENV == 'dev':
-        ci_base = config.backend_url
-    elif ENV == 'prod':
-        ci_base = 'https://curate.yeastgenome.org'
-    else:
-        ci_base = 'https://curate.qa.yeastgenome.org'
+    ci_base = config.curate_server
+    # if ENV == 'dev':
+    #     ci_base = config.backend_url
+    # elif ENV == 'prod':
+    #     ci_base = 'https://curate.yeastgenome.org'
+    # else:
+    #     ci_base = 'https://curate.qa.yeastgenome.org'
 
     return render_to_response(
         TEMPLATE_ROOT + 'iframe.jinja2', {
