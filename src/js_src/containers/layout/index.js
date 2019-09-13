1f88af8d49dda022463a834b01ff9fd9c74266d6
Sagar <sagar.jhaa@gmail.com>
2019-09-12 18:12:26 -0700

    Move routes to routes.js to fix redux issue

--- src/js_src/containers/layout/index.js
+++ src/js_src/containers/layout/index.js
@@ -8,34 +8,6 @@ import SearchBar from './searchBar';
 import curateLogo from './curateLogo.png';
 import Loader from './loader/index';
 import { clearError, clearMessage } from '../../actions/metaActions';
-import {Route,Switch} from 'react-router-dom';
-
-//Curator Interfaces
-import CurateHome from '../../containers/curateHome';
-import { requireAuthentication } from '../../containers/authenticateComponent';
-import TriageIndex from '../../containers/triage';
-import ColleaguesIndex from '../../containers/colleagues/index';
-
-import GeneNameReservationIndex from '../../containers/reserve/index';
-import GeneNameReservation from '../../containers/reserve/show';
-import GeneNameReservationEdit from '../../containers/reserve/edit';
-import GeneNameReservationStandardize from '../../containers/reserve/standardize';
-import ColleaguesShow from '../../containers/colleagues/show';
-import SpreadsheetUpload from '../../containers/spreadsheetUpload/index';
-import Settings from '../../containers/settings/index';
-import NewsLetter from '../../containers/newsLetter/index';
-import PostTranslationModification from '../../containers/postTraslationModificaition/index';
-import Help from '../../containers/help';
-import Search from '../../containers/search';
-import PublicHome from '../../containers/publicHome';
-import GoogleLogin from '../../containers/googleLogin';
-import NewReference from '../../containers/newReference';
-import CurateLit from '../../containers/curateLit/layout';
-import Regulation from '../../containers/regulation/index';
-import LocusLayout from '../../containers/locus/layout';
-import FileCurate from '../../containers/fileCurate';
-import FileCurateUpdate from '../../containers/fileCurate/updateFile.js';
-import NotFound from '../../containers/curateHome/notFound';
 
 class LayoutComponent extends Component {
   renderSearch () {
@@ -151,31 +123,6 @@ class LayoutComponent extends Component {
             {this.renderBody()}
           </div>
         </div>
-        <Switch>
-          <Route component={requireAuthentication(CurateHome)} path='/' exact />
-          <Route component={requireAuthentication(TriageIndex)} path='/triage' />
-          <Route component={requireAuthentication(ColleaguesIndex)} path='/colleagues/triage' exact />
-          <Route component={requireAuthentication(ColleaguesShow)} path='/colleagues/triage/:id' />
-          <Route component={requireAuthentication(SpreadsheetUpload)} path='/spreadsheet_upload' />
-          <Route component={requireAuthentication(Settings)} path='/settings' />
-          <Route component={requireAuthentication(NewsLetter)} path='/newsletter' />
-          <Route component={requireAuthentication(PostTranslationModification)} path='/ptm' />
-          <Route component={Help} path='/help' />
-          <Route component={requireAuthentication(Search)} path='/search' />
-          <Route component={PublicHome} path='/login' />
-          <Route component={GoogleLogin} path='/google_login' />
-          <Route component={requireAuthentication(NewReference)} path='/curate/reference/new' />
-          <Route component={requireAuthentication(CurateLit)} path='/curate/reference/:id' />
-          <Route component={requireAuthentication(Regulation)} path='/regulation' />
-          <Route component={requireAuthentication(LocusLayout)} path='/curate/locus/:id' />
-          <Route component={requireAuthentication(FileCurate)} path='/file_curate' />
-          <Route component={requireAuthentication(FileCurateUpdate)} path='/file_curate_update' />
-          <Route component={requireAuthentication(GeneNameReservationIndex)} path='/reservations' exact />
-          <Route component={requireAuthentication(GeneNameReservation)} path='/reservations/:id' exact />
-          <Route component={requireAuthentication(GeneNameReservationEdit)} path='/reservations/:id/edit' exact />
-          <Route component={requireAuthentication(GeneNameReservationStandardize)} path='/reservations/:id/standardize' exact />
-          <Route component={NotFound} path='*' />
-        </Switch>
       </div>
     );
   }
