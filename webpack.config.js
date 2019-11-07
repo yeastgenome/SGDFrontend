bcb314c5aad0982805e8b0d1ec5c70909bc3cb5e
Sagar <sagar.jhaa@gmail.com>
2019-11-06 16:10:12 -0800

    add webpack.config file

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./client/jsx/application.jsx",
+  output: {
+    filename: "./application.js",
+    path:path.join(__dirname, "src/sgd/frontend/yeastgenome/static/js")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+            presets: ["react","es2015"]
+          }
+        }
+      }
+    ]
+  },
+  resolve:{
+    extensions: ['*','.jsx','.js','.json']
+  },
+  stats: {
+    colors: true,
+    modules: true,
+    reasons: true,
+    errorDetails: false
+  },
+  externals: {
+    foundation: 'foundation'
+  }
+} 
