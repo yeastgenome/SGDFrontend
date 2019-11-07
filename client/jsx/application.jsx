f00544a639e94f7ca44a1e06b81d563963b8b572
Sagar <sagar.jhaa@gmail.com>
2019-11-06 21:02:47 -0800

    import all views as es6 modules in application.jsx

--- client/jsx/application.jsx
+++ client/jsx/application.jsx
@@ -2,34 +2,55 @@
 if (process.env.NODE_ENV === 'production') {
 	process.env.NODE_ENV = 'production';
 }
-var setup = require('./lib/setup.jsx');
+import setup from './lib/setup.jsx';
 
 /*
 	Assign views as the values in a views object, which gets assigned to the global views object.
 	To execute view logic, use views.example.render()
 */
 
+import expressionView from './views/expression_view.jsx';
+import interactionSearch from './views/interaction_search_view.jsx';
+import sequenceView from './views/sequence_view.jsx';
+import snapshotView from './views/snapshot_view.jsx';
+import suggestionView from './views/suggestion_view.jsx';
+import blastSgdView from './views/blast_sgd_view.jsx';
+import blastFungalView from './views/blast_fungal_view.jsx';
+import patmatchView from './views/patmatch_view.jsx';
+import restMapperView from './views/restrictionmapper_view.jsx';
+import seqToolsView from './views/seqTools_view.jsx';
+import goTermFinderView from './views/gotermfinder_view.jsx';
+import goSlimMapperView from './views/goslimmapper_view.jsx';
+import strainAlignmentView from './views/strain_alignment_view.jsx';
+import networkView from './views/network_view.jsx';
+import proteinView from './views/protein_view.jsx';
+import regulationView from './views/regulation_view.jsx';
+import summaryView from './views/summary_view.jsx'
+import view from './views/variant_viewer_view.jsx';
+import litView from './views/literature_view.jsx';
+import reactRouterRender from './react_router_render.jsx';
+
 var views = {
-  expression: require('./views/expression_view.jsx'),
-  interactionSearch: require('./views/interaction_search_view.jsx'),
-  sequence: require('./views/sequence_view.jsx'),
-  snapshot: require('./views/snapshot_view.jsx'),
-  suggestion: require('./views/suggestion_view.jsx'),
-  blast_sgd: require('./views/blast_sgd_view.jsx'),
-  blast_fungal: require('./views/blast_fungal_view.jsx'),
-  patmatch: require('./views/patmatch_view.jsx'),
-  restrictionmapper: require('./views/restrictionmapper_view.jsx'),
-  seqTools: require('./views/seqTools_view.jsx'),
-  goTermFinder: require('./views/gotermfinder_view.jsx'),
-  goSlimMapper: require('./views/goslimmapper_view.jsx'),
-  strainAlignment: require('./views/strain_alignment_view.jsx'),
-  network: require('./views/network_view.jsx'),
-  protein: require('./views/protein_view.jsx'),
-  regulation: require('./views/regulation_view.jsx'),
-  summary: require('./views/summary_view.jsx'),
-  variantViewer: require('./views/variant_viewer_view.jsx'),
-  literature: require('./views/literature_view.jsx'),
-  router: require('./react_router_render.jsx')
+  expression:expressionView,
+  interactionSearch: interactionSearch,
+  sequence: sequenceView,
+  snapshot: snapshotView,
+  suggestion: suggestionView,
+  blast_sgd: blastSgdView,
+  blast_fungal: blastFungalView,
+  patmatch: patmatchView,
+  restrictionmapper: restMapperView,
+  seqTools: seqToolsView,
+  goTermFinder: goTermFinderView,
+  goSlimMapper: goSlimMapperView,
+  strainAlignment: strainAlignmentView,
+  network: networkView,
+  protein: proteinView,
+  regulation: regulationView,
+  summary :summaryView,
+  variantViewer: view,
+  literature: litView,
+  router: reactRouterRender
 };
 
 // call setup script
@@ -37,4 +58,3 @@ setup();
 
 // assign to global view object
 window.views = views;
-module.exports = views;
