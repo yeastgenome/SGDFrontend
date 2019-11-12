e26ba52c0eeb4c459851770cdf179dff4fb69bd3
sweng66 <sweng@stanford.edu>
2019-11-12 14:08:32 -0800

    testing

--- src/sgd/tools/seqtools/__init__.py
+++ src/sgd/tools/seqtools/__init__.py
@@ -47,7 +47,7 @@ def run_emboss(p):
     seq = p.get('seq')
     seqname = p.get('seqname')
 
-    return { "content": emboss }
+    return { "content": emboss + ":" + seqname }
 
     ## get seq for seqname
     # if seq is None and seqname is not None:
