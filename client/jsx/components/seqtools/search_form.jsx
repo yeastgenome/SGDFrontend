fc80996dfda808ada8dc018f1bb57e517cc126d6
sweng66 <sweng@stanford.edu>
2019-11-12 21:44:34 -0800

    testing

--- client/jsx/components/seqtools/search_form.jsx
+++ client/jsx/components/seqtools/search_form.jsx
@@ -1480,8 +1480,8 @@ const GeneSequenceResources = React.createClass({
 		 }
 		 
 	     	 return (<div>
-                           <span style={ style.textFont }><strong>{ title }</strong> for gene/sequence in strain <div style={{ fontSize: 18, color: 'red' }}> {strain}: { seqname }</div></span>
-                         </div>);
+		     
+		     <span style={ style.textFont }><strong>{ title }</strong> for gene/sequence in strain <strong style={{ color: 'red' }}>{ strain }: { seqname }</strong></span>
 
 	     }
 	     
