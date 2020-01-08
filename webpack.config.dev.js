ab207328344a83dd161e270873462e4336cedb90
Sagar <sagar.jhaa@gmail.com>
2020-01-08 14:58:59 -0800

    Change to config files

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,7 +2,8 @@ let merge = require('webpack-merge');
 let base = require('./webpack.config.base');
 
 let dev = {
-  mode:'development'
+  mode:'development',
+  devtool:'source-map'
 }
 
 module.exports = merge(base,dev);
