ab90fe50a14b237146dfd57849164dfc16506149
Sagar <sagar.jhaa@gmail.com>
2020-01-16 11:59:33 -0800

    Update to use react-router-dom

--- client/jsx/containers/facet_selector.jsx
+++ client/jsx/containers/facet_selector.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import { routeActions, push } from 'react-router-redux';
 import Radium from 'radium';
 import _ from 'underscore';
