d2c16963a26fb0c32afe1ad15e874f2dcef85db9
Sagar <sagar.jhaa@gmail.com>
2020-01-27 15:13:24 -0800

    Use react-router and not router-dom

--- client/jsx/containers/search_breadcrumb.jsx
+++ client/jsx/containers/search_breadcrumb.jsx
@@ -1,7 +1,7 @@
 import Radium from 'radium';
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 
 import { getHrefWithoutAgg, getCategoryDisplayName } from '../lib/search_helpers';
 
