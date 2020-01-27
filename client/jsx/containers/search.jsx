ed504342a73c4c8308bced0925ba830b732120ea
Sagar <sagar.jhaa@gmail.com>
2020-01-27 15:13:24 -0800

    Use react-router and not router-dom

--- client/jsx/containers/search.jsx
+++ client/jsx/containers/search.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import Radium from 'radium';
 import { connect } from 'react-redux';
 import _ from 'underscore';
