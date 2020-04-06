9f3aa21a38dc07e7e8c0b200b16b6e5ebe789cfd
Sagar <sagar.jhaa@gmail.com>
2020-04-06 13:03:56 -0700

    PatMatch working

--- client/jsx/views/patmatch_view.jsx
+++ client/jsx/views/patmatch_view.jsx
@@ -1,13 +1,13 @@
+'use strict';
 
-"use strict";
-
-const React = require("react");
-const SearchForm = require("../components/patmatch/search_form.jsx");
+import React from 'react';
+import ReactDOM from 'react-dom';
+const SearchForm = require('../components/patmatch/search_form.jsx');
 
 var patmatchView = {};
 
 patmatchView.render = function () {
-	React.render(<SearchForm />,  document.getElementById("j-main"));
+  ReactDOM.render(<SearchForm />, document.getElementById('j-main'));
 };
 
 module.exports = patmatchView;
