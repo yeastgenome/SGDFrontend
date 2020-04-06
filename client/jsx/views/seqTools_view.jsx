d5329e8115513eafab6614a37dadba5d711824a2
Sagar <sagar.jhaa@gmail.com>
2020-04-06 14:26:16 -0700

    Seqtool working

--- client/jsx/views/seqTools_view.jsx
+++ client/jsx/views/seqTools_view.jsx
@@ -1,13 +1,13 @@
+'use strict';
 
-"use strict";
-
-const React = require("react");
-const SearchForm = require("../components/seqtools/search_form.jsx");
+import React from 'react';
+import ReactDOM from 'react-dom';
+const SearchForm = require('../components/seqtools/search_form.jsx');
 
 var seqToolsView = {};
 
 seqToolsView.render = function () {
-	React.render(<SearchForm />,  document.getElementById("j-main"));
+  ReactDOM.render(<SearchForm />, document.getElementById('j-main'));
 };
 
 module.exports = seqToolsView;
