b323e5a523a39635824b5f70a1f2c5847d13a804
Sagar <sagar.jhaa@gmail.com>
2020-04-06 12:21:51 -0700

    GoTermFinder

--- client/jsx/views/gotermfinder_view.jsx
+++ client/jsx/views/gotermfinder_view.jsx
@@ -1,13 +1,13 @@
+'use strict'
 
-"use strict";
+import React from 'react'
+import ReactDOM from 'react-dom'
+const SearchForm = require('../components/gotools/gotermfinder_form.jsx')
 
-const React = require("react");
-const SearchForm = require("../components/gotools/gotermfinder_form.jsx");
-
-var goTermFinderView = {};
+var goTermFinderView = {}
 
 goTermFinderView.render = function () {
-	React.render(<SearchForm />,  document.getElementById("j-main"));
-};
+    ReactDOM.render(<SearchForm />, document.getElementById('j-main'))
+}
 
-module.exports = goTermFinderView;
+module.exports = goTermFinderView
