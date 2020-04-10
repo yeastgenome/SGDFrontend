30a5f1885bcd618cb52aa66f4d0db9256cf1973f
Sagar <sagar.jhaa@gmail.com>
2020-04-10 00:45:58 -0700

    deepscan

--- client/jsx/components/sequence/sequence_toggler.jsx
+++ client/jsx/components/sequence/sequence_toggler.jsx
@@ -1,4 +1,3 @@
-import d3 from 'd3';
 import React from 'react';
 import _ from 'underscore';
 
@@ -20,13 +19,13 @@ const SequenceToggler = React.createClass({
       showCustomRetrieval: false,
       showSequence: true,
       subFeatureData: [],
-      text: null
+      text: null,
     };
   },
 
   getInitialState: function () {
     return {
-      activeSequenceType: this.props.sequences[0].key
+      activeSequenceType: this.props.sequences[0].key,
     };
   },
 
@@ -40,7 +39,7 @@ const SequenceToggler = React.createClass({
     var _downloadParams = {
       header: _activeSequence.header,
       sequence: _activeSequence.sequence,
-      filename: _activeSequence.filename
+      filename: _activeSequence.filename,
     };
 
     var dropdownNode = this._getDropdownNode();
@@ -48,32 +47,56 @@ const SequenceToggler = React.createClass({
 
     var customRetrievalNode = null;
     if (this.props.showCustomRetrieval) {
-      customRetrievalNode = (<ul className="button-group radius">
-        <a className="button small secondary" href={"/seqTools?seqname=" + this.props.locusFormatName}>Custom Sequence Retrieval</a>
-      </ul>);
+      customRetrievalNode = (
+        <ul className="button-group radius">
+          <a
+            className="button small secondary"
+            href={'/seqTools?seqname=' + this.props.locusFormatName}
+          >
+            Custom Sequence Retrieval
+          </a>
+        </ul>
+      );
     }
 
-    return (<div>
-      {textNode}
-      {dropdownNode}
-      {sequenceTextNode}
-      <div className="button-bar">
-        <ul className="button-group radius">
-          <li><DownloadButton buttonId={this.props.buttonId} text="Download Sequence" url="/download_sequence" extension=".fsa" params={_downloadParams}/></li>
-        </ul>
-        {customRetrievalNode}
+    return (
+      <div>
+        {textNode}
+        {dropdownNode}
+        {sequenceTextNode}
+        <div className="button-bar">
+          <ul className="button-group radius">
+            <li>
+              <DownloadButton
+                buttonId={this.props.buttonId}
+                text="Download Sequence"
+                url="/download_sequence"
+                extension=".fsa"
+                params={_downloadParams}
+              />
+            </li>
+          </ul>
+          {customRetrievalNode}
+        </div>
       </div>
-
-    </div>);
+    );
   },
 
   _getDropdownNode: function () {
-    var _isDisabled = (s) => { return !s.sequence; };
-    var _elements = _.map(this.props.sequences, s => {
+    var _isDisabled = (s) => {
+      return !s.sequence;
+    };
+    var _elements = _.map(this.props.sequences, (s) => {
       s.value = s.key;
       return s;
     });
-    return <DropdownSelector elements={_elements} isDisabled={_isDisabled} onChange={this._handleChangeSequence} />;
+    return (
+      <DropdownSelector
+        elements={_elements}
+        isDisabled={_isDisabled}
+        onChange={this._handleChangeSequence}
+      />
+    );
   },
 
   _formatActiveSequenceTextNode: function () {
@@ -85,8 +108,12 @@ const SequenceToggler = React.createClass({
       var legendNode = null;
       if (this._canColorSubFeatures()) {
         sequenceNode = this._getComplexSequenceNode(seq);
-        var _featureTypes = _.uniq(_.map(this.props.subFeatureData, f => { return f.class_type; }));
-        var _colors = _.map(_featureTypes, f => {
+        var _featureTypes = _.uniq(
+          _.map(this.props.subFeatureData, (f) => {
+            return f.class_type;
+          })
+        );
+        var _colors = _.map(_featureTypes, (f) => {
           return { text: f, color: subFeatureColorScale(f) };
         });
         legendNode = <Legend elements={_colors} />;
@@ -94,18 +121,19 @@ const SequenceToggler = React.createClass({
         sequenceNode = this._getSimpleSequenceNode(seq);
       }
 
-      node = (<div>
-        {legendNode}
-        <pre>
-          <blockquote style={{ fontFamily: "Monospace", fontSize: 14 }}>
-            {sequenceNode}
-          </blockquote>
-        </pre>
-      </div>);
+      node = (
+        <div>
+          {legendNode}
+          <pre>
+            <blockquote style={{ fontFamily: 'Monospace', fontSize: 14 }}>
+              {sequenceNode}
+            </blockquote>
+          </pre>
+        </div>
+      );
     }
 
     return node;
-
   },
 
   _handleChangeSequence: function (value) {
@@ -124,55 +152,84 @@ const SequenceToggler = React.createClass({
   },
 
   _getSimpleSequenceNode: function (sequence) {
-    var tenChunked = sequence.match(/.{1,10}/g).join(" ");
+    var tenChunked = sequence.match(/.{1,10}/g).join(' ');
     var lineArr = tenChunked.match(/.{1,66}/g);
-    var maxLabelLength = ((lineArr.length * LETTERS_PER_LINE + 1).toString().length)
+    var maxLabelLength = (lineArr.length * LETTERS_PER_LINE + 1).toString()
+      .length;
     lineArr = _.map(lineArr, (l, i) => {
       var lineNum = i * LETTERS_PER_LINE + 1;
       var numSpaces = maxLabelLength - lineNum.toString().length;
-      var spacesStr = Array(numSpaces + 1).join(" ");
+      var spacesStr = Array(numSpaces + 1).join(' ');
       return `${spacesStr}${lineNum} ${l}`;
     });
     return _.map(lineArr, (l, i) => {
-      return <span key={'seq' + i}>{l}<br /></span>;
+      return (
+        <span key={'seq' + i}>
+          {l}
+          <br />
+        </span>
+      );
     });
   },
 
   _getComplexSequenceNode: function (sequence) {
     var maxLabelLength = sequence.length.toString().length + 1;
-    var chunked = sequence.split("");
+    var chunked = sequence.split('');
     var offset = this.state.activeSequenceType === '1kb' ? 1000 : 0;
-        
+
     return _.map(chunked, (c, i) => {
       i++;
-      var sp = (i % LETTERS_PER_CHUNK === 0 && !(i % LETTERS_PER_LINE === 0)) ? " " : "";
-      var cr = (i % LETTERS_PER_LINE === 0) && (i > 1) ? "\n" : "";
+      var sp =
+        i % LETTERS_PER_CHUNK === 0 && i % LETTERS_PER_LINE !== 0 ? ' ' : '';
+      var cr = i % LETTERS_PER_LINE === 0 && i > 1 ? '\n' : '';
       var str = c + sp + cr;
       var _classType = this._getSubFeatureTypeFromIndex(i - offset);
 
-      var labelNode = (i - 1) % LETTERS_PER_LINE === 0 ? <span style={{ color: "#6f6f6f" }}>{`${Array(maxLabelLength - i.toString().length).join(" ")}${i} `}</span> : null;
+      var labelNode =
+        (i - 1) % LETTERS_PER_LINE === 0 ? (
+          <span style={{ color: '#6f6f6f' }}>{`${Array(
+            maxLabelLength - i.toString().length
+          ).join(' ')}${i} `}</span>
+        ) : null;
 
-      return <span key={`sequence-car${i}`} style={{ color: subFeatureColorScale(_classType) }}>{labelNode}{str}</span>;
+      return (
+        <span
+          key={`sequence-car${i}`}
+          style={{ color: subFeatureColorScale(_classType) }}
+        >
+          {labelNode}
+          {str}
+        </span>
+      );
     });
   },
 
   _canColorSubFeatures: function () {
     // must have sub-features and be on genomic DNA
-    if (this.props.subFeatureData.length <= 1 || this.state.activeSequenceType === "coding_dna" || this.state.activeSequenceType === "protein") {
+    if (
+      this.props.subFeatureData.length <= 1 ||
+      this.state.activeSequenceType === 'coding_dna' ||
+      this.state.activeSequenceType === 'protein'
+    ) {
       return false;
     }
 
     // no overlaps
-    var _allTracks = _.uniq(_.map(this.props.subFeatureData, f => { return f.track; }));
+    var _allTracks = _.uniq(
+      _.map(this.props.subFeatureData, (f) => {
+        return f.track;
+      })
+    );
     if (_allTracks.length > 1) return false;
 
     return true;
   },
 
   _getActiveSequence: function () {
-    return _.findWhere(this.props.sequences, { key: this.state.activeSequenceType });
-  }
-
+    return _.findWhere(this.props.sequences, {
+      key: this.state.activeSequenceType,
+    });
+  },
 });
 
 module.exports = SequenceToggler;
--- client/jsx/views/example_view.jsx
+++ client/jsx/views/example_view.jsx
@@ -1,12 +1,12 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
+// var React = require("react");
+// var ReactDOM = require("react-dom");
 
 // var ExampleReactComponent = require("../components/example/example_component.jsx");
 
 var exampleView = {};
 exampleView.render = function () {
-	// add view logic here
-	// ReactDOM.render(<ExampleReactComponent />, document.getElementById("j-main"));
+  // add view logic here
+  // ReactDOM.render(<ExampleReactComponent />, document.getElementById("j-main"));
 };
 
 module.exports = exampleView;
