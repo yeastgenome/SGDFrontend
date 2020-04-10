eb51b178ea69f67a8a8256df471f29047f979113
Sagar <sagar.jhaa@gmail.com>
2020-04-10 00:01:25 -0700

    deepscan

--- client/jsx/components/widgets/paginator.jsx
+++ client/jsx/components/widgets/paginator.jsx
@@ -1,13 +1,12 @@
-"use strict";
-var React = require("react");
-var _ = require("underscore");
-var Radium = require("radium");
+'use strict';
+var React = require('react');
+var Radium = require('radium');
 
 var Paginator = React.createClass({
   propTypes: {
     onPaginate: React.PropTypes.func, // (newPageNum) =>
     currentPage: React.PropTypes.number,
-    totalPages: React.PropTypes.number.isRequired
+    totalPages: React.PropTypes.number.isRequired,
   },
 
   getDefaultProps: function () {
@@ -15,38 +14,60 @@ var Paginator = React.createClass({
   },
 
   render: function () {
-    var text = `Page ${this.props.currentPage + 1} of ${this.props.totalPages.toLocaleString()}`;
+    var text = `Page ${
+      this.props.currentPage + 1
+    } of ${this.props.totalPages.toLocaleString()}`;
     var prevPageNum = this.props.currentPage - 1;
     var nextPageNum = this.props.currentPage + 1;
-    var onClickPrev =  e => { this._onPaginate(prevPageNum); };
-    var onClickNext = e => { this._onPaginate(nextPageNum); };
-    var prevKlass = prevPageNum < 0 ? " disabled" : "";
-    var nextKlass = nextPageNum > this.props.totalPages - 1 ? " disabled" : "";
+    var onClickPrev = (e) => {
+      this._onPaginate(prevPageNum);
+    };
+    var onClickNext = (e) => {
+      this._onPaginate(nextPageNum);
+    };
+    var prevKlass = prevPageNum < 0 ? ' disabled' : '';
+    var nextKlass = nextPageNum > this.props.totalPages - 1 ? ' disabled' : '';
 
     return (
       <div style={[style.wrapper]}>
         <p style={[style.text]}>{text}</p>
-        <p style={[style.text]}><a onClick={onClickPrev} className={"button tiny secondary" + prevKlass} style={[style.button]}><i className="fa fa-chevron-left" /></a> <a onClick={onClickNext} className={"button tiny secondary" + nextKlass} style={[style.button]}><i className="fa fa-chevron-right" /></a></p>
+        <p style={[style.text]}>
+          <a
+            onClick={onClickPrev}
+            className={'button tiny secondary' + prevKlass}
+            style={[style.button]}
+          >
+            <i className="fa fa-chevron-left" />
+          </a>{' '}
+          <a
+            onClick={onClickNext}
+            className={'button tiny secondary' + nextKlass}
+            style={[style.button]}
+          >
+            <i className="fa fa-chevron-right" />
+          </a>
+        </p>
       </div>
     );
   },
 
   _onPaginate: function (newPageNum) {
     if (newPageNum < 0 || newPageNum > this.props.totalPages - 1) return;
-    if (typeof this.props.onPaginate === "function") this.props.onPaginate(newPageNum);
-  }
+    if (typeof this.props.onPaginate === 'function')
+      this.props.onPaginate(newPageNum);
+  },
 });
 
 const style = {
   wrapper: {
-    marginBottom: '1rem'
+    marginBottom: '1rem',
   },
   button: {
-    userSelect: 'none'
+    userSelect: 'none',
   },
   text: {
-    marginBottom: '0.5rem'
-  }
-}
+    marginBottom: '0.5rem',
+  },
+};
 
 module.exports = Radium(Paginator);
--- client/jsx/redux_application.jsx
+++ client/jsx/redux_application.jsx
@@ -5,21 +5,23 @@ import { Provider } from 'react-redux';
 // import routes
 import Routes from './routes.jsx';
 
-const ReduxApplication = React.createClass({
-  propTypes: {
-    history: React.PropTypes.object.isRequired,
-    store: React.PropTypes.object.isRequired
-  },
+class ReduxApplication extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-	render() {
+  render() {
     return (
       <Provider store={this.props.store}>
-        <Router history={this.props.history}>
-          {Routes}
-        </Router>
+        <Router history={this.props.history}>{Routes}</Router>
       </Provider>
     );
-	}
-});
+  }
+}
+
+ReduxApplication.propTypes = {
+  history: React.PropTypes.object.isRequired,
+  store: React.PropTypes.object.isRequired,
+};
 
 module.exports = ReduxApplication;
