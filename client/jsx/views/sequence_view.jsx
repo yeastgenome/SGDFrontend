4b98e3f12ce954338ce297b22946c27cdf0ca103
sweng66 <sweng@stanford.edu>
2020-04-24 09:33:02 -0700

    adding code to display LSP/PROTEIN/SEQUENCE pages for genes that are not in REF genome  (#297)
    * fixed to display the complex/pathway names along with gene names in REF page
    
    * fixed to work with NISS genes

--- client/jsx/views/sequence_view.jsx
+++ client/jsx/views/sequence_view.jsx
@@ -12,11 +12,12 @@ sequenceView.render = function () {
   document.getElementById("sequence_tab").className += " active";
 
   // define render nav bar function
-  var renderNavBar = function (hasAltStrains, hasOtherStrains) {
+  var renderNavBar = function (hasAltStrains, hasOtherStrains, mainStrain) {
     var _tabModel = new TabsModel({
       tabType: "sequence",
       hasAltStrains: hasAltStrains,
       hasOtherStrains: hasOtherStrains,
+      mainStrain: mainStrain
     });
     var _navTitleText = _tabModel.getNavTitle(bootstrappedData.displayName, bootstrappedData.formatName);
     ReactDOM.render(
@@ -27,7 +28,7 @@ sequenceView.render = function () {
 
   // render navbar with alt & other strain info
   var _detailsCallback = (err, detailsModel) => {
-    renderNavBar(detailsModel.attributes.altStrains.length, detailsModel.attributes.otherStrains.length);
+    renderNavBar(detailsModel.attributes.altStrains.length, detailsModel.attributes.otherStrains.length, bootstrappedData.main_strain);
   };
 
   // async sequence view, fetches data, renders main strain, alt strains, and other strains (if present)
@@ -35,9 +36,14 @@ sequenceView.render = function () {
   var _showVariants = bootstrappedData.featureType === 'ORF';
   ReactDOM.render(
     <AsyncSequenceView
-      locusId={bootstrappedData.locusId} locusDisplayName={bootstrappedData.displayName}
-      locusFormatName={bootstrappedData.formatName} locusHistoryData={bootstrappedData.locusHistory}
-      detailsCallback={_detailsCallback} locusSGDID={bootstrappedData.sgdid} showVariants={_showVariants}
+      locusId={bootstrappedData.locusId} 
+      mainStrain={bootstrappedData.main_strain}
+      locusDisplayName={bootstrappedData.displayName}
+      locusFormatName={bootstrappedData.formatName} 
+      locusHistoryData={bootstrappedData.locusHistory}
+      detailsCallback={_detailsCallback} 
+      locusSGDID={bootstrappedData.sgdid} 
+      showVariants={_showVariants}
     />,
     document.getElementById("sequence-viz")
   );
