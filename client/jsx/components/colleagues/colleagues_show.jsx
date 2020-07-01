eec5998cec4583ea8183df29798e32e64794b1b2
felixgondwe <gondwefelix8@gmail.com>
2020-07-01 12:42:04 -0700

    undefined formatName bug fix

--- client/jsx/components/colleagues/colleagues_show.jsx
+++ client/jsx/components/colleagues/colleagues_show.jsx
@@ -15,7 +15,7 @@ const ColleaguesShow = createReactClass({
         <ColleaguesFormShow
           isReadOnly={true}
           isCurator={false}
-          colleagueDisplayName={this.props.routeParams.formatName}
+          colleagueDisplayName={this.props.match.params.formatName}
         />
         <a className="button secondary small" href="/colleague_update">
           Update Colleague
