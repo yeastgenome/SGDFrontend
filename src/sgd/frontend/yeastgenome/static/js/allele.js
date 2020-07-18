a7752f8fae0ef5bf73fe02cf49184f7a6f6180e9
Shuai Weng <sweng@stanford.edu>
2020-07-17 17:07:10 -0700

    testing

--- src/sgd/frontend/yeastgenome/static/js/allele.js
+++ src/sgd/frontend/yeastgenome/static/js/allele.js
@@ -4,43 +4,11 @@ $(document).ready(function() {
 	$.getJSON('/backend/allele/' + allele['sgdid']  + '/phenotype_details', function(data) {
 	    var phenotype_table = create_phenotype_table(data);
   	    create_download_button("phenotype_table_download", phenotype_table, allele['display_name'] + "_phenotype_annotations");
-            var refs = create_reference_list(allele['references']);
 	});
 
 
         
 });
-
-function create_reference_list(references) {
-
-    return "Hello world";
-    
-    // var itemNodes = _.map(references, (r, i) => {
-    //  var _text = r.citation.replace(r.display_name, '');
-    //  var refNodes = _.map(r.urls, (url, _i) => {
-    //    return (
-    //      <li key={'refListInner' + _i}>
-    //        <a href={url.link}>{url.display_name}</a>
-    //      </li>
-    //    );
-    //  });
-    //  refNodes.unshift(
-    //    <li key={'sgdNode' + i}>
-    //      <a href={r.link}>SGD Paper</a>
-    //    </li>
-    //  );
-    //  var pubmedNode = r.pubmed_id ? <small>PMID: {r.pubmed_id}</small> : null;
-    //  return (
-    //    <li className="reference-list-item" key={'refListOuter' + i}>
-    //      <a href={r.link}>{r.display_name}</a> {_text} {pubmedNode}
-    //      <ul className="ref-links">{refNodes}</ul>
-    //    </li>
-    //  );
-    // });
-
-    // return <ol className="reference-list" id='reference'>{itemNodes}</ol>;
-
-}
 
 function create_phenotype_table(data) {
   	var datatable = [];
@@ -59,7 +27,7 @@ function create_phenotype_table(data) {
     options["oLanguage"] = {"sEmptyTable": "No phenotype data for " + allele['display_name']};
 	options["aaData"] = datatable;
 
-    return create_table("phenotype_table", options);
+    return create_table("allele_phenotype_table", options);
 }
 
 
