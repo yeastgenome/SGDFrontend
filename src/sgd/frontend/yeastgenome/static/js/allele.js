80e418fd4d0de892e25a32b05a27e7a8a130f08e
Shuai Weng <sweng@stanford.edu>
2020-07-22 21:16:37 -0700

    testing

--- src/sgd/frontend/yeastgenome/static/js/allele.js
+++ src/sgd/frontend/yeastgenome/static/js/allele.js
@@ -38,7 +38,7 @@ function create_interaction_table(data) {
 
         options["bPaginate"] = true;
         options["aaSorting"] = [[5, "asc"]];
-        options["aoColumns"] = [{"bSearchable":false, "bVisible":false}, {"bSearchable":false, "bVisible":false}, {"bSearchable":false, "bSortable":false}, {"bSearchable":false, "bVisible":false}, {"bSearchable":false, "bVisible":false}, null, {"bSearchable":false, "bVisible":false}, null, null, null, null, null, null, {"bSearchable":false, "bVisible":false}, null, {"bSearchable":false, "bVisible":false}];
+        options["aoColumns"] = [{"bSearchable":false, "bVisible":false}, {"bSearchable":false, "bVisible":false}, {"bSearchable":false, "bSortable":false}, null, {"bSearchable":false, "bVisible":false}, null, {"bSearchable":false, "bVisible":false}, null, null, null, null, null, null, {"bSearchable":false, "bVisible":false}, null, null];
         options["oLanguage"] = {"sEmptyTable": "No interaction data for " + allele['display_name']};
         options["aaData"] = datatable;
     }
