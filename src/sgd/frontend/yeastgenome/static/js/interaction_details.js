1195ba879d9633bc0b2a270da001a5c7a5109993
Shuai Weng <sweng@stanford.edu>
2020-08-18 22:22:45 -0700

    testing

--- src/sgd/frontend/yeastgenome/static/js/interaction_details.js
+++ src/sgd/frontend/yeastgenome/static/js/interaction_details.js
@@ -114,7 +114,7 @@ function create_genetic_interaction_table(data) {
 			var allele2_name = allele["allele2_name"];
 			var allele_pair = "<a href='/allele/'" + allele1_name + "' target='_new'>" + allele1_name + "</a>";
 			if (allele2_name != '') {
-			    allele_pair = allele_pair + " - " + "<a href='/allele/'" + allele2_name + "' target='_new'>" + allele2_name + "</a>";
+			    allele_pair = allele_pair + " | " + "<a href='/allele/'" + allele2_name + "' target='_new'>" + allele2_name + "</a>";
 			}
 			var score = "SGA score = " + allele["sga_score"] + ", P-value = " + allele["pvalue"];
 			datatable.push(genetic_interaction_data_to_table(data[i], k++, allele_pair, score));
