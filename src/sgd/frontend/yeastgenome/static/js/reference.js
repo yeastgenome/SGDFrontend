f5cb0275e1e29e283c1e82e3887fd8ebaa35f873
Shuai Weng <sweng@stanford.edu>
2020-08-19 21:34:03 -0700

    testing

--- src/sgd/frontend/yeastgenome/static/js/reference.js
+++ src/sgd/frontend/yeastgenome/static/js/reference.js
@@ -243,7 +243,7 @@ function create_genetic_interaction_table(data) {
         options["bPaginate"] = true;
         options["aaSorting"] = [[5, "asc"]];
         options["aoColumns"] = [{"bSearchable":false, "bVisible":false}, {"bSearchable":false, "bVisible":false}, {"bSearchable":false, "bSortable":false}, null, {"bSearchable":false, "bVisible":false}, null, {"bSearchable":false, "bVisible":false}, null, null, null, null, null, null, {"bSearchable":false, "bVisible":false}, null, {"bSearchable":false, "bVisible":false}];
-        options["oLanguage"] = {"sEmptyTable": "No genetic interaction data for " + locus['display_name']};
+        options["oLanguage"] = {"sEmptyTable": "No genetic interaction data for " + reference['display_name']};
         options["aaData"] = datatable;
     }
 
