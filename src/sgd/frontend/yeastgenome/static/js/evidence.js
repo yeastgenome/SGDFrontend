1ba469b39de3d6a1aef8b22f623d23ea0b2a3d67
sweng66 <sweng@stanford.edu>
2021-04-28 14:02:35 -0700

    rebase code to master

--- src/sgd/frontend/yeastgenome/static/js/evidence.js
+++ src/sgd/frontend/yeastgenome/static/js/evidence.js
@@ -61,8 +61,9 @@ function complement_data_to_table(evidence) {
     
     let complement = create_link(evidence['dbxref_id'], evidence['obj_url']);
     let paper = create_link(evidence['references'][0]['display_name'], evidence['references'][0]['link']) + "<br>PMID:" + evidence['references'][0]['pubmed_id'];
+    let gene = create_link(evidence['locus']['display_name'], evidence['locus']['link']);
 	
-    return [evidence['id'], evidence['locus']['id'], "<i>" + evidence['species'] + "</i>", complement, evidence['strain_background'], evidence['direction'], evidence['curator_comment'], evidence['source']['display_name'], paper]
+    return [evidence['id'], evidence['locus']['id'], gene, "<i>" + evidence['species'] + "</i>", complement, evidence['strain_background'], evidence['direction'], evidence['curator_comment'], evidence['source']['display_name'], paper]
 	    
 }
 
