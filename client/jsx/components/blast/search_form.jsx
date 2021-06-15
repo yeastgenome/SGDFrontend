a76bf759eaf6d3b2d7fbbf4d7b75d9afd65df498
sweng66 <sweng@stanford.edu>
2021-06-15 11:35:01 -0700

    testing

--- client/jsx/components/blast/search_form.jsx
+++ client/jsx/components/blast/search_form.jsx
@@ -8,7 +8,8 @@ var RadioSelector = require('./radio_selector.jsx');
 var BlastBarChart = require('./blast_bar_chart.jsx');
 var Params = require('../mixins/parse_url_params.jsx');
 
-var BLAST_URL = '/run_blast';
+// var BLAST_URL = '/run_blast';
+var BLAST_URL = 'https://blast.dev.yeastgenome.org/blast_search'
 
 var SearchForm = createReactClass({
   displayName: 'SearchForm',
