82d8f7bd4543f7da289ef6dd723016fef688a5d2
sweng66 <sweng@stanford.edu>
2021-06-28 10:51:53 -0700

    fixed setup.py to work for both new/old pip versions

--- setup.py
+++ setup.py
@@ -2,14 +2,23 @@ import os
 
 import pip
 from setuptools import setup, find_packages
-# from pip.download import PipSession
-# from pip.req import parse_requirements
-from pip._internal.download import PipSession
-from pip._internal.req import parse_requirements
+# from pip._internal.download import PipSession
+# from pip._internal.req import parse_requirements
+try:
+    from pip.req import parse_requirements
+except ImportError:
+    from pip._internal.req import parse_requirements
 
-install_reqs = parse_requirements("requirements.txt", session=PipSession())
-requires = [str(ir.req) for ir in install_reqs]
+# install_reqs = parse_requirements("requirements.txt", session=PipSession()) 
+# requires = [str(ir.req) for ir in install_reqs]
+install_reqs = parse_requirements("requirements.txt", session=False)
 
+requires = None
+try:
+    requires = [str(ir.req) for ir in install_reqs]
+except:
+    requires = [str(ir.requirement) for ir in install_reqs]
+    
 setup(name='SGDFrontend',
       version='0.0',
       description='SGDFrontend',
