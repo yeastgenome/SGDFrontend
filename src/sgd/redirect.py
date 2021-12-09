9b89767e337508462cef6ced92008080c14a261b
sweng66 <sweng@stanford.edu>
2021-12-09 12:22:57 -0800

    testing

--- src/sgd/redirect.py
+++ src/sgd/redirect.py
@@ -2,15 +2,16 @@ import json
 from pyramid.response import Response
 # from urllib.request import Request, urlopen
 # from urllib.error import URLError, HTTPError
-from src.sgd.frontend import config
-import requests
+# from src.sgd.frontend import config
+# import requests
 
 def do_redirect(request):
 
     p = dict(request.params)
     data = {}
     if p.get('param'):
-        url = config.backend_url
+        # url = config.backend_url
+        url = 'https://www.qa.yeastgenome.org/backend'
         if url[-1] == '/':
             url[0:-1]
         if p.get('param').startswith('/'):
