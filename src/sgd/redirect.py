1d75404427c924b8b2053496eb56ad3b15fa80f0
sweng66 <sweng@stanford.edu>
2021-12-09 11:16:42 -0800

    testing

--- src/sgd/redirect.py
+++ src/sgd/redirect.py
@@ -1,9 +1,10 @@
 import json
 from pyramid.response import Response
 # from urllib.request import Request, urlopen
+from urllib.request import Request, urlopen  
 from urllib.error import URLError, HTTPError
 from src.sgd.frontend import config
-import requests
+#import requests
 
 def do_redirect(request):
 
@@ -18,11 +19,11 @@ def do_redirect(request):
         else:
             url = url + '/' + p.get('param')        
         try:
-            #req = Request(url=url)
-            #res = urlopen(req)
-            #data = json.loads(res.read())
-            res = requests.get(url)
-            data = json.loads(res.text)
+            req = Request(url=url)
+            res = urlopen(req)
+            data = json.loads(res.read())
+            # res = requests.get(url)
+            # data = json.loads(res.text)
         except HTTPError:
             return 404
     return Response(body=json.dumps(data), content_type='application/json')
