21f48f911d0f6546a2a94d024463203cfbcdea42
sweng66 <sweng@stanford.edu>
2022-01-25 19:59:29 -0800

    testing

--- client/jsx/components/primer3/primer3.jsx
+++ client/jsx/components/primer3/primer3.jsx
@@ -5,8 +5,7 @@ import t from 'tcomb-form';
 import createReactClass from 'create-react-class';
 const DataTable = require('../widgets/data_table.jsx');
 import PropTypes from 'prop-types';
-const PRIMER3URL = '/redirect_backend?param=primer3';
-// const PRIMER3URL = 'https://www.yeastgenome.org/backend/primer3'; 
+const PRIMER3URL = '/run_primer3';
 const queryString = require('query-string');
 
 const Primer3 = createReactClass({
