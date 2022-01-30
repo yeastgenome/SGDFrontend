c80a299a96667d03bc479b4e5bba6a705dd39570
sweng66 <sweng@stanford.edu>
2022-01-29 16:54:53 -0800

    testing

--- src/sgd/tools/primer3/__init__.py
+++ src/sgd/tools/primer3/__init__.py
@@ -4,23 +4,22 @@ from src.sgd.frontend import config
 from urllib.request import Request, urlopen
 from urllib.error import URLError
 from urllib.parse import urlencode
+import requests
 
 url = config.backend_url + '/primer3'
 
 def do_primer3(request):
-
-    # p = dict(request.params)
-    # paramData = urlencode(p)
-    params = request.json_body
-    p_keys = list(params.keys())
-    paramData = {}
-    for key in p_keys:
-        paramData[key] = params.get(key)
     
-    req = Request(url=url, data=paramData.encode('utf-8'))
-    res = urlopen(req)
-    result = res.read().decode('utf-8')
-    data = json.loads(result)
+    params = request.json_body
+
+    # params = {'gene_name': 'ACT1', 'sequence': None, 'input_start': 500, 'input_end': 900, 'maximum_product_size': None, 'end_point': 'NO', 'minimum_length': 18, 'optimum_primer_length': 20, 'maximum_length': 23, 'minimum_tm': 57, 'optimum_tm': 59, 'maximum_tm': 62, 'minimum_gc': 30, 'optimum_gc': 50, 'maximum_gc': 70, 'max_self_complementarity': 8, 'max_three_prime_self_complementarity': 3, 'max_pair_complementarity': 8, 'max_three_prime_pair_complementarity': 3}
+    
+    paramData = urlencode(params)
+
+    # paramData = 'gene_name=ACT1&sequence=None&input_start=500&input_end=900&maximum_product_size=None&end_point=NO&minimum_length=18&optimum_primer_length=20&maximum_length=23&minimum_tm=57&optimum_tm=59&maximum_tm=62&minimum_gc=30&optimum_gc=50&maximum_gc=70&max_self_complementarity=8&max_three_prime_self_complementarity=3&max_pair_complementarity=8&max_three_prime_pair_complementarity=3'
+    
+    res = requests.get(url, params=paramData)
+    data =  json.loads(res.text)
     
     return Response(body=json.dumps(data), content_type='application/json', charset='UTF-8')
 
