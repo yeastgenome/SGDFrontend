a8a14a6ac64b147a3065a7b329a51e207873324b
sweng66 <sweng@stanford.edu>
2022-02-19 19:26:37 -0800

    testing

--- client/jsx/components/gotools/gotermfinder_form.jsx
+++ client/jsx/components/gotools/gotermfinder_form.jsx
@@ -576,7 +576,7 @@ const GoTermFinder = createReactClass({
     }
     
     if (warningMsg != '') {
-      var h = ambiguousGeneCount * 100;
+      var h = ambiguousGeneCount * 120 + 100;
       var win = window.open('', 'popUpWindow', "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=600,height="+ h + ",top="+(screen.height-600)+",left="+(screen.width-500));
       win.document.body.innerHTML = "<html>" + warningMsg + "</html>";
       e.preventDefault();
