1df180b1aadea78bc64125216b06b09b785a7e69
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2022-06-23 13:55:26 -0700

    Reverting recent changes.

--- src/sgd/frontend/default_config.py
+++ src/sgd/frontend/default_config.py
@@ -1,9 +1,9 @@
 heritage_url = None
-backend_url = 'https://be.staging.yeastgenome.org/'
+backend_url = 'https://www.yeastgenome.org/webservice'
 secret_key = 'secret key here'
 sender = 'email address here'
 author_response_file = 'file with full path here'
 compute_url = ''
 # elasticsearch_address = 'http://localhost:9200'
-elasticsearch_address = 'https://es.staging.yeastgenome.org/'
+elasticsearch_address = 'http://ec2-34-221-214-103.us-west-2.compute.amazonaws.com:9200'
 log_directory = None
