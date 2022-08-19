d5cfa1394a8c23f8e5b53b370137668441b24456
sweng66 <sweng@stanford.edu>
2022-08-19 13:29:36 -0700

    testing

--- src/sgd/frontend/yeastgenome/static/js/rna_secondary_structure.js
+++ src/sgd/frontend/yeastgenome/static/js/rna_secondary_structure.js
@@ -10,6 +10,6 @@ function show_secondary_structure(URS_ID) {
     console.log(html)
     
     var win = window.open('', 'popUpWindow', "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=1000,height=800,top="+(screen.height-600)+",left="+(screen.width-500));
-    win.document.body.innerHTML = "<html><head><title>R2DT</title></head><body>" + html + "<script type='text/javascript' src='https://rnacentral.github.io/r2dt-web/dist/r2dt-web.js'></script></body></html>";
+    win.document.body.innerHTML = "<html><head><title>R2DT</title></head><body><r2dt-web search=\"{&#34;urs&#34;:&#34;URS0000289310&#34;}\"></r2dt-web><script type='text/javascript' src='https://rnacentral.github.io/r2dt-web/dist/r2dt-web.js'></script></body></html>";
 
 }
