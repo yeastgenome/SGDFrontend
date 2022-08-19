a14032d1b5fc8166d00995c54b971b90672d5930
sweng66 <sweng@stanford.edu>
2022-08-18 19:38:03 -0700

    testing

--- src/sgd/frontend/yeastgenome/static/js/rna_secondary_structure.js
+++ src/sgd/frontend/yeastgenome/static/js/rna_secondary_structure.js
@@ -1,8 +1,8 @@
 function show_secondary_structure(URS_ID) {
 
-    var search = '{"urs": "' + URS_ID + '"}'
-    var html = '<r2dt-web search=' + search + ' />'
-
+    var search = "{'urs': '" + URS_ID + "'}"
+    var html = "<r2dt-web search='" + search + "' />"
+    
     console.log(html)
     
     var win = window.open('', 'popUpWindow', "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=800,height=800,top="+(screen.height-600)+",left="+(screen.width-500));
