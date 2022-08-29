757df0d7fcc81b0cbf4b8e616bb8481dcfae25a9
sweng66 <sweng@stanford.edu>
2022-08-28 19:50:51 -0700

    testing

--- client/jsx/components/sequence/rna_2d_structure_viewer.jsx
+++ client/jsx/components/sequence/rna_2d_structure_viewer.jsx
@@ -22,10 +22,11 @@ module.exports = createReactClass({
     // var searchNode = "{&#34;urs&#34;:&#34;" + this.props.ursID + "&#34;}"
     // <r2dt-web search={searchNode}></r2dt-web>
     // <r2dt-web search="{&#34;urs&#34;:&#34;URS000029384E&#34;}"></r2dt-web>
-    var searchNode = "{&#34;urs&#34;:&#34;" + "URS000029384E&#34" + "&#34;}"   
+    var URS_ID = "URS000029384E"
+      
     return (
       <div class="panel">                                                                        
-        <r2dt-web search={searchNode}></r2dt-web>
+        <r2dt-web search="{&#34;urs&#34;:&#34;" + URS_ID + "&#34;}"></r2dt-web>
       </div>
     );
   },
