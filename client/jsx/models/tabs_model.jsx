8672a95721d6e0ca7939ae0b506fcaf9a9e45a58
sweng66 <sweng@stanford.edu>
2022-09-08 23:57:04 -0700

    testing

--- client/jsx/models/tabs_model.jsx
+++ client/jsx/models/tabs_model.jsx
@@ -36,7 +36,7 @@ module.exports = class TabsModel {
   }
 
   _getSequenceTabs() {
-    var ssElement = this.attributes.URS_ID
+    var ssElement = this.attributes.ursID
       ? { name: 'Secondary Structure', target: 'secondary_structure' }  
       : null;
     var altElement = this.attributes.hasAltStrains
