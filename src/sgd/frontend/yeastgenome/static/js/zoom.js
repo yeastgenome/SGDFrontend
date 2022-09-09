000172ee0c95046f8614f062e7aa5b14e22c3c4e
sweng66 <sweng@stanford.edu>
2022-09-08 20:59:00 -0700

    adding zoom in/zoom out function

---
+++ src/sgd/frontend/yeastgenome/static/js/zoom.js
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+    var clicked = 1;
+    function zoom() {
+        if (clicked == 3) {
+            clicked = 1;
+        }
+        else {
+            clicked = 3;
+        }
+        zoomOut(clicked)
+    }
+
+    function zoomOut(count) {
+        var image = document.getElementById('strucImg');
+        var src = image.src;
+        image.src = src.substring(0, src.indexOf('dimensions=')) + 'dimensions=' + (200 * count);
+        image.style.display = "block";
+    }
+</script>
