2de5bb1174d1d8ec360faff17153c16cd80667ef
sweng66 <sweng@stanford.edu>
2022-10-09 08:06:08 -0700

    adding code for redirecting GO version

--- src/sgd/redirect.py
+++ src/sgd/redirect.py
@@ -12,18 +12,21 @@ def do_redirect(request):
     
     data = {}
     if p.get('param'):
-        url = os.environ['BACKEND_URL']
-        if url[-1] == '/':
-            url[0:-1]
-        if p.get('param').startswith('/'):
-            url = url + p.get('param')
+        if p.get('param') == 'go_release':
+            url = 'http://current.geneontology.org/metadata/release-date.json'
         else:
-            url = url + '/' + p.get('param')
-        if not p.get('param').endswith('locus_details_all') and not p.get('param').startswith('go/'):
-            for key in p:
-                if key == 'param':
-                    continue
-                url = url + "&" + key + "=" + p.get(key)
+            url = os.environ['BACKEND_URL']
+            if url[-1] == '/':
+                url[0:-1]
+            if p.get('param').startswith('/'):
+                url = url + p.get('param')
+            else:
+                url = url + '/' + p.get('param')
+            if not p.get('param').endswith('locus_details_all') and not p.get('param').startswith('go/'):
+                for key in p:
+                    if key == 'param':
+                        continue
+                    url = u + "&" + key + "=" + p.get(key)
         try:
             #req = Request(url=url)
             #res = urlopen(req)
