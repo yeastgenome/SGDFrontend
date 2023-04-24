c5143235070d25c6ea696849924638f22320e72c
sweng66 <sweng@stanford.edu>
2023-04-24 12:34:25 -0700

    fixed code to search newly set up PatMatch server in fargate

--- src/sgd/tools/patmatch/__init__.py
+++ src/sgd/tools/patmatch/__init__.py
@@ -1,11 +1,11 @@
+import os
 import json
+import requests
 from pyramid.response import Response
-# from src.sgd.frontend import config
-from urllib.request import Request, urlopen
-from urllib.error import URLError
 
-patmatch_url = "https://patmatch.yeastgenome.org/"
-                
+patmatch_url = os.environ['PATMATCH_SERVER'] + "/patmatch"
+
+
 def do_patmatch(request):
 
     p = dict(request.params)
@@ -13,96 +13,41 @@ def do_patmatch(request):
     if p.get('conf'):
         data = _get_config()
         return Response(body=json.dumps(data), content_type='application/json', charset='UTF-8')
-
+        
     if p.get('seqname'):
-        data = _get_seq(p)
+        data = _get_seq(p.get('seqname'), p.get('dataset'))
         return Response(body=json.dumps(data), content_type='application/json', charset='UTF-8')
 
-    data = _run_patmatch(p)
+    data = _run_patmatch(request)
     return Response(body=json.dumps(data), content_type='application/json', charset='UTF-8')
-
-def _get_seq(p):
-
-    import urllib.request, urllib.parse, urllib.error
-
-    paramData = urllib.parse.urlencode({ 'seqname': p.get('seqname'),
-                                         'dataset': p.get('dataset') });
-
-    url = patmatch_url + "cgi-bin/aws-patmatch"
-    req = Request(url=url, data=paramData.encode('utf-8'))
-    res = urlopen(req)
-    result = res.read();
-
-    return json.loads(result.decode('utf-8'))
-
-
-def _run_patmatch(p):
-
-    paramData = _construct_patmatch_parameters(p)
-
-    url = patmatch_url + "cgi-bin/aws-patmatch" 
-   
-    req = Request(url=url, data=paramData.encode('utf-8'))
-    res = urlopen(req)
-    result = res.read().decode('utf-8');
-
-    dataSet = result.split("\t")
-
-    if dataSet[1]:
-        data = { "hits": json.loads(dataSet[0]),
-                 "uniqueHits": dataSet[1],
-                 "totalHits": dataSet[2],
-                 "downloadUrl": dataSet[3]}
-    else:
-        data = { "hits": json.loads(dataSet[0]),
-                 "uniqueHits": 0,
-                 "totalHits": 0,
-                 "downloadUrl": ""}
-                 
-    return data
-
-    data = json.loads(res.read())
-    return data
-
-def _construct_patmatch_parameters(p):
-
-    # insertion = 1 if p.get('insertion') else 0 
-    # deletion = 1 if p.get('deletion') else 0
-    # substitution = 1  if p.get('substitution') else 0
-    # strain = p.get('strain')
-    # if strand.startswith('Both'):
-    #    strand = 'both'
-    # elif strand.startswith('Reverse'):
-    #    strand = 'crick'
-    # else:
-    #    strand = 'watson'
-
-    seqtype = p.get('seqtype') if p.get('seqtype') is not None else 'pep'
-    dataset = p.get('dataset') if p.get('dataset') is not None else 'orf_pep'
     
-    import urllib.request, urllib.parse, urllib.error
+    
+def _get_seq(seqname, dataset):
 
-    paramData = urllib.parse.urlencode({ 'pattern': p.get('pattern'),
-                                   'strain': p.get('strain'),
-                                   'seqtype': seqtype,
-                                   'dataset': dataset,
-                                   'maxhits': p.get('max_hits'),
-                                   'strand': p.get('strand'),
-                                   'mismatch': p.get('mismatch'),
-                                   'insertion': p.get('insertion'),
-                                   'deletion': p.get('deletion'),
-                                   'substitution': p.get('substitution') })
-    return paramData
+    search_url = patmatch_url + "?dataset=" + dataset + "&seqname=" + seqname
+    return _get_data_from_server(search_url)
+
+
+def _run_patmatch(request):
+
+    post_data = request.POST
+
+    response = requests.post(patmatch_url, data=post_data)
+    data = json.loads(response.text)
+    return data
 
 
 def _get_config():
 
-    url = patmatch_url + "patmatch/patmatch.json"
-    req = Request(url)
-    res = urlopen(req)
-    data = json.loads(res.read().decode('utf-8')) 
-    return data
+    search_url = patmatch_url + "?conf=patmatch.json" 
+    return _get_data_from_server(search_url)
 
+
+def _get_data_from_server(url):
+
+    response = requests.get(url)
+    data = json.loads(response.text)    
+    return data
 
              
 
--- src/sgd/tools/restrictionmapper/__init__.py
+++ src/sgd/tools/restrictionmapper/__init__.py
@@ -1,97 +1,29 @@
+import os
 import json
+import requests
 from pyramid.response import Response
-# from src.sgd.frontend import config
-from urllib.request import Request, urlopen
-from urllib.error import URLError, HTTPError
 
-rest_url = "https://patmatch.yeastgenome.org/cgi-bin/aws-restrictionmapper2"
-validate_url = "https://www.yeastgenome.org/backend/locus/"
+rest_url = os.environ['PATMATCH_SERVER'] + "/restrictionmapper"
+
             
 def do_restmap(request):
 
-    p = dict(request.params)
-
-    data = _run_restrictionmap(p)
+    data = _run_restrictionmap(request)
     return Response(body=json.dumps(data), content_type='application/json', charset='UTF-8')
 
-def _run_restrictionmap(p):
 
-    [display_name, paramData] = _construct_parameters(p)
-
-    if display_name is None:
-        return { "ERROR": "not valid name" }
-
-    req = Request(url=rest_url, data=paramData.encode('utf-8'))
-    res = urlopen(req)
-    result = res.read().decode('utf-8');
-
-    dataSet = result.split("\t")
- 
-    if dataSet[1]:
-        data = { "data": json.loads(dataSet[0]),
-                 "seqName": display_name,
-                 "chrCoords": dataSet[2],
-                 "seqLength": dataSet[3],
-                 "notCutEnzyme": json.loads(dataSet[4]),
-                 "downloadUrl": dataSet[5],
-                 "downloadUrl4notCutEnzyme": dataSet[6] }
-    else:
-        data = { "ERROR": json.loads(dataSet[0]),
-                 "seqName": display_name,
-                 "chrCoords": dataSet[2],
-                 "seqLength": dataSet[3],
-                 "notCutEnzyme": [],
-                 "downloadUrl": '',
-                 "downloadUrl4notCutEnzyme": ''}
-
+def _run_restrictionmap(request):
+    
+    post_data = request.POST
+    response = requests.post(rest_url, data=post_data)
+    data = json.loads(response.text)
     return data
 
 
-def _construct_parameters(p):
+def _get_data_from_server(url):
 
-    import urllib.request, urllib.parse, urllib.error
-    
-    seq = p.get('seq')
-    if seq is None:
-        seq = ""
-    type = p.get('type')
-    if type is None:
-        type = ""
-    name = p.get('name')
-    sgdid = ""
-    display_name = ""
-    if name is not None:
-        name = name.replace("SGD:", "")
-        url = validate_url + name
-        res = _get_json_from_server(url)
-        if res and res != 404 and res != "null" and 'sgdid' in res:
-            gene_name = res.get('display_name')
-            if gene_name:
-                gene_name = gene_name.upper()
-            format_name = res.get('format_name').upper()
-            if name.upper() in [res.get('sgdid'), gene_name, format_name]:
-                sgdid = res.get('sgdid')
-                if gene_name and gene_name != format_name:
-                    display_name = res.get('display_name') + '/' + res.get('format_name')
-                else:
-                    display_name = res.get('format_name')
-    if sgdid or seq:
-        paramData = urllib.parse.urlencode({ 'seq': seq,
-                                       'type': type,
-                                       'name': sgdid })
-        return [display_name, paramData]
-    else:
-        return [None, None]
+    response = requests.get(url)
+    data = json.loads(response.text)    
+    return data
 
-
-def _get_json_from_server(url):
-
-    try:
-        req = Request(url)
-        res = urlopen(req)
-        data = json.loads(res.read().decode('utf-8'))
-        return data
-    except HTTPError:
-        return 404
-             
 
