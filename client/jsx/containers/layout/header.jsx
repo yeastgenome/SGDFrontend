0b2fc41df897f5540993f4e478e20809360d2ca3
Kalpana Karra <kkarra@stanford.edu>
2023-11-01 14:39:39 -0700

    change textpresso link to point to alliance (#400)

--- client/jsx/containers/layout/header.jsx
+++ client/jsx/containers/layout/header.jsx
@@ -431,7 +431,7 @@ class Header extends Component {
                 <li>
                   <a
                     className="disabled-header-a"
-                    href="https://textpresso.yeastgenome.org/"
+                    href="https://sgd-textpresso.alliancegenome.org/tpc/search"
                   >
                     Full-text Search
                   </a>
